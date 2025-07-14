4ca615774b8129ea529b3b8ea8dd87f6a88050dd
Taras P. Galchenko <t.galchenko@e-soft.ru>
2025-07-14 21:46:21 +0300

    L13

--- hanja-flash/chars.js
+++ hanja-flash/chars.js
@@ -2051,15 +2051,52 @@ const DATA =
 				"q": "前",
 				"s": "전",
 				"m": "in front",
-				"d": "A butcher hacking away with his knife at a slab of meat on his table with a pair of ram’s horns placed in front of him (or on his head, if you prefer).",
+				"d": "A butcher hacking away with his knife at a slab of meat on his table with a pair of ram’s <b>horns</b> placed <b>in front</b> of him (or on his head, if you prefer).",
 				"e": "午前 오전 forenoon"
 			},
 			{
-				"q": "",
-				"s": "",
-				"m": "",
-				"d": "",
+				"q": "夂",
+				"s": "치",
+				"m": "fall behind, *walking legs",
+				"d": "We call this element walking legs because it indicates “legs in motion,” whether you want to think of them as jogging or walking in long strides, as the shape seems to suggest.",
 				"e": ""
+			},
+			{
+				"q": "格",
+				"s": "격",
+				"m": "straighten, *status",
+				"d": "If you can imagine trees as <b>status</b> symbols (as they might well be for those living in Japan’s congested modern cities), then <b>each</b> might be aiming to have his/her own <b>tree</b>, just to keep up with the Suzukis.",
+				"e": "性格 성격 character"
+			},
+			{
+				"q": "各",
+				"s": "각",
+				"m": "each",
+				"d": "<i>Suum cuique</i>. Someone walking with his/her mouth between his/her walking legs, giving us an image of “To each his/her own.”",
+				"e": "各自 각자 each"
+			},
+			
+
+			{
+				"q": "略",
+				"s": "략",
+				"m": "suppress, control, *abbreviation",
+				"d": "Each field has its own abbreviations (chemistry, philosophy, sports, etc.). Needless to say, the stronger primitive goes to the left, even though the story would read them off the other wayaround.",
+				"e": "省略 생략 omission, ellipis, abbreviation"
+			},
+			{
+				"q": "客",
+				"s": "객",
+				"m": "guest",
+				"d": "When you are a guest in a courteous town, each household has its own way of welcoming you, and each house becomes your home.",
+				"e": "乘客 승객 passanger"
+			},
+			{
+				"q": "額",
+				"s": "액",
+				"m": "forehead",
+				"d": "Out of respect, you do not look straight into the eyes of your guest, but look at the top button of their collar. Here, however, you are told to look above the eyes to the forehead of your guest.",
+				"e": "金額 금액 amount/sum of money"
 			},
 			{
 				"q": "",
@@ -2069,12 +2106,33 @@ const DATA =
 				"e": ""
 			},
 			{
-				"q": "",
-				"s": "",
-				"m": "",
-				"d": "",
+				"q": "夏",
+				"s": "하",
+				"m": "summer",
+				"d": "In the summer, fatigued by the heat, your head hangs down nearly as far as your walking legs, or rather, your <b>“dragging legs.”</b> Note how the walking legs (instead of “animal legs”) are the only thing that distinguishes this character from that for page (head).",
+				"e": "夏季 하계 summer time"
+			},
+			{
+				"q": "処",
+				"s": "처",
+				"m": "place, *dispose",
+				"d": "Both the stretching out of the walking legs and the little bit of wind tucked in on the right suggest using one’s legs to kick something out of the way, or dispose of it.",
 				"e": ""
 			},
+			{
+				"q": "条",
+				"s": "조",
+				"m": "branch, twig",
+				"d": "Geppetto made walking legs for his little Pinocchio from two twigs of a tree, giving him a set of “twiggy” shanks.",
+				"e": ""
+			},
+			{
+				"q": "落",
+				"s": "락,낙",
+				"m": "fall",
+				"d": "When <b>water</b> falls, it splats and splashes; when <b>flower</b> petals fall, they float gently in the breeze. To <b>each</b> thing its own way of falling.",
+				"e": "落葉 낙엽 fallen leaves"
+			}
 		]
 	}
 ];
