a1c77fbc20809c2b9220e33b6fecde199d06ee79
Taras P. Galchenko <t.galchenko@e-soft.ru>
2025-08-06 20:40:27 +0300

    L15

--- hanja-flash/chars.js
+++ hanja-flash/chars.js
@@ -2064,7 +2064,7 @@ const DATA =
 			{
 				"q": "格",
 				"s": "격",
-				"m": "straighten, *status",
+				"m": "pattern, standard, *status",
 				"d": "If you can imagine trees as <b>status</b> symbols (as they might well be for those living in Japan’s congested modern cities), then <b>each</b> might be aiming to have his/her own <b>tree</b>, just to keep up with the Suzukis.",
 				"e": "性格 성격 character"
 			},
@@ -2148,7 +2148,7 @@ const DATA =
 				"s": "군",
 				"m": "army",
 				"d": "The crowned vehicle depicted here is a “chariot”, symbol of an army.",
-				"e": "군사 soldier"
+				"e": "軍士 군사 soldier"
 			},
 			{
 				"q": "輝",
@@ -2224,7 +2224,7 @@ const DATA =
 				"q": "熟",
 				"s": "숙",
 				"m": "ripen, mellow",
-				"d": "The tall and fat children from the last frame are here cast into a cauldron over an oven µame until they have sufficiently mellowed that they can return to the normal life of a student.",
+				"d": "The tall and fat children from the last frame are here cast into a cauldron over an oven flame until they have sufficiently mellowed that they can return to the normal life of a student.",
 				"e": "熟成 숙성 ripening, maturation"
 			},
 			{
@@ -2326,7 +2326,230 @@ const DATA =
 				"e": ""
 			}
 		]
+	},
+	{
+		"name" : "L15",
+		"cards" : [
+			{
+				"q": "学",
+				"s": "학",
+				"m": "study",
+				"d": "The child in the little red schoolhouse is there for one reason only: to study. Anyone who has gone through the schooling system knows well enough that study is one thing and learning quite another again.",
+				"e": ""
+			},
+			{
+				"q": "覚",
+				"s": "각",
+				"m": "memorize",
+				"d": "Memorizing involves seeing things that are not really there",
+				"e": ""
+			},
+			{
+				"q": "栄",
+				"s": "영",
+				"m": "flourish",
+				"d": "The botanical connotations of the word µourish (to bud and burst into bloom, much as a tree does) are part of the ideal of the schoolhouse as well.",
+				"e": ""
+			},
+			{
+				"q": "聿",
+				"s": "율",
+				"m": "writing brush",
+				"d": "Let the first 3 strokes represent the hairs at the tip of the brush, and the following two strokes the thumb and forefinger that guide it when you write.",
+				"e": ""
+			},
+			{
+				"q": "書",
+				"s": "서",
+				"m": "document, writing, *write",
+				"d": "The sage talks rapidly with his tongue wagging in his mouth, while the brush of the scribe runs apace to write down the master’s words.",
+				"e": "文書 문서 document"
+			},
+			{
+				"q": "津",
+				"s": "진",
+				"m": "ferry, ford, saliva, *haven",
+				"d": "Seeing the tiny boats of poor mortals tossed about in a stormy sea like so many corks, the All-Merciful took its brushand drew little inlets of water where the hapless creatures might seek shelter. And so it is that we have havens.",
+				"e": "津하다 진핟다 thick, saturated"
+			},
+			{
+				"q": "攵",
+				"s": "복",
+				"m": "to tap, *taskmaster",
+				"d": "First find the long rod (the 1st stroke), held in the hand of someone seated (the next 3 strokes) -  taskmaster/taskmistress “never to forget.”",
+				"e": ""
+			},
+			{
+				"q": "牧",
+				"s": "목",
+				"m": "tend cattle, *breed",
+				"d": "When it is time to breed new cattle, the bull is usually willing but the cow is often not. Thus the taskmaster to the right forces the cow into a compromising position, so to speak, so that she and her mate can breed.",
+				"e": "牧師 목사 clergyman, pastor"
+			},
+			{
+				"q": "攻",
+				"s": "공",
+				"m": "attack, aggression",
+				"d": "The special craft of successful taskmasters is their ability to remain constantly on the aggressive, never allowing their underlings a moment to ponder a counter-aggression of their own.",
+				"e": "攻擊 공격 attack, assault"
+			},
+			{
+				"q": "失",
+				"s": "실",
+				"m": "failure",
+				"d": "The taskmaster is acknowledging the failure of a clam to make the grade in some marine school or other.",
+				"e": "失敗 실패 failure"
+			},
+			{
+				"q": "枚",
+				"s": "매",
+				"m": "a sheet of",
+				"d": "English counts thin, flat objects, like bed linen and paper, in sheets. The kanji does this with a taskmaster whipping a tree into producing sheets against its will.",
+				"e": "枚數 매수 number of leaves/sheets"
+			},
+			{
+				"q": "故",
+				"s": "고",
+				"m": "ancient, *happenstance",
+				"d": "Call it fate or providence or plain old Lady Luck, happenstance is the oldest taskmaster we know. It nearly always has its way.",
+				"e": "故鄕 고향 native place"
+			},
+			{
+				"q": "敬",
+				"s": "경",
+				"m": "respect, *awe",
+				"d": "Standing in awe of someone, you get self-conscious and may try to speak in µowery phrases out of veneration or fear. The taskmaster at the right is drilling you in the practice of your “honorifics.”",
+				"e": "尊敬 존경 respect, reverence"
+			},
+			{
+				"q": "言",
+				"s": "언",
+				"m": "words, say",
+				"d": "Of all the things we can do with our mouths, speech is the one that requires the greatest distinctness and clarity. Hence the kanji for say has four little sound-waves, indicating the complexity of the achievement.",
+				"e": "助言 조언 advice"
+			},
+			{
+				"q": "警",
+				"s": "경",
+				"m": "watch, alarm, *admonish",
+				"d": "The idea of being admonished for something already sets up a superior-inferior relationship between you and the person you are supposed to stand in awe of. While you are restricted to answering in honori³cs, the superior can use straightforward and ordinary words.",
+				"e": "警察 경찰 police"
+			},
+			{
+				"q": "計",
+				"s": "계",
+				"m": "plan, *plot",
+				"d": "Words and a meter’s needle combine to form the sense of plot: to talk over plans and to calculate a course of action.",
+				"e": "計算 계산 calculation"
+			},
+			{
+				"q": "獄",
+				"s": "옥",
+				"m": "prison",
+				"d": "The prison depicts a pack of wild dogs (the long-timers and hardened criminals) into which the poor little chihuahua (first-offender) has been cast. The only thing he has to protect himself against the pack are his shrill and frightened words.",
+				"e": "監獄 감옥 prison"
+			},
+			{
+				"q": "訂",
+				"s": "정",
+				"m": "arrange, *revise",
+				"d": "After completing the first draft, you revise it by nailing down your words and “hammering” them into shape.",
+				"e": "改訂 개정 revision"
+			},
+			{
+				"q": "討",
+				"s": "토",
+				"m": "discuss, *chastise",
+				"d": "Words spoken to chastise us stick to us like glue in a way no other words can.",
+				"e": "討論 토론 discussion, debate"
+			},
+			{
+				"q": "訓",
+				"s": "훈",
+				"m": "instruction",
+				"d": "The personalism connoted by the word instruction, as opposed to “teaching” or “discipline,” suits the picture here of words guiding one’s progress like the gentle flowing of a stream. Even the etymology of the word instruction suggests the sense of “pouring into”.",
+				"e": "敎訓 교훈 lesson, instruction"
+			},
+			{
+				"q": "詔",
+				"s": "조,소",
+				"m": "decree, proclaim, *imperial decree",
+				"d": "The imperial edict, spoken with the force of unquestionable law, is made up of words intended to seduce the masses—be it through fear or respect—to follow obediently.",
+				"e": ""
+			},
+			{
+				"q": "詰",
+				"s": "힐",
+				"m": "question, *packed",
+				"d": "A piece of writing that is pregnant with meaning and needs to be reread several times to be understood we refer to colloquially as “packed.” The character sees the words as sealed tightly inside an aerosol can.",
+				"e": "詰難 힐난 blame, reproach"
+			},
+			{
+				"q": "話",
+				"s": "화",
+				"m": "tale",
+				"d": "That the words of the tongue should come to mean a tale is clear from the etymology: a tale is something “talked,” not something read from a book.",
+				"e": "童話 동화 fairy tale"
+			},
+			{
+				"q": "詠",
+				"s": "영",
+				"m": "sing, chant, *recitation",
+				"d": "Listening to the words of poets reciting their poetry is like being transported for a moment into eternity where the rules of everyday life have been suspended.",
+				"e": ""
+			},
+			{
+				"q": "詩",
+				"s": "시",
+				"m": "poetry, poem",
+				"d": "Since silence is treasured so highly at a Buddhist temple the words spoken there must be well-chosen. Perhaps this is why the records of the monks often read to us like poems. Before going on, back up a frame and make sure you have kept poem and recitation distinct in your mind.",
+				"e": "詩 poem, 詩人 시인 poet"
+			},
+			{
+				"q": "語",
+				"s": "어",
+				"m": "language, *word",
+				"d": "This character stresses the fact that although it is <b>I</b> who say them, the words of a language are not my own. You can see the clear distinction between I and words just by looking at the kanji.",
+				"e": "國語 국어 national language"
+			},
+			{
+				"q": "読",
+				"s": "독",
+				"m": "read",
+				"d": "In the age of advertising, most words we read are out to <b>sell</b> some product or point of view.",
+				"e": ""
+			},
+			{
+				"q": "調",
+				"s": "조",
+				"m": "transfer, change, *tune",
+				"d": "A complete tune is composed not only of a succession of notes but also of one <b>lap</b> of the words that go with it.",
+				"e": "腔調 강조 stress, emphasis; 調節 조절 regulation, tuning"
+			},
+			{
+				"q": "談",
+				"s": "담",
+				"m": "talk, conversation, *discuss",
+				"d": "In almost every attempt to discuss an issue, the fervor of one’s convictions comes to the surface and creates an inflammation of words (if you will, the “cuss” in discuss). ",
+				"e": "相談 상담 consultation, counsel; 和談 화담 conversation, conference"
+			},
+			{
+				"q": "諾",
+				"s": "락",
+				"m": "consent",
+				"d": "The words of the young do not have legal validity unless backed up by “parental consent.”",
+				"e": "許諾 허락 consent, approval"
+			},
+			{
+				"q": "諭",
+				"s": "유",
+				"m": "proclaim, instruct, *rebuke",
+				"d": "The stern tone of a rebuke is seen here in the image of words spoken at a meeting of butchers waving their choppers at one another and “cutting one another down” as only butchers can.",
+				"e": ""
+			}
+		]
 	}
+
 ];
 /*
 	{
