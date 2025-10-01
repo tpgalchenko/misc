a00a6785a7debeda8e2897d92ac5b7e3d1a51796
Taras P. Galchenko <t.galchenko@e-soft.ru>
2025-10-01 16:09:41 +0300

    .

--- hanja-flash/l19.js
+++ hanja-flash/l19.js
@@ -1,243 +1,272 @@
+const L19 =
 {
-  "name" : "L19",
-  "cards" : [
-  	{
-  		"q": "咅",
-  		"s": "부,투",
-  		"m": "spit out, *muzzle",
-  		"d": "The element for muzzle shows a vase fixed over a mouth, perhaps with a rubber band running around the back of the head to keep it in place.",
-  		"e": ""
-  	},
-  	{
-  		"q": "賠",
-  		"s": "배",
-  		"m": "compensation",
-  		"d": "Picture a <b>clam</b> used as a <b>muzzle</b> to quiet the complaints of a fisherman’s widow asking <b>compensation</b> for her husband lost at sea.",
-  		"e": "賠償 배상 compensation, indemnity"
-  	},
-  	{
-  		"q": "培",
-  		"s": "배",
-  		"m": "bank up with dirt, *cultivate",
-  		"d": "The barrel hoops used by many Japanese farmers to stretch clear plastic over row of vegetables in a garden patch in the hopes of <b>cultivating</b> bigger and bigger vegetables is a way of <b>muzzling</b> the <b>soil</b>.",
-  		"e": "栽培 재배 cultivation, raising, growing"
-  	},
-  	{
-  		"q": "剖",
-  		"s": "부",
-  		"m": "split in two, dissect, *divide",
-  		"d": "To “divide and conquer” you use a saber and a muzzle.",
-  		"e": "剖檢 부검 autopsy, postmorted examination"
-  	},
-  	{
-  		"q": "音",
-  		"s": "음",
-  		"m": "sound",
-  		"d": "The kanji for sound depicts something standing in the air over a tongue wagging in a mouth, much the same as a sound does for the briefest of moments before disappearing.",
-  		"e": "音樂 음악 music"
-  	},
-  	{
-  		"q": "暗",
-  		"s": "암",
-  		"m": "dark, obscure; in secret, covert",
-  		"d": "When “darkness covered the earth” at the beginning of time, there was neither sun nor sound.",
-  		"e": "暗黑 암흑 darkness, blackness"
-  	},
-  	{
-  		"q": "韻",
-  		"s": "운",
-  		"m": "*rhyme, vowel",
-  		"d": "Poetry restricted to verses that <b>rhyme</b> often finds it has to abandon clarity of thought in order to make the <b>rhyme</b> of the words work. In this kanji’s picture, one becomes a kind of <b>“sound-employee.”</b>",
-  		"e": "韻文 운문 verse, poem, poetry"
-  	},
-  	{
-  		"q": "戠",
-  		"s": "치,직",
-  		"m": "potter's clay; *kazoo",
-  		"d": "It pictures the <b>sound</b> of a <b>fiesta</b>, namely a <b>kazoo</b>.",
-  		"e": ""
-  	},
-  	{
-  		"q": "識",
-  		"s": "식",
-  		"m": "recognize, know, *discriminate",
-  		"d": "A person of <b>discriminating</b> intellect can tell the difference between mere <b>kazoo</b>-buzzing and <b>words</b> spoken wisely.",
-  		"e": "知識 지식 knowledge, acquaintaince"
-  	},
-  	{
-  		"q": "竟",
-  		"s": "경",
-  		"m": "finally; *mirror",
-  		"d": "This kanji shows a pair of human legs and a tongue-wagging mouth looking at a mirror standing on the wall, asking perhaps who might be the fairest of them all.",
-  		"e": "畢竟 필경 after all, finally"
-  	},
-  	{
-  		"q": "鏡",
-  		"s": "경",
-  		"m": "mirror, lens, glass(es)",
-  		"d": "After lakes but before glass, polished <b>metal</b> was used for mirrors. These <b>metal mirrors</b> are recalled in this character for a <b>mirror</b>.",
-  		"e": "眼鏡 안경 glasses, spectacles"
-  	},
-  	{
-  		"q": "境",
-  		"s": "경",
-  		"m": "boundary",
-  		"d": "Imagine the <b>boundary</b> of a plot of <b>land</b> marked with gigantic <b>mirrors</b> enabling the landowner to keep trespassers in sight at all times.",
-  		"e": "地境 지경 boundary, border"
-  	},
-  	{
-  		"q": "亡",
-  		"s": "망",
-  		"m": "death, destroyed, perished",
-  		"d": "A <b>top hat</b> hanging on a hook in the front hall, right where the <b>deceased</b> left it the day he died, reminds us of him and his kanji.",
-  		"e": "死亡 사망 death, decease"
-  	},
-  	{
-  		"q": "盲",
-  		"s": "맹",
-  		"m": "blind",
-  		"d": "If one’s eyes <b>perish</b> before death, one remains <b>blind</b> for the rest of life.",
-  		"e": "盲人 맹인 blind person"
-  	},
-  	{
-  		"q": "妄",
-  		"s": "망",
-  		"m": "absurd, foolish; false; *delusion",
-  		"d": "The “ideal <b>woman</b>” one daydreams about is no more than a <b>delusion</b>. Hence, <b>perish</b> the thought of her.",
-  		"e": "妄言 망언 reckless remark, thoughtless words"
-  	},
-  	{
-  		"q": "荒",
-  		"s": "황",
-  		"m": "wasteland, *laid waste",
-  		"d": "The <b>flowers</b> that <b>perish</b> in the <b>flood</b> are taken here as symbols of an area that has been <b>laid waste</b>.",
-  		"e": "荒野 황야 wilderness, waste, wasteland"
-  	},
-  	{
-  		"q": "望",
-  		"s": "망",
-  		"m": "look forward, hope, *ambition",
-  		"d": "The story of ambition talks of a <b>king</b> walking under the perishing (or “waning”) <b>moon</b> dreaming great dreams about his kingdom. (The roots of ambition are from the same word as “ambulate,” meaning to walk about.)",
-  		"e": "希望 희망 hope, wish, aspiration"
-  	},
-  	{
-  		"q": "方",
-  		"s": "방",
-  		"m": "square, rectangle; *direction/compass",
-  		"d": "Spinning a <b>dagger</b> about on its hilt on the top of a <b>top hat</b> waiting to see in which direction it points when it comes to rest — one leaves to fate where one is going next.",
-  		"e": "方向 방향 direction"
-  	},
-  	{
-  		"q": "妨",
-  		"s": "방",
-  		"m": "interfere with, impede, *disturb",
-  		"d": "Imagine a <b>compass</b> that is disturbed every time a <b>woman</b> passes by, sending the needle spinning madly round and round.",
-  		"e": "妨害 방해 disturbance, obstruction, interference"
-  	},
-  	{
-  		"q": "坊",
-  		"s": "방",
-  		"m": "neighborhood; *boy",
-  		"d": "The character for a <b>boy</b> shows us a <b>Boy</b> Scout cleaning the <b>dirt</b> out of his <b>compass</b> — the more <b>dirt</b>, the better.",
-  		"e": "坊坊曲曲 방방곡곡 everywhere"
-  	},
-  	{
-  		"q": "芳",
-  		"s": "방",
-  		"m": "fragrant, *perfumed",
-  		"d": "Here we see a special <b>compass</b> used to pick out those <b>flowers</b> most suited for making good <b>perfumes</b>.",
-  		"e": "芳年 방년 sweet age"
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  	{
-  		"q": "",
-  		"s": "",
-  		"m": "",
-  		"d": "",
-  		"e": ""
-  	},
-  ]
-}
+	"name": "L19",
+	"cards": [
+		{
+			"q": "咅",
+			"s": "부,투",
+			"m": "spit out, *muzzle",
+			"d": "The element for muzzle shows a vase fixed over a mouth, perhaps with a rubber band running around the back of the head to keep it in place.",
+			"e": ""
+		},
+		{
+			"q": "賠",
+			"s": "배",
+			"m": "compensation",
+			"d": "Picture a <b>clam</b> used as a <b>muzzle</b> to quiet the complaints of a fisherman’s widow asking <b>compensation</b> for her husband lost at sea.",
+			"e": "賠償 배상 compensation, indemnity"
+		},
+		{
+			"q": "培",
+			"s": "배",
+			"m": "bank up with dirt, *cultivate",
+			"d": "The barrel hoops used by many Japanese farmers to stretch clear plastic over row of vegetables in a garden patch in the hopes of <b>cultivating</b> bigger and bigger vegetables is a way of <b>muzzling</b> the <b>soil</b>.",
+			"e": "栽培 재배 cultivation, raising, growing"
+		},
+		{
+			"q": "剖",
+			"s": "부",
+			"m": "split in two, dissect, *divide",
+			"d": "To “divide and conquer” you use a saber and a muzzle.",
+			"e": "剖檢 부검 autopsy, postmorted examination"
+		},
+		{
+			"q": "音",
+			"s": "음",
+			"m": "sound",
+			"d": "The kanji for sound depicts something standing in the air over a tongue wagging in a mouth, much the same as a sound does for the briefest of moments before disappearing.",
+			"e": "音樂 음악 music"
+		},
+		{
+			"q": "暗",
+			"s": "암",
+			"m": "dark, obscure; in secret, covert",
+			"d": "When “darkness covered the earth” at the beginning of time, there was neither sun nor sound.",
+			"e": "暗黑 암흑 darkness, blackness"
+		},
+		{
+			"q": "韻",
+			"s": "운",
+			"m": "*rhyme, vowel",
+			"d": "Poetry restricted to verses that <b>rhyme</b> often finds it has to abandon clarity of thought in order to make the <b>rhyme</b> of the words work. In this kanji’s picture, one becomes a kind of <b>“sound-employee.”</b>",
+			"e": "韻文 운문 verse, poem, poetry"
+		},
+		{
+			"q": "戠",
+			"s": "치,직",
+			"m": "potter's clay; *kazoo",
+			"d": "It pictures the <b>sound</b> of a <b>fiesta</b>, namely a <b>kazoo</b>.",
+			"e": ""
+		},
+		{
+			"q": "識",
+			"s": "식",
+			"m": "recognize, know, *discriminate",
+			"d": "A person of <b>discriminating</b> intellect can tell the difference between mere <b>kazoo</b>-buzzing and <b>words</b> spoken wisely.",
+			"e": "知識 지식 knowledge, acquaintaince"
+		},
+		{
+			"q": "竟",
+			"s": "경",
+			"m": "finally; *mirror",
+			"d": "This kanji shows a pair of human legs and a tongue-wagging mouth looking at a mirror standing on the wall, asking perhaps who might be the fairest of them all.",
+			"e": "畢竟 필경 after all, finally"
+		},
+		{
+			"q": "鏡",
+			"s": "경",
+			"m": "mirror, lens, glass(es)",
+			"d": "After lakes but before glass, polished <b>metal</b> was used for mirrors. These <b>metal mirrors</b> are recalled in this character for a <b>mirror</b>.",
+			"e": "眼鏡 안경 glasses, spectacles"
+		},
+		{
+			"q": "境",
+			"s": "경",
+			"m": "boundary",
+			"d": "Imagine the <b>boundary</b> of a plot of <b>land</b> marked with gigantic <b>mirrors</b> enabling the landowner to keep trespassers in sight at all times.",
+			"e": "地境 지경 boundary, border"
+		},
+		{
+			"q": "亡",
+			"s": "망",
+			"m": "death, destroyed, perished",
+			"d": "A <b>top hat</b> hanging on a hook in the front hall, right where the <b>deceased</b> left it the day he died, reminds us of him and his kanji.",
+			"e": "死亡 사망 death, decease"
+		},
+		{
+			"q": "盲",
+			"s": "맹",
+			"m": "blind",
+			"d": "If one’s eyes <b>perish</b> before death, one remains <b>blind</b> for the rest of life.",
+			"e": "盲人 맹인 blind person"
+		},
+		{
+			"q": "妄",
+			"s": "망",
+			"m": "absurd, foolish; false; *delusion",
+			"d": "The “ideal <b>woman</b>” one daydreams about is no more than a <b>delusion</b>. Hence, <b>perish</b> the thought of her.",
+			"e": "妄言 망언 reckless remark, thoughtless words"
+		},
+		{
+			"q": "荒",
+			"s": "황",
+			"m": "wasteland, *laid waste",
+			"d": "The <b>flowers</b> that <b>perish</b> in the <b>flood</b> are taken here as symbols of an area that has been <b>laid waste</b>.",
+			"e": "荒野 황야 wilderness, waste, wasteland"
+		},
+		{
+			"q": "望",
+			"s": "망",
+			"m": "look forward, hope, *ambition",
+			"d": "The story of ambition talks of a <b>king</b> walking under the perishing (or “waning”) <b>moon</b> dreaming great dreams about his kingdom. (The roots of ambition are from the same word as “ambulate,” meaning to walk about.)",
+			"e": "希望 희망 hope, wish, aspiration"
+		},
+		{
+			"q": "方",
+			"s": "방",
+			"m": "square, rectangle; *direction/compass",
+			"d": "Spinning a <b>dagger</b> about on its hilt on the top of a <b>top hat</b> waiting to see in which direction it points when it comes to rest — one leaves to fate where one is going next.",
+			"e": "方向 방향 direction"
+		},
+		{
+			"q": "妨",
+			"s": "방",
+			"m": "interfere with, impede, *disturb",
+			"d": "Imagine a <b>compass</b> that is disturbed every time a <b>woman</b> passes by, sending the needle spinning madly round and round.",
+			"e": "妨害 방해 disturbance, obstruction, interference"
+		},
+		{
+			"q": "坊",
+			"s": "방",
+			"m": "neighborhood; *boy",
+			"d": "The character for a <b>boy</b> shows us a <b>Boy</b> Scout cleaning the <b>dirt</b> out of his <b>compass</b> — the more <b>dirt</b>, the better.",
+			"e": "坊坊曲曲 방방곡곡 everywhere"
+		},
+		{
+			"q": "芳",
+			"s": "방",
+			"m": "fragrant, *perfumed",
+			"d": "Here we see a special <b>compass</b> used to pick out those <b>flowers</b> most suited for making good <b>perfumes</b>.",
+			"e": "芳年 방년 sweet age"
+		},
+		{
+			"q": "肪",
+			"s": "방",
+			"m": "fat, *obese",
+			"d": "One who eats too much soon needs a <b>compass</b> to find one’s way around the obese mass of <b>flesh</b> that accumulates in the midsection.",
+			"e": "脂肪 지방 fat, grease"
+		},
+		{
+			"q": "訪",
+			"s": "방",
+			"m": "visit, *call on; ask",
+			"d": "When making a courtesy <b>call on</b> a dignitary, one has to gauge one’s words with great care. Hence the need for a <b>compass</b>.",
+			"e": "訪問 방문 call, visit, interview"
+		},
+		{
+			"q": "放",
+			"s": "방",
+			"m": "put, release, *free",
+			"d": "The <b>taskmaster</b> sets an unruly servant <b>free</b>, giving him no more than a quick glance at the compass and a boot from behind.",
+			"e": "開放 개방 opening"
+		},
+		{
+			"q": "激",
+			"s": "격",
+			"m": "arouse, excite, *violent",
+			"d": "Some cosmic <b>taskmaster</b> hovering overhead whips up the waves to make them dash <b>violently</b> against the shore. In the <b>white</b> foam that covers the <b>water</b> we see a broken <b>compass</b> floating, all that remains of a shipwreck.",
+			"e": "激하다 격하다 (get) excited, (be) enraged, become violent"
+		},
+		{
+			"q": "兑/兌",
+			"s": "태",
+			"m": "cash, exchange, *devil",
+			"d": "The two horns on the head of the <b>teenager</b> are enough to suggest to most parents of adolescents a good image of a <b>devil</b>.",
+			"e": "兌換 태환 conversion"
+		},
+		{
+			"q": "脱/脫",
+			"s": "탈",
+			"m": "take off, *undress",
+			"d": "To <b>undress</b> is to expose the <b>flesh</b> and tempt the <b>devil</b> in the eyes of one’s onlookers. Ignore the moral if you want, but not the <b>devil</b>.",
+			"e": "脫色 탈색 decoloration, decolorization"
+		},
+		{
+			"q": "説/說",
+			"s": "설",
+			"m": "speak, *rumor",
+			"d": "Not inappropriately, this character likens a <b>rumor</b> to the <b>devil</b>’s own <b>words</b>.",
+			"e": "說話 설화 tale, story, fable; 說得 설득 persuasion"
+		},
+		{
+			"q": "鋭/銳",
+			"s": "예",
+			"m": "sharp, *pointed",
+			"d": "<b>Metal</b> that has been <b>pointed</b> (as an awl, a pick, a nail, or a knife) tends to serve the <b>devil</b>’s purposes as well as civilization’s: our tools are also our weapons.",
+			"e": "銳敏하다 예민하다 sharp, keen, acute, fine"
+		},
+		{
+			"q": "曽/曾",
+			"s": "증",
+			"m": "already, *formerly",
+			"d": "This primitive is composed of a pair of <b>horns</b> growing out of a <b>brain</b> with a <b>tongue wagging</b> in the <b>mouth</b> beneath. Think of “former” in connection with administrators or heads of state who have just left office but continue to make a nuisance of themselves by advertising their opinions on public policy.",
+			"e": "曾孫 증손 great-grandchild;  曾祖 증조 great-grandfather"
+		},
+		{
+			"q": "増/增",
+			"s": "증",
+			"m": "increase",
+			"d": "This kanji depicts an <b>increase</b> of <b>soil</b>, multiplying so fast that it literally buries everything in its path.",
+			"e": "增加 증가 increase"
+		},
+		{
+			"q": "贈",
+			"s": "증",
+			"m": "give present",
+			"d": "The <b>presents</b> offered here are <b>money</b> that <b>increases</b> each time you give it away.",
+			"e": "寄贈 기증 contribution, donation, presentation"
+		},
+		{
+			"q": "東",
+			"s": "동",
+			"m": "east",
+			"d": "As a “Western” language, English identifies the <b>east</b> with the rising <b>sun</b>. In more fanciful terms, we see the sun <b>piercing</b> through a <b>tree</b> as it rises in the <b>east</b>.",
+			"e": "東洋 동양 the Orient, the East"
+		},
+		{
+			"q": "棟",
+			"s": "동",
+			"m": "house beam, *ridgepole",
+			"d": "If the piece of wood in the roof known as the <b>ridgepole</b> points <b>east</b>, the sunrise will be visible from the front door.",
+			"e": "別棟 별동 a house/building separate/detached from the main building; an outbuilding"
+		},
+		{
+			"q": "凍",
+			"s": "동",
+			"m": "frozen",
+			"d": "The whole secret to breaking the <b>ice</b> with the <b>East</b> is to peek behind those mysteriously “<b>frozen</b> smiles.”",
+			"e": "冷凍 냉동 freezing, refrigeration"
+		},
+		{
+			"q": "壬",
+			"s": "임",
+			"m": "9th heavenly stem; *porter",
+			"d": "Let the extended dot at the top represent the load that the <b>samurai</b> is carrying in his role as the master’s <b>porter</b>.",
+			"e": ""
+		},
+		{
+			"q": "妊",
+			"s": "임",
+			"m": "pregnancy",
+			"d": "A <b>woman</b> who is in her <b>pregnancy</b> is a bit like a <b>porter</b>, bearing her new companion wherever she goes.",
+			"e": "妊娠 임신 pregrancy"
+		},
+		{
+			"q": "廷",
+			"s": "廷",
+			"m": "court",
+			"d": "Those who rule the <b>courts</b>, the <b>porters</b> of justice and order, are often found to <b>stretch</b> the law to suit their own purposes.",
+			"e": "法廷 법정 (law) court, judgement hall"
+		}
+	]
+};
