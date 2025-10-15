e5f81bc726543893994f0d416b6b77b886cf62ba
Taras P. Galchenko <t.galchenko@e-soft.ru>
2025-10-15 20:21:33 +0300

    L20

---
+++ hanja-flash/l20.js
@@ -0,0 +1,48 @@
+const L20 =
+{
+	"name": "L20",
+	"cards": [
+		{
+			"q": "染",
+			"s": "염",
+			"m": "dye",
+			"d": "Tom Sawyer tricked Huckleyberry Finn to whitewash a house after receiving <b>nine</b> dead cats hung on a <b>tree</b>.",
+			"e": "染色 염색 dyeing"
+		},
+		{
+			"q": "燃",
+			"s": "연",
+			"m": "burn",
+			"d": "Burning is placing wood, coal and all <b>sorts of things</b> to <b>fire</b>.",
+			"e": "燃料 연료 fuel"
+		},
+		{
+			"q": "賓",
+			"s": "빈",
+			"m": "guest, visitor; VIP",
+			"d": "To receive important <b>guests</b> the house is decorated with <b>few</b> <b>shells</b> hung under the <b>ceiling</b> so that the guests bump into the shells with their heads.",
+			"e": "貴賓 귀빈 an honored/important guet"
+		},
+		{
+			"q": "歳/歲",
+			"s": "세",
+			"m": "year (of age)",
+			"d": "Time does not <b>stop</b> its <b>march</b> but a <b>little</b> for the New Year holidays.",
+			"e": "歲拜 세배 New Year's bow; 歲月 세월 time, times, conditions"
+		},
+		{
+			"q": "県/縣",
+			"s": "현",
+			"m": "county, district, *prefecture",
+			"d": "In every <b>prefecture</b> of Japan there are poor fishermen who possess as <b>little</b> as mere <b>fishhooks</b>.",
+			"e": ""
+		},
+		{
+			"q": "栃",
+			"s": "회",
+			"m": "oak, *horse chestnut",
+			"d": "A lonely <b>oak tree</b> standing on a cliff bring <b>ten thousand</b> acorns every year in hope to grow up a company for itself.",
+			"e": ""
+		}
+	]
+};
