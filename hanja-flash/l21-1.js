b4426737438adc340da0ff094d7835483f4d2203
Taras P. Galchenko <t.galchenko@e-soft.ru>
2025-10-24 17:14:37 +0300

    .

---
+++ hanja-flash/l21-1.js
@@ -0,0 +1,265 @@
+const L21_1 =
+{
+	"name": "L21-1",
+	"cards": [
+		{
+			"q": "也",
+			"s": "야",
+			"m": "also, *scorpion",
+			"d": "This primitive is a pictograph of the <b>scorpion</b>, the first 2 strokes representing its head and pincers, the last stroke its barbed tail, in which you may recognize the <b>fishhook</b>.",
+			"e": "及其也 급기야 at last, finally, in the end"
+		},
+		{
+			"q": "地",
+			"s": "지",
+			"m": "soil, *ground",
+			"d": "<b>Scropion</b> is an <b>earth</b> animal.",
+			"e": "地域 지역 area, region"
+		},
+		{
+			"q": "池",
+			"s": "지",
+			"m": "pool, *pond",
+			"d": "A <b>scorpion</b> letting its venom out <b>drop by drop</b> until it has made a whole <b>pond</b> of the stuff.",
+			"e": "貯水池 저수지 water resevoir"
+		},
+		{
+			"q": "虫",
+			"s": "훼,충",
+			"m": "insect",
+			"d": "A larva with big head and a poisonous tail.",
+			"e": ""
+		},
+		{
+			"q": "蛍/螢",
+			"s": "형",
+			"m": "glow-worm, lightning bug",
+			"d": "A <b>glow-worm</b> who shines so bright to set a <b>house</b> on <b>fire</b>.",
+			"e": "螢光 형광 fluorescence"
+		},
+		{
+			"q": "蛇",
+			"s": "사",
+			"m": "snake",
+			"d": "A <b>snake</b> is a <b>larva</b> grew up so as to leave its cocoon-<b>house</b> drilling a way out with a <b>spoon</b>.",
+			"e": "蛇足 사족 'snake's legs', redundant, superfluent"
+		},
+		{
+			"q": "虹",
+			"s": "홍",
+			"m": "rainbow",
+			"d": "A <b>rainbow</b> is not the light refracted by water drops but by the wings of <b>insects</b>, it's their <b>craft</b>, their job.",
+			"e": "虹彩 홍채 iris"
+		},
+		{
+			"q": "蝶",
+			"s": "접",
+			"m": "butterfly",
+			"d": "<b>Butterflies</b> must be the greatest pest to the <b>trees</b> since they eat up all <b>generations</b> of <b>tree</b> leaves.",
+			"e": "蝶泳 접영 butterfly stroke (swimming technique)"
+		},
+		{
+			"q": "独/獨",
+			"s": "독",
+			"m": "alone, single",
+			"d": "A <b>dog</b> is so <b>lonely</b>, that the only joy it has is to collect <b>insects</b> in a <b>bag</b> and to stare at them (<b>eye</b>).",
+			"e": "獨立 독립 independence, self-reliance"
+		},
+		{
+			"q": "蚕",
+			"s": "천,잠",
+			"m": "*silkworm",
+			"d": "Heavens are no more than a blue silk carpet created by silkworms.",
+			"e": ""
+		},
+		{
+			"q": "風",
+			"s": "풍",
+			"m": "wind",
+			"d": "Only <b>wind</b> can save us from the swarms of gnats, those most <b>ordinary 凡</b> of <b>insects</b>.",
+			"e": "颱風 태풍 typhoon"
+		},
+		{
+			"q": "己",
+			"s": "기",
+			"m": "self, *snake",
+			"d": "The deep-down inner structure of the human person that mythology has often depicted is a snake, which is what the kanji shows pictographically.",
+			"e": "自己 자기 self, oneself; 利己的 이기적 egoistic"
+		},
+		{
+			"q": "起",
+			"s": "기",
+			"m": "rise, stand up; begin",
+			"d": "If you wake up and see a <b>snake</b>, <b>stand up</b> and <b>run</b>!",
+			"e": "起立 기립 rising, standing up"
+		},
+		{
+			"q": "妃",
+			"s": "비",
+			"m": "wife; *queen",
+			"d": "Cleopatre wanted to be Cesar's queen, but ended up with a suicide employing venomous snakes.",
+			"e": "王妃 왕비 queen"
+		},
+		{
+			"q": "改",
+			"s": "개",
+			"m": "improve, *reform",
+			"d": "If you want to improve the world, start with becoming a good <b>taskmaster</b> for your<b>self</b>.",
+			"e": "改善 개선 improvement, reform"
+		},
+		{
+			"q": "記",
+			"s": "기",
+			"m": "record, remember",
+			"d": "If one wants to remember some words, he has better write them down in a record.",
+			"e": "記錄 기록 record, archives"
+		},
+		{
+			"q": "包",
+			"s": "포",
+			"m": "wrap, bundle, pack",
+			"d": "A bundle of snakes.",
+			"e": "小包 소포 package, parcel; 包裝 포장 packing, packaging"
+		},
+		{
+			"q": "胞",
+			"s": "포",
+			"m": "womb, *placenta",
+			"d": "Womb is the part of the body that wraps up child body.",
+			"e": "細胞 세포 cell; 同胞 동포 fellow countrymen, compatriot"
+		},
+		{
+			"q": "砲",
+			"s": "포",
+			"m": "*cannon",
+			"d": "A cannon is a metal wrapping for the cannonballs made of stones although a temporary one, just for few seconds.",
+			"e": "砲丸 포환 cannonball; 大砲 대포 gun, cannon"
+		},
+		{
+			"q": "泡",
+			"s": "포",
+			"m": "bubble",
+			"d": "Bubbles wrap up a fast stream of water.",
+			"e": "水泡 수포 bubbles, foam"
+		},
+		{
+			"q": "亀/龜",
+			"s": "귀, 균",
+			"m": "*tortoise; burst",
+			"d": "Side view of a tortoise.",
+			"e": "龜鑑 귀감 model, example"
+		},
+		{
+			"q": "电/電",
+			"s": "전",
+			"m": "lightning, *electricity",
+			"d": "A lightning spreading in the rain (like an eel 电).",
+			"e": ""
+		},
+		{
+			"q": "竜/龍",
+			"s": "룡,용",
+			"m": "dragon",
+			"d": "Snake with a comb and a body of three segments who comes out of a magical vase under full moon.",
+			"e": "恐龍 공룡 dinosaur"
+		},
+		{
+			"q": "滝/瀧",
+			"s": "롱,농",
+			"m": "falling water, wet",
+			"d": "Water falling off a dragon",
+			"e": ""
+		},
+		{
+			"q": "豕",
+			"s": "시",
+			"m": "pig, boar, *sow",
+			"d": "Easier than pulling it apart into smaller elements is remembering its shape as a highly stylized pictograph.",
+			"e": ""
+		},
+		{
+			"q": "豚",
+			"s": "돈",
+			"m": "*pork, small pig",
+			"d": "A flesh of sow.",
+			"e": ""
+		},
+		{
+			"q": "逐",
+			"s": "축",
+			"m": "chase, expel, *pursue",
+			"d": "Pigs hit the road.",
+			"e": "逐出 축출 driving out, expulsion"
+		},
+		{
+			"q": "遂",
+			"s": "수",
+			"m": "finally; *consummate",
+			"d": "Finally the pig has got away while parting the grass (八).",
+			"e": "遂行 수행 accomplishment, execution"
+		},
+		{
+			"q": "家",
+			"s": "가",
+			"m": "house",
+			"d": "This kanji recalls the times when the “domestic” animals were, as the word itself suggests, really kept in the house.",
+			"e": "家族 가족 family, household, kinsfolk"
+		},
+		{
+			"q": "嫁",
+			"s": "가",
+			"m": "marry into, give a daughter in marriage",
+			"d": "The kanji in this frame demonstrates the traditional Japanese approach to marriage: it is the woman who leaves her family for another household, thus marrying into a man’s family.",
+			"e": "出嫁 출가 a wonans's being married and leaving home"
+		},
+		{
+			"q": "豪",
+			"s": "호",
+			"m": "hero, *overpowering",
+			"d": "One of the Hercules' tall exploits is capturing the Erymanthian boar. It was a reason to crown him and to call hero.",
+			"e": "豪傑 호걸 hero, great man"
+		},
+		{
+			"q": "勿",
+			"s": "물,몰",
+			"m": "must not, do not, never",
+			"d": "A sword in a sheath is enough to deter from hazardous actions.",
+			"e": "勿論 물론 of course, undoubtedly"
+		},
+		{
+			"q": "-+勿",
+			"s": "",
+			"m": "piglets",
+			"d": "This abbreviation of the full primitive for a sow, quite naturally, means piglets.",
+			"e": ""
+		},
+		{
+			"q": "昜",
+			"s": "양",
+			"m": "open; sunlight; *piggy bank",
+			"d": "Just remember that each day you put a few pennies into the back of the little piglet on your bureau that you call a <b>piggy bank</b>.",
+			"e": ""
+		},
+		{
+			"q": "腸",
+			"s": "장",
+			"m": "intestines",
+			"d": "A piggy bank is has no intestines, for its flesh has been removed to give place for our savings.",
+			"e": "胃腸 위장 stomach and intestines"
+		},
+		{
+			"q": "場",
+			"s": "장",
+			"m": "location",
+			"d": "Location indicates the mound of soil that hides a piggy bank full of coins.",
+			"e": "場所 장소 place, spot"
+		},
+		{
+			"q": "湯",
+			"s": "탕",
+			"m": "hot water, soup, broth",
+			"d": "The savings of a piggy bank is only enough to get a soup.",
+			"e": "溫湯 온탕 hot water of the SPA"
+		}
+	]
+};
---
+++ hanja-flash/l21-2.js
@@ -0,0 +1,251 @@
+const L21_2 =
+{
+	"name": "L21-2",
+	"cards": [
+		{
+			"q": "羊",
+			"s": "양",
+			"m": "sheep",
+			"d": "This pictograph shows the animal horns at the top attached to the head (3rd stroke), the front and back legs (strokes 4 and 5) and body (final stroke).",
+			"e": "羊毛 양모 wool"
+		},
+		{
+			"q": "美",
+			"s": "미",
+			"m": "beauty",
+			"d": "Try to think of what the Chinese were on to when they associated the idea of beauty with a large sheep.",
+			"e": "美術 미술 art, fine arts"
+		},
+		{
+			"q": "洋",
+			"s": "양",
+			"m": "ocean; overseas, foreign, western",
+			"d": "Ocean is a herd of seas, just like a herd of sheeps.",
+			"e": "海洋 해양 ocean; 西洋 서양 the West, Europe and America"
+		},
+		{
+			"q": "詳",
+			"s": "상",
+			"m": "detailed",
+			"d": "The sheeps were ofter sacrificed to gods, that is they became a kind of words substitute in the message to gods. He who make sacrifice must be <b>detailed</b> in his message to that the god understood him correctly.",
+			"e": "昭詳하다 소상하다 full, detailed, minute"
+		},
+		{
+			"q": "鮮",
+			"s": "선",
+			"m": "fresh",
+			"d": "A fresh embryon of sheep looks just like a fish embryon - the sign that life come from the ocean.",
+			"e": "生鮮 생선 fresh/raw fish; 朝鮮 조선 Korea"
+		},
+		{
+			"q": "達",
+			"s": "달",
+			"m": "arrive at, reach; *accomplished",
+			"d": "While going to their native sheepfold a herd of sheeps finds a mould of soil that blocks on the road and hinders to reach the goal. The sheeps start stubbornly to run their heads into the mould just to break throught and arrive at home.",
+			"e": "配達 배달 delivery; 達成 달성 accomplishment"
+		},
+		{
+			"q": "羨",
+			"s": "선",
+			"m": "envy",
+			"d": "A sheep lacking water envies everyone who drinks.",
+			"e": "羨望 선망 envy"
+		},
+		{
+			"q": "羊 + /",
+			"s": "",
+			"m": "*wool",
+			"d": "This rather uncommon primitive is made by pulling the tail of the sheep to one side to create a semienclosure. The meaning of wool is derived from the fact that the shearer is holding the sheep by the tail in order to trim its wool.",
+			"e": ""
+		},
+		{
+			"q": "差",
+			"s": "차",
+			"m": "differ, distinction",
+			"d": "The sheep shearer must know well his craft and notice the distinction of various types of wool.",
+			"e": "差異 차이 difference"
+		},
+		{
+			"q": "着",
+			"s": "착",
+			"m": "make move, take action; *to don",
+			"d": "Donning (putting on) one’s clothes is “pulling the wool over one’s eyes.",
+			"e": "到着 도착; 着用 착용 put on, wear"
+		},
+		{
+			"q": "隹",
+			"s": "추,최,유",
+			"m": "bird, *turkey",
+			"d": "An old turkey, complete with pipe and monocle. Let the first four strokes stand for the turkey’s head, neck, and drooping chin. The remainder can then be pictographic of the plumage.",
+			"e": ""
+		},
+		{
+			"q": "唯",
+			"s": "유",
+			"m": "only, solely",
+			"d": "The only use of turkey is to fill our mouths at the Thanksgiving day.",
+			"e": "唯一하다 유일하다 sole, only, unique"
+		},
+		{
+			"q": "焦",
+			"s": "초",
+			"m": "burned, charry; anxious",
+			"d": "Impatient like a burned turkey.",
+			"e": "焦點 초점 focus; 焦燥 초조 fretfulness, impatience, irritation"
+		},
+		{
+			"q": "礁",
+			"s": "초",
+			"m": "reef",
+			"d": "A reef is a rock that got scorched after a ship wrecked and was burned down by fire.",
+			"e": "暗礁 암초 submerged rocks, unknown reef"
+		},
+		{
+			"q": "集",
+			"s": "집",
+			"m": "assemble, gather",
+			"d": "A flock of birds gathers on the top of a tree.",
+			"e": "集團 집단 group, mass"
+		},
+		{
+			"q": "准",
+			"s": "준",
+			"m": "approve, ratify; *quasi-",
+			"d": "A frozen turkey in ice is not yet a ready meal for the Thanksgiving day, it still has to be approved by the chief cook.",
+			"e": "批准 비준 ratification"
+		},
+		{
+			"q": "進",
+			"s": "진",
+			"m": "advance, progress",
+			"d": "Birds can only fly forward, so they are always advancing on their way.",
+			"e": "先進 선진 advanced, superior"
+		},
+		{
+			"q": "雑/雜",
+			"s": "잡",
+			"m": "mixed, *miscellaneous",
+			"d": "Nine birds gathered on top of a tree to make a mess with their cries because of a top hot.",
+			"e": "複雜 복잡 complexity, complication, intricacy"
+		},
+		{
+			"q": "雌",
+			"s": "자",
+			"m": "feminine",
+			"d": "As soon as woman stops and sits down a flock of birds fly up looking for her kindness.",
+			"e": " 雌雄 자웅 male and female, both sexes"
+		},
+		{
+			"q": "準",
+			"s": "준,절",
+			"m": "rule, guideline, standard; *semi-",
+			"d": "Water paths serve as guidelines and crossroads for birds.",
+			"e": "水準 수준 water level; standard"
+		},
+		{
+			"q": "奪",
+			"s": "탈",
+			"m": "rob, take by force",
+			"d": "In order to save the turkeys from robbery the guardian St. Bernard dog simply glued the birds to their perches.",
+			"e": "強奪 강탈 extortion, robbery"
+		},
+		{
+			"q": "奮",
+			"s": "분",
+			"m": "strive; arouse",
+			"d": "St. Bernard dog exerts to guard the turkeys that graze on a rice field.",
+			"e": "興奮 흥분 excitement, agitation"
+		},
+		{
+			"q": "確",
+			"s": "확",
+			"m": "sure, certain",
+			"d": "Turkey's coop is made of rock, so it is assured against bad weather and all other unexpected troubles.",
+			"e": "確認 확인 confirmation, validation"
+		},
+		{
+			"q": "午",
+			"s": "오",
+			"m": "noon",
+			"d": "With a bit of stretching, you might see a horse’s head pointing leftwards in this character. That gives the primary meaning of the Chinese zodiacal sign of the horse, which corresponds to the hour of noon. Note how this kanji primitive differs from that for cow.",
+			"e": "午後 오후 afternoon"
+		},
+		{
+			"q": "許",
+			"s": "허",
+			"m": "permit, allow",
+			"d": "Yahoo horses can speak, so a rider must ask for permission if he wants to have a ride.",
+			"e": "許諾 허락 consent, approval"
+		},
+		{
+			"q": " 𠂉+一+隹",
+			"s": "",
+			"m": "Pegasus",
+			"d": "By combining the horse (giving a twist to its final stroke a bit to the left to keep the strokes from overlapping) with the turkey, we get a flying horse or Pegasus. Be sure not to confuse with the turkey house.",
+			"e": ""
+		},
+		{
+			"q": "雚",
+			"s": "관,환",
+			"m": "stork",
+			"d": "The stork Leonid - a bird with large eyes and thick eyebrows.",
+			"e": ""
+		},
+		{
+			"q": "歓/歡",
+			"s": "환",
+			"m": "pleased, glad, *delight",
+			"d": "Flying horses that yawn. A satiated stork yawns from happiness and delight.",
+			"e": "歡心 환심 good graces, favor"
+		},
+		{
+			"q": "権/權",
+			"s": "권,관",
+			"m": "authority",
+			"d": "Pegasus/stork sits on a tree like a king on his throne and imposes his authority on to the subjects.",
+			"e": "權勢 권세 power, authority"
+		},
+		{
+			"q": "観/觀",
+			"s": "관",
+			"m": "see, view; *outlook",
+			"d": "Flying horses and storks love to watch Olympic games but each time they arrive there they become the object of overall attention due to their extravagant outlook.",
+			"e": "觀客 관객 spectator, audience; 外觀 외관 external appearance"
+		},
+		{
+			"q": "羽",
+			"s": "우,호",
+			"m": "feather; *wings",
+			"d": "From the pictograph of two bird-wings, we get feathers.",
+			"e": ""
+		},
+		{
+			"q": "習",
+			"s": "습",
+			"m": "practice, *learn",
+			"d": "A bird flying high above white clouds practices flapping its wings.",
+			"e": "復習 복습 review, revision; 學習 학습 learning, study"
+		},
+		{
+			"q": "翌",
+			"s": "익",
+			"m": "bright, daybreak; *the next (day)",
+			"d": "What will you do the next day as the sun comes up? Piss into a shiny bronze vase with wings.",
+			"e": "翌日 익일 the next day"
+		},
+		{
+			"q": "曜",
+			"s": "요",
+			"m": "daylight; *weekday",
+			"d": "As sun rises up a cock cries and flaps its wings : a new day has come.",
+			"e": "曜日 요일 a day of the week"
+		},
+		{
+			"q": "濯",
+			"s": "탁",
+			"m": "laundry",
+			"d": "A bird washes itself by spraying water on itself and then flaps its wings to shake off the water and the dirt. A kind of bird laundry for bird clothes - feathers.",
+			"e": "洗濯 세탁 washing, laundry"
+		}
+	]
+};
