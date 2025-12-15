ff98cd8a421c29c5736a3aa51eaed156ac6cd344
Taras P. Galchenko <t.galchenko@e-soft.ru>
2025-12-15 18:49:20 +0300

    .

--- hanja-flash/l23-1.js
+++ hanja-flash/l23-1.js
@@ -821,20 +821,6 @@ const L23 = {
       "e": "英雄 영웅 hero, great man"
     },
     {
-      "q": "𠫔",
-      "s": "굉",
-      "m": "forearm",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "至",
-      "s": "지,질",
-      "m": "reach, arrive; extremely, very",
-      "d": "It is hard to reach the ground with fingertips standing upright and bending downwards. And it is extremely hard to reach it with elbows or forearms (𠫔).",
-      "e": "至高 지고 sublimity, supremacy"
-    },
-    {
       "q": "台/臺",
       "s": "태/대",
       "m": "platform, *pedestal",
@@ -842,39 +828,172 @@ const L23 = {
       "e": "舞臺 무대 stage"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
+      "q": "怠",
+      "s": "태",
+      "m": "idle, remiss. *neglect",
+      "d": "Putting someone on a pedestal and being negligent towards oneself is a direct way to the broken heart.",
+      "e": "懶怠 나태 idleness, laziness"
+    },
+    {
+      "q": "治",
+      "s": "치",
+      "m": "reign, govern, administrate",
+      "d": "The statues of the soviet rulers on high pedestals were everywhere even besides the rivers and canals.",
+      "e": "政治 정치 politics"
+    },
+    {
+      "q": "始",
+      "s": "시",
+      "m": "commence",
+      "d": "Pygmalion started with a simple woman statue on a pedetal, but how did it end?",
+      "e": "始作 시작 beginning, start"
+    },
+    {
+      "q": "胎",
+      "s": "태",
+      "m": "unborn child, embryo, fetus. *womb",
+      "d": "Fetus is like a part of the mother's body which is only a platform for the development of the new life.",
+      "e": "受胎 수태 conception, impregnation"
+    },
+    {
+      "q": "窓",
+      "s": "창",
+      "m": "window",
+      "d": "Prague defenestrations: crowd with hearts full of anger entered the town hall and threw several members of the city council out of the window, the members opposed wtih their legs but the elbows helped to push them through the window.",
+      "e": "窓門 창문 window"
+    },
+    {
+      "q": "去",
+      "s": "거",
+      "m": "go away, depart",
+      "d": "Terminator went away sinking into the soil while showing his farewell with an elbow above the surface.",
+      "e": "過去 과거 the past"
+    },
+    {
+      "q": "法",
+      "s": "법",
+      "m": "method, rule, law",
+      "d": "One cannot step twice into the same water for it is fast gone, that is the inflexible law of time and nature.",
+      "e": "方法 방법 way, method"
+    },
+    {
+      "q": "𠫔",
+      "s": "굉",
+      "m": "forearm, *wall",
+      "d": "The elbow hanging under a ceiling will become our element for a wall.",
       "e": ""
     },
     {
-      "q": "",
+      "q": "会/會",
+      "s": "회",
+      "m": "gather, meet",
+      "d": "This is a full version of the character 亼. 会: The official meetings like international summits often start or end with a common photo session by a wall. 會: Gathering of minds to use their mouths either to talk or to eat together.",
+      "e": "會談 회담 conversation, talk, conference"
+    },
+    {
+      "q": "至",
+      "s": "지,질",
+      "m": "reach, arrive; extremely, *climax",
+      "d": "It is hard to reach the ground with fingertips standing upright and bending downwards. And it is extremely hard to reach it with elbows or forearms (𠫔).",
+      "e": "至高 지고 sublimity, supremacy"
+    },
+    {
+      "q": "室",
+      "s": "실",
+      "m": "room",
+      "d": "The essence of a house is its bedroom. The bedroom is the place where its owner wants to arrive, that is the place where the climax of its desires arrives.",
+      "e": "敎室 교실 classroom"
+    },
+    {
+      "q": "到",
+      "s": "도",
+      "m": "arrive",
+      "d": "When one arrives home, he can finally put off the saber and rest.",
+      "e": "到着 도착 arrival"
+    },
+    {
+      "q": "致",
+      "s": "치",
+      "m": "send, deliver. cause. *doth",
+      "d": "Zen taskmaster has already achieved the mental climax and helps his disciples to reach the englightment.",
+      "e": "景致 경치 scenery, view, landscape"
+    },
+    {
+      "q": "互",
+      "s": "호",
+      "m": "mutually",
+      "d": "When you draw this character think of linking two walls together, one right side up and the other upside down.",
+      "e": "相互 상호 reciprocity, mutuality"
+    },
+    {
+      "q": "亠+厶",
       "s": "",
-      "m": "",
-      "d": "",
+      "m": "*infant",
+      "d": "This primitive can be seen as an abbreviation of the full primitive for child 子.",
       "e": ""
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "棄",
+      "s": "기",
+      "m": "reject, abandon",
+      "d": "Chinese buckled a dead child in a wooden basket and carried him to throw away, to abandon.",
+      "e": "暴棄 포기 giving up, abandonment, renouncement"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "育",
+      "s": "육",
+      "m": "bring up, educate",
+      "d": "Raising <b>infants</b> to be strong both in mind and body: corpore sana (<b>flesh</b>), anima sana.",
+      "e": "敎育 교육 education, schooling"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "撤",
+      "s": "철",
+      "m": "remove, withdraw",
+      "d": "Taskmaster that brings up school pupils and teaches how to use a lathe, tells to an unobedient child to remove his fingers far from the lathe to avoid injure.",
+      "e": "撤收 철수 withdrawal, removal, evacuation"
     },
+    {
+      "q": "充",
+      "s": "충",
+      "m": "fill, be full, supply, *allot",
+      "d": "A worker infant runs all day on his emaciated legs to earn his family living and to fill his empty stomach.",
+      "e": "充分하다 충분하다 enough, sufficient"
+    },
+    {
+      "q": "銃",
+      "s": "총",
+      "m": "gun",
+      "d": "A gun is of no use if it is not filled up with deadly metal bullets. Think of a soldier charging his gun with a ramrod.",
+      "e": "銃彈 총탄 ball, bullet"
+    },
+    {
+      "q": "硫",
+      "s": "류,유",
+      "m": "sulfur",
+      "d": "Italian children working in the sulfur mines on the slopes of Etna volcano. They dig the rocks to get the sulfur that flooded from the insides of the Earth.",
+      "e": "硫黃 유황 sulfur"
+    },
+    {
+      "q": "流",
+      "s": "류",
+      "m": "flow, drift, *current",
+      "d": "An infant fell into a water stream and drifts away from his parents and homeland. Yet it is thanks to the stream that he finds a new home. Hence the stream plays a role twice in his life and it is shown twice in the character.",
+      "e": "本流 본류 mainstream"
+    },
+    {
+      "q": "允",
+      "s": "윤,연",
+      "m": "grant, allow, *license",
+      "d": "To obtain a royal permission it is necessary to touch the ground with knees and elbows.",
+      "e": "允許 윤허 royal sanction/permission"
+    },
+    {
+      "q": "唆",
+      "s": "사",
+      "m": "incite, instigate, *tempt",
+      "d": "Malicious instigators walk around and instill poisonous words from their mouths drop by drop to obtain one's consent to realize their nefarious plans.",
+      "e": "示唆 시사 suggestion, hint"
+    }
   ]
 }
