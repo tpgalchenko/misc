e730fe5241b2013d8a66ae04adbc81a575d0b007
Taras P. Galchenko <t.galchenko@e-soft.ru>
2025-12-17 13:53:18 +0300

    .

---
+++ hanja-flash/l24.js
@@ -0,0 +1,215 @@
+const L24 = {
+  "name" : "L24",
+  "cards" : [
+    {
+      "q": "出",
+      "s": "출",
+      "m": "go out, *exit",
+      "d": "The kanji for exit pictures a series of mountain peaks coming out of the earth.",
+      "e": "出張 출장 business trip"
+    },
+    {
+      "q": "山",
+      "s": "산",
+      "m": "mountain",
+      "d": "Note the clearer outline of a triangular mountain here.",
+      "e": "登山 등산 mountain climbing"
+    },
+    {
+      "q": "拙",
+      "s": "졸",
+      "m": "stupid, clumsy; *bungling",
+      "d": "A studpi tailor made a pair of poor gloves such as fingers exit out of the gloves. Really, a bungle.",
+      "e": "拙速 졸속 headlong, hasty"
+    },
+    {
+      "q": "岩",
+      "s": "암",
+      "m": "boulder, rocks",
+      "d": "A rock big as a mountain.",
+      "e": ""
+    },
+    {
+      "q": "炭",
+      "s": "탄",
+      "m": "charcoal",
+      "d": "Although it is now made artificially, the first charcoal humans encountered was naturally created during forest fires. Therefore, the character 炭 can be said to be a character that expresses the place where charcoal was first created by combining the characters 山 (mountain), 厂 (foothill), and 火 (fire).",
+      "e": " 煉炭 연탄 a charcoal briquet"
+    },
+    {
+      "q": "岐",
+      "s": "기",
+      "m": "fork in road, branch off",
+      "d": "The place a road meets a mountain is a turning point, where the road branches off. One has to decide which branch to follow - left, right or upwards.",
+      "e": "岐路 기로 crossroads, turning point"
+    },
+    {
+      "q": "峠",
+      "s": "상",
+      "m": "mountain pass; mountain *peak",
+      "d": "A lake on the mountain top reflects the sky above as if it were below.",
+      "e": ""
+    },
+    {
+      "q": "崩",
+      "s": "붕",
+      "m": "crumble",
+      "d": "The Earth lies on three whales, the sky is supported by Atlas, so every mountain is supported by two companions dwarves.",
+      "e": "崩壞 붕괴 fall, breakdown, collapse"
+    },
+    {
+      "q": "密",
+      "s": "밀",
+      "m": "dense, tight, intimate; *secrecy",
+      "d": "Houses in mountains are invariably secret places invariably hard to reach. Think of a Hitler's secret bunker.",
+      "e": "祕密 비밀 secret"
+    },
+    {
+      "q": "蜜",
+      "s": "밀",
+      "m": "honey",
+      "d": "Bees are insects invariably true to their home, they bring the collected nectar to the house and produce honey.",
+      "e": "蜜月 밀월 honeymoon"
+    },
+    {
+      "q": "嵐",
+      "s": "람,남",
+      "m": "mountain mist, haze; *storm",
+      "d": "Mountain weather is often unstable and winds bring fog, mist or even storm.",
+      "e": "嵐氣 남기 mist"
+    },
+    {
+      "q": "崎",
+      "s": "기",
+      "m": "rough, uneven; *promontory",
+      "d": "Karadag. Mountainous cape - promontory - is hard to reach and to traverse due to its uneven surface.",
+      "e": "崎嶇 기구 vicissitudes, ups and downs, adversity"
+    },
+    {
+      "q": "入",
+      "s": "입",
+      "m": "enter",
+      "d": "",
+      "e": "加入 가입 joining, entrance, subscription"
+    },
+    {
+      "q": "込",
+      "s": "입",
+      "m": "crowd into, go into; *crowded",
+      "d": "People leave their homes and crowd in the street.",
+      "e": ""
+    },
+    {
+      "q": "分",
+      "s": "분",
+      "m": "divide",
+      "d": "A dagger goes into an apple and divides it in two.",
+      "e": "部分 부분 part, portion"
+    },
+    {
+      "q": "貧",
+      "s": "빈",
+      "m": "poverty",
+      "d": "Poverty is the state where one has no money to share with anyone.",
+      "e": "貧富 빈부 wealth and poverty"
+    },
+    {
+      "q": "頒",
+      "s": "반",
+      "m": "confer, bestow; publish; *partition",
+      "d": "Promulgation of important information is bringing it to the attention of everyone, that is sharing it with every 'head'.",
+      "e": "頒布 반포 proclamation, promulgation"
+    },
+    {
+      "q": "公",
+      "s": "공",
+      "m": "fair; *public",
+      "d": "A public politician comes into a hall with his elbows bent, shaking his arms to greet the crowd.",
+      "e": "公式 공식 formula, formality, official ceremony; 公平 공평 fairness, equitability"
+    },
+    {
+      "q": "松",
+      "s": "송",
+      "m": "pine tree",
+      "d": "Pine tree forests were once an important source of wood for ship building, so these forests were made public, state property.",
+      "e": "松津 송진 resin"
+    },
+    {
+      "q": "翁",
+      "s": "옹",
+      "m": "venerable old man",
+      "d": "While young citizens can serve the public affairs (res publica) with their muscles, the only thing venerable old men can do is to use their brain and to write clever laws with pen (feather).",
+      "e": "老翁 노옹 an elderly gentlemen, an old man"
+    },
+    {
+      "q": "訟",
+      "s": "송",
+      "m": "accuse, dispute, *sue",
+      "d": "Public accusation is suject to a lawsuit.",
+      "e": "訴訟 소송 lawsuit, litigation"
+    },
+    {
+      "q": "谷",
+      "s": "곡",
+      "m": "valley",
+      "d": "If you stand on your head and look at this kanji, the image of avalley stands out more clearly: the mouth of the river whose water flows down at the intersection of the two mountains, with the final two strokes adding the element of perspective.",
+      "e": "溪谷 계곡 valley, gorge, ravine"
+    },
+    {
+      "q": "浴",
+      "s": "욕",
+      "m": "bathe",
+      "d": "Water in the valley - bathing.",
+      "e": "沐浴 목욕 bath, bathing"
+    },
+    {
+      "q": "容",
+      "s": "용",
+      "m": "looks, appearance, figure; *contain",
+      "d": "This character depicts a house so large that it can contain an entire valley.",
+      "e": "內容 내용 content(s); 形容 형용 figure, shape; 美容 미용 beauty culture"
+    },
+    {
+      "q": "溶",
+      "s": "용",
+      "m": "melt",
+      "d": "A chocolate with filling melts down and lets out the liquor it contains.",
+      "e": "溶解 용해 dissolution, melting"
+    },
+    {
+      "q": "欲/慾",
+      "s": "욕",
+      "m": "longing",
+      "d": "A person with an open mouth (yawning) is longing to drink all the water of a valley.",
+      "e": "意欲 의욕 will, volition, desire"
+    },
+    {
+      "q": "裕",
+      "s": "유",
+      "m": "abundant",
+      "d": "This character shows the typical cloak of valley folk, which, unlike the tailor-made, high-fashion overcoats of city folk, is loose-fitting and free-form. Hence the key word’s meaning of abundant.",
+      "e": "富裕 부유 wealth, opulence"
+    },
+    {
+      "q": "㕣",
+      "s": "연",
+      "m": "mountain swamp, *gully (small ravine, ditch)",
+      "d": "As an abbreviation of the kanji for a valley, this primitive gets its meaning as a small valley or gully.",
+      "e": ""
+    },
+    {
+      "q": "鉛",
+      "s": "연",
+      "m": "lead (metal)",
+      "d": "Lead is a heavy metal, but it is flexible and easy to work with. Here the character 㕣 which means heavy and flexible mud of a swamp expresses the same property of lead.",
+      "e": ""
+    },
+    {
+      "q": "沿",
+      "s": "연",
+      "m": "go along",
+      "d": "Railway tracks go alongside water streams or ravines. Think of  the 'corniche' roads at Côte d'azure.",
+      "e": "沿海 연해 the sea along the coast, coastal waters"
+    }
+  ]
+};
