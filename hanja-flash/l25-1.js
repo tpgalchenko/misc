90205446603de7a56e7eaff03248eec60bf0454e
Taras P. Galchenko <t.galchenko@e-soft.ru>
2025-12-19 13:31:48 +0300

    .

---
+++ hanja-flash/l25-1.js
@@ -0,0 +1,362 @@
+const L25_1 = {
+  "name" : "L25-1",
+  "cards" : [
+  	{
+  		"q": "龸+口/尙",
+  		"s": "",
+  		"m": "*outhouse",
+  		"d": "尙 - house with a window. The combination of the element for <b>little</b>, the basic <b>roof</b> structure here (in which the chimney was overwritten, as it was in the element for vulture), combined with the <b>window</b> (<b>mouth</b>) below, gives this element its meaning of <b>outhouse</b>.",
+  		"e": ""
+  	},
+  	{
+  		"q": "賞",
+  		"s": "상",
+  		"m": "prize",
+  		"d": "The prize received is so huge that it is stored in an outhouse, where McDuck can swim in the mountains of money.",
+  		"e": "賞金 상금 prize money, award"
+  	},
+  	{
+  		"q": "党/黨",
+  		"s": "당",
+  		"m": "party, gang",
+  		"d": "A <b>party</b> of conspirators gathered in an <b>outhouse</b around an improvisory <b>black</b> furnace.",
+  		"e": "政黨 정당 political party"
+  	},
+  	{
+  		"q": "堂",
+  		"s": "당",
+  		"m": "hall, government office/chamber",
+  		"d": "Roman public offices on the Capitolium land, lots of outhouses for various purposes.",
+  		"e": "食堂 식당 dining room, buffet"
+  	},
+  	{
+  		"q": "常",
+  		"s": "상",
+  		"m": "usual, common, regular",
+  		"d": "In Russia the bath-houses are common, they are built as outhouses. There people wear towels instead of usual clothes.",
+  		"e": "日常 일상 daily, every day, usually"
+  	},
+  	{
+  		"q": "裳",
+  		"s": "상",
+  		"m": "skirt; clothes",
+  		"d": "In many cultures women often live in separate rooms. Here we see a special outhouse for woman garment - skirts.",
+  		"e": "衣裳 의상 clothes, dress"
+  	},
+  	{
+  		"q": "掌",
+  		"s": "장",
+  		"m": "palm of hand, sole of foot; *manipulate",
+  		"d": "WC-outhouses are often scary places where one does not want to dirty his hands and must manipulate everything with fingertips against disgust.",
+  		"e": "掌匣 장갑 gloves"
+  	},
+  	{
+  		"q": "皮",
+  		"s": "피",
+  		"m": "skin, *pelt",
+  		"d": "The simplest way to remember this character is to see it as built up from that for branch 支. The first stroke can then stand for something “hanging” down from the branch, namely its bark or pelt.",
+  		"e": "皮膚 피부 skin"
+  	},
+  	{
+  		"q": "波",
+  		"s": "파",
+  		"m": "waves",
+  		"d": "If one keeps its hand under the water for a long time, the skin becomes wrinkled, wavy.",
+  		"e": "波濤 파도 waves, billows, surges"
+  	},
+  	{
+  		"q": "婆",
+  		"s": "파",
+  		"m": "old woman",
+  		"d": "Old woman's skin is all wrinkled, like water surface at windy day.",
+  		"e": "老婆 노파 old woman"
+  	},
+  	{
+  		"q": "披",
+  		"s": "피",
+  		"m": "wear; *expose, reveal",
+  		"d": "A stone-age woman looks at her reflection in the water and adjusts the pelt she wears with light pinches of fingers.",
+  		"e": "猖披 창피 shame, disgrace"
+  	},
+  	{
+  		"q": "破",
+  		"s": "파",
+  		"m": "break, ruin, *rend",
+  		"d": "To peel a stone (to rend its pelt) = to break, to ruin.",
+  		"e": "破壞 파괴 destruction, demolition"
+  	},
+  	{
+  		"q": "被",
+  		"s": "피",
+  		"m": "passive indicator, *incur; put on",
+  		"d": "One has torn its pelt garment into stripes, sewed them together to make a quilt, then put on and fell asleep.",
+  		"e": "被害 피해 damage, injury, harm"
+  	},
+  	{
+  		"q": "歹",
+  		"s": "알,대",
+  		"m": "bad, wicked; *bone",
+  		"d": "This character is meant to be a pictograph of a bone attached to a piece of flesh (or vice versa.) The first stroke serves to keep it distinct from the character for evening.",
+  		"e": ""
+  	},
+  	{
+  		"q": "残/殘",
+  		"s": "잔",
+  		"m": "*remainder; injure, spoil, cruel",
+  		"d": "The character 戔 composed of the overlapping characters 戈 (spear) means ‘harm.’ The character 殘 expresses the appearance of poking a bone fragment with a spear. It is extremely cruel to poke a spear again into the bones of an already dead person, that is the reaminder of a person.",
+  		"e": "殘額 잔액 money left over, the remainder"
+  	},
+  	{
+  		"q": "殉",
+  		"s": "순",
+  		"m": "martyrdom",
+  		"d": "The martyrdom of the man who withstood his beatings for 10 days until all that remained of him was bones.",
+  		"e": "殉敎者 순교자 (religious) martyr"
+  	},
+  	{
+  		"q": "殊",
+  		"s": "수",
+  		"m": "particularly",
+  		"d": "Bone 歹 is normally white; so a bone that's vermilion 朱 is particularly unusual.",
+  		"e": "特殊 특수 special, peculiar, particular"
+  	},
+  	{
+  		"q": "殖",
+  		"s": "식",
+  		"m": "breed, spawn; increase, augment",
+  		"d": "Advertisement for Breast Implants -- Tired of nights all alone? Want to get <b>boned</b> <b>straightaway</b>? <b>AUGMENT</b> your breasts and all your troubles will be solved!",
+  		"e": "增殖 증식 increase, multiplication"
+  	},
+  	{
+  		"q": "列",
+  		"s": "렬(열)",
+  		"m": "a line, a file; to arange in order",
+  		"d": "A line of skeletal warriors, all armed with sabers.",
+  		"e": "列車 열차 (railway) train"
+  	},
+  	{
+  		"q": "裂",
+  		"s": "렬(열)",
+  		"m": "split, crack",
+  		"d": "They tried issuing the skeleton warriors with proper garments to wear, but they kept getting split by the sharp bones or by their sabres. In the end, it was decided they look scarier naked anyway.",
+  		"e": "分裂 분열 division, disunion, split"
+  	},
+  	{
+  		"q": "烈",
+  		"s": "렬(열)",
+  		"m": "violent, vehement, *ardent",
+  		"d": "A long line, in single file, of ardent admirers, their passion burning like flames for their beloved.",
+  		"e": "烈風 열풍 a heavy/severe wind, a gale"
+  	},
+  	{
+  		"q": "死",
+  		"s": "사",
+  		"m": "death",
+  		"d": "One evening he choked to death on a spoon.",
+  		"e": "死亡 사망 death, decease"
+  	},
+  	{
+  		"q": "葬",
+  		"s": "장",
+  		"m": "interment",
+  		"d": "There's an open casket at a funeral. The dead body (death) is holding flowers with both of its hands.",
+  		"e": "葬禮 장례 funeral"
+  	},
+  	{
+  		"q": "舛",
+  		"s": "천",
+  		"m": "oppose, deviate; *sunglasses",
+  		"d": "The feet crossed in a way to express the meaning of ‘to be crooked’ or ‘to be disorganized.",
+  		"e": ""
+  	},
+  	{
+  		"q": "瞬",
+  		"s": "순",
+  		"m": "wink",
+  		"d": "An owl opened his <b>eye</b> and <b>winked</b> at me from inside his <b>birdhouse</b> - I was so surprised that I <b>blinked</b> behind my <b>sunglasses</b>.",
+  		"e": "瞬間 순간 moment, instant"
+  	},
+  	{
+  		"q": "耳",
+  		"s": "이",
+  		"m": "ear",
+  		"d": "",
+  		"e": "耳目口鼻 이목구비 ear, eye, mouth and nose; features"
+  	},
+  	{
+  		"q": "取",
+  		"s": "취",
+  		"m": "take, obtain",
+  		"d": "Taking temperature in a crotch and in an ear.",
+  		"e": "採取 채취 picking, gathering, extraction"
+  	},
+  	{
+  		"q": "趣",
+  		"s": "취",
+  		"m": "*gist, what attracts one's attention",
+  		"d": "The <b>gist</b> of it is this: We <b>take</b> the money and <b>run</b>!",
+  		"e": "趣味 취미 interest, liking, hobby"
+  	},
+  	{
+  		"q": "最",
+  		"s": "최",
+  		"m": "utmost",
+  		"d": "If you were to pick the <b>utmost</b> celestial body from the sky, it's the <b>sun</b> you'd <b>take</b>.",
+  		"e": "最善 최선 the best"
+  	},
+  	{
+  		"q": "撮",
+  		"s": "촬",
+  		"m": "*snapshot; little bit, pinch",
+  		"d": "Take the <b>utmost</b> care that you don't have your <b>finger</b> in the way when you take a <b>snapshot</b>.",
+  		"e": "撮影 촬영 photographing, filming"
+  	},
+  	{
+  		"q": "恥",
+  		"s": "치",
+  		"m": "shame, humiliation",
+  		"d": "He was so ashamed he felt as if his heart was beating in his red ears.",
+  		"e": "國恥 국치 national humiliation, disgrace"
+  	},
+  	{
+  		"q": "職",
+  		"s": "직",
+  		"m": "post, profession, duty",
+  		"d": "Buckingham Palace guards are famous for staying at their post no matter what, even if you blow a kazoo in their ear!",
+  		"e": "職業 직업 occupation, vocation, profession"
+  	},
+  	{
+  		"q": "聖",
+  		"s": "성",
+  		"m": "holy",
+  		"d": "A true holy man uses his ear to listen (i.e., to God or his supplicant) before he opens his mouth to speak, unlike a charlatan who just makes something up. Of course, kings are often true holy men and prophets (e.g., King David in the bible).",
+  		"e": "聖人 성인 saint, holy man"
+  	},
+  	{
+  		"q": "敢",
+  		"s": "감",
+  		"m": "daring",
+  		"d": "The daring girl in Catholic school who used a nail 丁 to pierce her ears 耳 was attacked by her taskmaster 攵 of a teacher.",
+  		"e": "敢히 감히 boldly, daringly"
+  	},
+  	{
+  		"q": "懐/懷",
+  		"s": "회",
+  		"m": "bossom; carray in bossom; *pocket",
+  		"d": "'New York State of Mind': you need ten eyes to make sure no one will go through your garments and pick your pockets.<br>She had me in her pocket - it was a state of mind that would eventually demolish me..",
+  		"e": ""
+  	},
+  	{
+  		"q": "曼",
+  		"s": "만",
+  		"m": "long, vast; beautiful; *mandala",
+  		"d": "A mandala is a graphic representation of the universe, here split in 3 planes of existance, the sun representing the cosmos, the eye represents the human observer, the crotch represents the animal instincts.",
+  		"e": ""
+  	},
+  	{
+  		"q": "慢",
+  		"s": "만",
+  		"m": "lazy; arrogant; *ridicule",
+  		"d": "You may ridicule others because you are so <b>conceited</b> that in your state of mind you are at the center of the mandala.",
+  		"e": "倨慢 거만 arrogance, insolence"
+  	},
+  	{
+  		"q": "漫",
+  		"s": "만",
+  		"m": "overflow of water; spreading; *loose",
+  		"d": "In 만화 comics, boys have their eyes on loose girls' wet crotches all day.",
+  		"e": "漫畫 만화 comics"
+  	},
+  	{
+  		"q": "買",
+  		"s": "매",
+  		"m": "buy",
+  		"d": "When something catches my eye, I take out a few clams and buy before anybody else does.",
+  		"e": "購買 구매 purchase, buying"
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  	{
+  		"q": "",
+  		"s": "",
+  		"m": "",
+  		"d": "",
+  		"e": ""
+  	},
+  ]
+}
