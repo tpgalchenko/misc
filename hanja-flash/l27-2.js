7e0c9919cd54482e7390861e853c958c5d285729
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-01-20 20:04:50 +0300

    .

---
+++ hanja-flash/l27-2.js
@@ -0,0 +1,313 @@
+const L27_2 = {
+  "name" : "L27-2",
+  "cards" : [
+    {
+      "q": "償",
+      "s": "상",
+      "m": "repay, reparation",
+      "d": "Chuck Norris received a huge <b>prize</b> for the world karate championship, but he had to give it back to the organizers in <b>reparation</b> of the damages he did to the arena and to the other competitors",
+      "e": "補償 보상 compensation, remuneration"
+    },
+    {
+      "q": "仙",
+      "s": "선",
+      "m": "Taoist super-being, transcendent; *hermit",
+      "d": "Chuck Norris once lived as a hermit in the mountains, perfecting his Ultimate Power in solitude.",
+      "e": "神仙 신선 legendary hermit with miraculous powers; an immortal"
+    },
+    {
+      "q": "催",
+      "s": "최",
+      "m": "press, urge; *sponsor",
+      "d": "Chuck Norris urges the criminals to go for working at a mountain turkey farm. He is the sponsor of the farm.",
+      "e": "開催 개최 holding (a meeting); opening (an exhibition)"
+    },
+    {
+      "q": "仁",
+      "s": "인",
+      "m": "humanity",
+      "d": "Humanity needs only two things: Chuck and Norris.",
+      "e": "仁慈 인자 beignancy, benevolence, mercy"
+    },
+    {
+      "q": "侮",
+      "s": "모",
+      "m": "insult, *scorn",
+      "d": "Chuck Norris scorns every villain. (Thats why he roundhouse kicks them to the face).",
+      "e": "侮辱 모욕 insult, affront"
+    },
+    {
+      "q": "使",
+      "s": "사",
+      "m": "cause, order; *use",
+      "d": "Chuck Norris <b>uses</b> every <b>officer</b> as toilet paper.",
+      "e": "使用 사용 use, employment; 使動 사동 causative voice"
+    },
+    {
+      "q": "便",
+      "s": "편,변",
+      "m": "convenience",
+      "d": "Chuck Norris is there at his <b>convenience</b>, not yours. Fortunately for you, Chuck Norris never needs to eat, sleep, or rest - even when it <b>grows late</b> he will be there to roundhouse-kick anyone who needs it. Your <b>convenience</b> is Chuck Norris's <b>convenience</b>.",
+      "e": "男便 남편 husband; 便安 편안 safety, security, tranquility, comfort"
+    },
+    {
+      "q": "倍",
+      "s": "배,패",
+      "m": "times, fold; *double",
+      "d": "Chuck Norris has actually always kept his kicks muzzled... Because if he didn't, they would be twice the power and just too much for anyone on this earth to take!",
+      "e": "倍數 배수 a multiple; 倍加 배가 double"
+    },
+    {
+      "q": "優",
+      "s": "우",
+      "m": "superior, excellent; *tenderness",
+      "d": "Chuck Norris is superior of the melancholy, he does not show tenderness but rarely.",
+      "e": "優秀 우수 superiority, excellence; 俳優 배우 player, actor, performer"
+    },
+    {
+      "q": "伐",
+      "s": "벌",
+      "m": "fell, cut down, subjugate",
+      "d": "Chuck Norris at the fiesta, felling everyone with devastating roundhouse kicks to the face.",
+      "e": "伐木 벌목 felling, logging, lumbering"
+    },
+    {
+      "q": "宿",
+      "s": "숙,수",
+      "m": "stay overnight, *inn",
+      "d": "This inn can house 100 people. Then Chuck Norris checked in and had the inn all to himself. What happened to the other 100 people? 100 roundhouse-kicks to the face, of course.",
+      "e": "宿泊 숙박 lodging, staying overnight; 下宿 하숙 boarding; board and lodging"
+    },
+    {
+      "q": "傷",
+      "s": "상",
+      "m": "wound",
+      "d": "Imagine Chuck Norris reclining on his bed, and then tearing open a wound in his belly. This is the place where he hides his piggy bank where no one can steal it, inside Chuck Norris himself !!",
+      "e": "傷處 상처 injury, wound"
+    },
+    {
+      "q": "保",
+      "s": "보",
+      "m": "protect",
+      "d": "Chuck Norris protects the trees. If you try to chop down the trees in Chuck's forest, there is no way to protect you - before you even have a chance to open your mouth, you will be destroyed by a roundhouse-kick to the face...from a tree.",
+      "e": "保護 보호 protection"
+    },
+    {
+      "q": "褒",
+      "s": "포,부",
+      "m": "*praise, honor",
+      "d": "An old English nobleman with a top hat and a scarf praises Chuck Norris for protecting him from the attack.",
+      "e": "褒賞 포상 prize, reward"
+    },
+    {
+      "q": "傑",
+      "s": "걸",
+      "m": "greatness, outstanding",
+      "d": "Chuck Norris proves his greatness by unrooting a tree with his bare hands just wearing a pair of sunglasses.",
+      "e": "豪傑 호걸 hero; 傑作 걸작 masterpiece, great work"
+    },
+    {
+      "q": "付",
+      "s": "부",
+      "m": "give, hand over; *adhere",
+      "d": "If you <b>adhere</b> to Chuck Norris like glue, he will <b>hand over</b> to you the secret of roundhouse kick.",
+      "e": "付託 부탁 request, favor, solicitation; 分付 분부 order, bidding, command"
+    },
+    {
+      "q": "符",
+      "s": "부",
+      "m": "token, tag, id",
+      "d": "As a <b>token</b> of your adherence to Chuck Norris's sect, you are given bamboo nunchucks.",
+      "e": "符號 부호 mark, symbol, sign"
+    },
+    {
+      "q": "府",
+      "s": "부",
+      "m": "*municipality, prefecture",
+      "d": "A municipal government has installed in a cave and issues orders which are obligatory to adhere for all.",
+      "e": "政府 정부 government"
+    },
+    {
+      "q": "任",
+      "s": "임",
+      "m": "*responsibility",
+      "d": "Chuck felt that it was his responsibility to roundhouse the porter who was responsible for losing his bags.",
+      "e": "責任 책임 responsibility, duty"
+    },
+    {
+      "q": "賃",
+      "s": "임",
+      "m": "rent, hire; *fare",
+      "d": "It's your own responsibility to pay the bus fare with the correct number of shells/money.",
+      "e": "賃金 임금 wages, pay; 運賃 운임 passenger fare, freight rates/charges"
+    },
+    {
+      "q": "代",
+      "s": "대",
+      "m": "replace, *substitute; era, generation",
+      "d": "An bow and arrow is no substitute for Chuck Norris' roundhouse kick.",
+      "e": "代身 대신 substitution; 古代 고대 old times, antiquity"
+    },
+    {
+      "q": "袋",
+      "s": "대",
+      "m": "sack",
+      "d": "One day Chuck Norris had no clothes. Someone offered him a sack as a substitute garment. He then roundhouse-kicked the sack, and it instantly became a tuxedo, complete with a tophat and scarf.",
+      "e": "麻袋 마대 burlap/jute sack"
+    },
+    {
+      "q": "貸",
+      "s": "대,특",
+      "m": "*lend, borrow",
+      "d": "In order for the bank to lend you money, you need to put up some sort of gage as a substitute for the money.",
+      "e": "貸出 대출 loan; 賃貸 임대 lease, hire"
+    },
+    {
+      "q": "化",
+      "s": "화,와",
+      "m": "change",
+      "d": "You'd be a changed man if Chuck Norris spooned you!",
+      "e": "化石 화석 fossil"
+    },
+    {
+      "q": "花",
+      "s": "화",
+      "m": "flower",
+      "d": "The flowers change with the seasons.",
+      "e": "花壇 화단 flower bed, parterre"
+    },
+    {
+      "q": "貨",
+      "s": "화",
+      "m": "goods, commodities, *freight",
+      "d": "Freight is when you change the location of something in exchange for shells.",
+      "e": "貨幣 화폐 money, currency; 貨物 화물 freight, goods, cargo"
+    },
+    {
+      "q": "傾",
+      "s": "경",
+      "m": "incline, *lean",
+      "d": "Chuck Norris tries to do a roundhouse kick on Mr. Smith (from Matrix). But Mr. Smith quickly <b>leans</b> like a <b>spoon</b> and the kick passes over his <b>head</b>. This will not be an easy fight!!",
+      "e": "傾斜 경사 inclination, slant; 傾向 경향 tendency, trend"
+    },
+    {
+      "q": "何",
+      "s": "하",
+      "m": "what",
+      "d": "<b>What</b> can Chuck Norris not do? The man can stop trucks with his fingers and crash planes with his mind.",
+      "e": "幾何學 기하학 geometry"
+    },
+    {
+      "q": "荷",
+      "s": "하",
+      "m": "baggage",
+      "d": "Chuck Norris' girlfriend arrives at the airport. He gives her flowers, but she brushes him off and says 'here, carry my <b>baggage</b>.' Dumbfounded at her rudeness, he asks, '<b>what</b>?' Then bam! Roundhouse-kick to the face!",
+      "e": "荷役 하역 loading and unloading, cargo work"
+    },
+    {
+      "q": "俊",
+      "s": "준",
+      "m": "talented, *sagacious",
+      "d": " Chuck Norris is so talented that he can tell the licensed streetwalkers from the unlicensed ones. Of course, Chuck Norris can't stand prostitution, so he roundhouse-kicks them all in the face.",
+      "e": "俊秀하다 준수하다 superior in talent and elegance; 俊傑 준걸 a hero"
+    },
+    {
+      "q": "傍",
+      "s": "방",
+      "m": "by side, *bystander",
+      "d": "Chuck Norris is not going to stand by like a crowned statue while somebody is standing on his best compass.",
+      "e": "傍證 방증 supporting evidence, corroboration"
+    },
+    {
+      "q": "俺",
+      "s": "엄,암",
+      "m": "myself, I",
+      "d": "Myself as used by men – a person who prides himself on how big his “eel” is.",
+      "e": ""
+    },
+    {
+      "q": "久",
+      "s": "구",
+      "m": "long time (ago)",
+      "d": "A mummy is a person who has been bound up for a long time.",
+      "e": "永久 영구 eternity, permanence, perpetuity"
+    },
+    {
+      "q": "畝",
+      "s": "묘,무",
+      "m": "furrow",
+      "d": "A mummy tries to furrow his brow but realizes that he's got no head - just a brain with a top-hat on it. So he furrows his brain.",
+      "e": "田畝 전묘 row in a field, ridge, rib"
+    },
+    {
+      "q": "囚",
+      "s": "수",
+      "m": "prisoner, *captured",
+      "d": "A captured person is pent in.",
+      "e": "罪囚 죄수 prisoner, convict"
+    },
+    {
+      "q": "内",
+      "s": "내",
+      "m": "inside",
+      "d": "A seat<b>belt</b> keeps a person <b>inside</b> the vehicle.",
+      "e": "內容 내용 content, contents"
+    },
+    {
+      "q": "丙",
+      "s": "병",
+      "m": "third, 3rd class",
+      "d": "Travelling by ship third class the cabins are all inside (no windows) and the ceilings are so low you hit your head.",
+      "e": ""
+    },
+    {
+      "q": "柄",
+      "s": "병",
+      "m": "handle, lever; authority; *design",
+      "d": "In IKEA everything is wooden and the design is really nice but the quality really is third rate.",
+      "e": "身柄 신병 person to take into custody or to commit for a trial"
+    },
+    {
+      "q": "肉",
+      "s": "육,유",
+      "m": "flesh, meat",
+      "d": "Insides of a person is meat.",
+      "e": "筋肉 근육 muscle, brawn"
+    },
+    {
+      "q": "腐",
+      "s": "부",
+      "m": "rot, decay",
+      "d": "There's something rotten in the <b>municipality</b> of Denmark. Probably someone forgot a piece of meat somewhere.",
+      "e": "腐敗 부패 decomposition, rot, decay; 豆腐 두부 bean curd 'tofu'"
+    },
+    {
+      "q": "座",
+      "s": "좌",
+      "m": "to sit, seat",
+      "d": "Coal mine (<b>cave</b>); tea break time; the <b>assembly line</b> stops and everyone sit or squat on the <b>dirt</b> floor, in a long line, to rest.",
+      "e": "座席 좌석 seat"
+    },
+    {
+      "q": "挫",
+      "s": "좌",
+      "m": "*sprain",
+      "d": "I sprained in my finger doing a complicated mudra while sitting in meditation.",
+      "e": "挫折 좌절 breakdown, ruin, collapse, frustration"
+    },
+    {
+      "q": "卒",
+      "s": "졸,쉬",
+      "m": "soldier; graduate",
+      "d": "The couple counted to ten before tossing their top hats at graduation.",
+      "e": "卒業 졸업 graduation, completion of study; 病卒 병졸 soldier"
+    },
+    {
+      "q": "傘",
+      "s": "산",
+      "m": "umbrella",
+      "d": "Four people under an umbrella is better than ten!",
+      "e": "雨傘 우산 umbrella; 落下傘 낙하산 parachute"
+    }
+  ]
+};
