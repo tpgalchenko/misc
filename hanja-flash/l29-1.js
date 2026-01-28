e55badcf3f86cc28404daf5f2241690479ac8000
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-01-28 19:28:11 +0300

    .

---
+++ hanja-flash/l29-1.js
@@ -0,0 +1,208 @@
+const L29_1 = {
+  "name" : "L29-1",
+  "cards" : [
+    {
+      "q": "方 + 𠂉",
+      "s": "",
+      "m": "banner",
+      "d": "Compass and reclining. Think of the banner as a standard for rallying around; then imagine a crowd reclining before a compass (presumably to give them a “direction” in life).",
+      "e": ""
+    },
+    {
+      "q": "施",
+      "s": "시",
+      "m": "grant, bestow, *alms",
+      "d": "Under their banner, volunteers are requesting alms for scorpion sting victims.",
+      "e": "布施 보시 almsgiving, charity; 施設 시설 institution, establishment, equipment, facilities; 實施 실시 enforcement, implementation"
+    },
+    {
+      "q": "旋",
+      "s": "선",
+      "m": "rotation",
+      "d": "The zoo is now open and the electric <b>banner</b> that says <b>ZOO</b> is in full <b>rotation</b>.",
+      "e": "旋回 선회 revolution, rotation"
+    },
+    {
+      "q": "遊",
+      "s": "유",
+      "m": "wander, roam; *play",
+      "d": "The <b>banner</b> warns that <b>children</b> <b>wander</b> about and <b>play</b> on the <b>road</b>.",
+      "e": "遊覽 유람 sightseeing, excursion"
+    },
+    {
+      "q": "旅",
+      "s": "려",
+      "m": "trip",
+      "d": "After a long trip, any banner will be reduced to a rag.",
+      "e": "旅行 여행 trip, travel, journey"
+    },
+	{
+		"q": "勿",
+		"s": "물,몰",
+		"m": "not; *knot",
+		"d": "This kanji is <b>not</b> a piglett.",
+		"e": "勿論 물론 of course, undoubtedly"
+	},
+    {
+      "q": "物",
+      "s": "물",
+      "m": "thing",
+      "d": "For Hindus, a cow is not a thing, it's a god!",
+      "e": "物件 물건 thing"
+    },
+    {
+      "q": "易",
+      "s": "역, 이",
+      "m": "change; *easy",
+      "d": "The sun is not an easy place to get to.",
+      "e": "容易 용이 ease; 貿易 무역 trade, commerce"
+    },
+    {
+      "q": "賜",
+      "s": "사",
+      "m": "grant",
+      "d": "A grant is easy money.",
+      "e": "恩賜 은사 king's gift, present; 膳賜하다 선사하다 present, make a gift"
+    },
+    {
+      "q": "尸",
+      "s": "시",
+      "m": "corpse; *flag",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "尿",
+      "s": "뇨,요",
+      "m": "urine",
+      "d": "The flag primitive looks like a big 'P'. And, let's face it, urine is just pee water. So, Urine = P Water",
+      "e": "糖尿 당뇨 glucosuria; 放尿 방뇨 urination, pissing"
+    },
+    {
+      "q": "尼",
+      "s": "니",
+      "m": "Buddhist nun",
+      "d": "Red cross nuns sit in a tent under the red cross flag, spooning out food to the hungry.",
+      "e": "釋迦牟尼 석가모니 Shakyamuni"
+    },
+    {
+      "q": "尻",
+      "s": "고",
+      "m": "buttocks",
+      "d": "Her ass was so fine I gave it a nine and flagged it as mine.",
+      "e": ""
+    },
+    {
+      "q": "泥",
+      "s": "니",
+      "m": "mud",
+      "d": "TV gets weirder and weirder... Now they're doing mud wrestling shows with nuns splashing around in the water. What will they think of next?",
+      "e": "雲泥之差 운니지차 great difference (lit. between clouds and mud)"
+    },
+    {
+      "q": "塀/塀",
+      "s": "병",
+      "m": "fence",
+      "d": "The Earth is fenced off with flags indicating different countries, forming a complex puzzle of lands.",
+      "e": ""
+    },
+    {
+      "q": "履",
+      "s": "리",
+      "m": "footgear",
+      "d": "The flag has fallen! To win the flag must be <b>restor</b>ed. Whomever has to run up on the hill to restore the flag needs good footgear.",
+      "e": "履歷 이력 one's personal history, background, career"
+    },
+    {
+      "q": "屋",
+      "s": "옥",
+      "m": "house, room, shelter; *roof",
+      "d": "Flag at the climax (highest point) of a castle roof.",
+      "e": "韓屋 한옥 Korean-style house"
+    },
+    {
+      "q": "握",
+      "s": "악",
+      "m": "*grip, grasp",
+      "d": "Grip the roof with your fingers fast lest fall off.",
+      "e": "把握 파악 grasping, seizing, understanding; 握手 악수 handshake"
+    },
+    {
+      "q": "屈",
+      "s": "굴",
+      "m": "bend, flex; *yield",
+      "d": "Trench warfare: when yielding, soldiers raise the white flag before exiting from the trenches.",
+      "e": "屈服 굴복 submission, yielding"
+    },
+    {
+      "q": "掘",
+      "s": "굴",
+      "m": "dig",
+      "d": "Trench warfare: the army chief said that he would prefer to dig a trenches with his own fingers than to yield to the enemy.",
+      "e": "發掘 발굴 excavation, exhumation"
+    },
+    {
+      "q": "堀",
+      "s": "굴",
+      "m": "cave, hole, *ditch",
+      "d": "Trench warfare :the yielding army abandons it's patch of soil, leaving behind a landscape of ditchs and trenches.",
+      "e": ""
+    },
+    {
+      "q": "居",
+      "s": "거",
+      "m": "reside",
+      "d": "The <b>old</b> tradition dictates that when the the head of the state <b>resides</b> at the primary residence the state flag is hoisted up.",
+      "e": "住居 주거 dwelling (house), residence"
+    },
+    {
+      "q": "据",
+      "s": "거",
+      "m": "to occupy; a base; *set",
+      "d": "I was bored with life. So I bought a globe and gave it a good spin. Then I set my finger on the globe and said, 'wherever it stops is where I shall reside.' That is why I have to learn all these kanji now. Could have been worse, I guess.",
+      "e": "据置 거치 leaving (a loan) unredeemed; deferment"
+    },
+    {
+      "q": "裾",
+      "s": "거",
+      "m": "lapel, border of garment, *hem",
+      "d": "The hem of a cloak is the best place for your secret items to reside.",
+      "e": ""
+    },
+    {
+      "q": "層",
+      "s": "층",
+      "m": "stratum",
+      "d": "Formerly, in Japan, each social stratum had to wear specific types of clothes to flag their social class and caste. Of course, these days Japanese society isn't so regimented anymore.",
+      "e": "層 stratum, layer"
+    },
+    {
+      "q": "局",
+      "s": "국",
+      "m": "*bureau, office",
+      "d": "The Federal <b>Bureau</b> of Investigation has email <b>scanners</b> that flag certain <b>phrases</b>.",
+      "e": "結局 결국 end, close, after all, finally; 藥局 약국 pharmacy, drugstore"
+    },
+    {
+      "q": "遅/遲",
+      "s": "지",
+      "m": "late, tardy, *slow",
+      "d": "A flock of sheeps/rhinoceros was slowly crossing the road so I was tardy.",
+      "e": "遲刻 지각 being late, tardiness"
+    },
+    {
+      "q": "漏",
+      "s": "루,누",
+      "m": "leak",
+      "d": "Water leaked through the flag hole every time it rained.",
+      "e": " 漏泄/漏洩 누설 leak, leakage; disclosure"
+    },
+    {
+      "q": "刷",
+      "s": "쇄",
+      "m": "brush, scrub; *printing",
+      "d": "A cash register prints slips unrolling a bundle and then clipping it off with a special blade.",
+      "e": "印刷 인쇄 print, printing; 刷新 쇄신 complete reform, renovation, cleanup"
+    }
+  ]
+};
---
+++ hanja-flash/l29-2.js
@@ -0,0 +1,138 @@
+const L29_2 = {
+  "name" : "L29-2",
+  "cards" : [
+    {
+      "q": "尺",
+      "s": "척",
+      "m": "shaku",
+      "d": "Chinese measure approx. 'foot'.<br><b>*</b>As a primitive, this will mean the <b>shakuhachi flute</b>",
+      "e": "尺度 척도 a measure, a scale, a criterion"
+    },
+    {
+      "q": "尽/盡",
+      "s": "진",
+      "m": "exhaust",
+      "d": "This old Japanese man totally exhausted himself giving a winter-time performance of shakuhachi. That winter was so cold, the shakuhachi was getting clogged with ice as the man was playing. ",
+      "e": "消盡 소진 exhaustion, vanishing completely"
+    },
+    {
+      "q": "辛",
+      "s": "신",
+      "m": "bitter; *spicy",
+      "d": "This character pictures food whose taste is so hot and spicy that it makes the hairs on your body stand up as straight as needles.",
+      "e": "辛辣 신랄 sharp, bitter, severe, harsh; 艱辛히 간신히 barely, hardly, labouriously"
+    },
+    {
+      "q": "幸",
+      "s": "행",
+      "m": "luck(ily), *happiness",
+      "d": "Happiness is the <b>one</b> bite of <b>spicy</b> curry.",
+      "e": "幸福 행복 happiness; 多幸 다행 luck, good fortune"
+    },
+    {
+      "q": "睪",
+      "s": "역,택",
+      "m": "spy on",
+      "d": "The eyes of James Bond spy on the happinness of the criminals and enemies just to put an end to their luck and bliss.",
+      "e": ""
+    },
+    {
+      "q": "沢/澤",
+      "s": "택",
+      "m": "swamp",
+      "d": "You're hiding from the enemy underwater in a swamp. To breathe, you have your shakuhachi flute poking up through the surface of the water.<br>A spy does not hesitate to hide in water, even in a swamp to be able to spy on.",
+      "e": "惠澤 혜택 benefit, boon, favor, grace; 德澤 덕택 indebtness, favor, grace, help"
+    },
+    {
+      "q": "訳/譯",
+      "s": "역",
+      "m": "translate",
+      "d": "You cannot pretend to translate into words the melody of the shakuhachi!<br>A perfect spy like James Bond does not need a translator, he speaks all languages.",
+      "e": "飜譯 (翻譯) 번역 translation"
+    },
+    {
+      "q": "択/擇",
+      "s": "택",
+      "m": "choose",
+      "d": "The fingers on the Shakuhachi 'choose' the notes.<br>The fingers of the spy scan over the walls to find the secret niches where the important documents are hidden.",
+      "e": "選擇 선택 choice, selection"
+    },
+    {
+      "q": "昼/晝",
+      "s": "주",
+      "m": "daytime",
+      "d": "Roosters in Japan are very civilised, they play the shakuhachi at nightbreak to signify the beginning of daytime.<br>It is best to take a brush to study at the nightbreak to profit of the daylight to the maximum.",
+      "e": "白晝 백주 broad daylight"
+    },
+    {
+      "q": "戸/戶",
+      "s": "호",
+      "m": "*door; household",
+      "d": "If you are too poor to afford a door, you can just hang a flag instead (noren).",
+      "e": "門戶 문호 door; 富戶 부호 a rich man"
+    },
+    {
+      "q": "肩",
+      "s": "견",
+      "m": "shoulder",
+      "d": "The shoulder is the best part of the body to bash a door open with (Don't ever try this. Use your foot).",
+      "e": "肩章 견장 shoulder strap, epaulet"
+    },
+    {
+      "q": "房",
+      "s": "방",
+      "m": "building, room; *tassel",
+      "d": "A huge building with many doors and many rooms, a true maze. No compass will help to get out, but a tassel can help to catch a wind and find a direction out.",
+      "e": "廚房 주방 kitchen; 冷房 냉방 cold room, air conditioning"
+    },
+    {
+      "q": "扇",
+      "s": "선",
+      "m": "*fan; door panel",
+      "d": "A door panel flaps and produces a stream of air not worse than a large fan of feathers.",
+      "e": "扇風機 선풍기 electric fan"
+    },
+    {
+      "q": "炉/爐",
+      "s": "로",
+      "m": "*hearth, fireplace",
+      "d": "- Honey? Do we have any wood left for the hearth?<br>- Aagh, just use the frickin' door!",
+      "e": "暖爐 (煖爐) 난로 stove, heater, fireplace; 火爐 화로 brazier, fire pot/box"
+    },
+    {
+      "q": "戻",
+      "s": "태,대,제",
+      "m": "re-",
+      "d": "The St. Bernard dog whining at the door of his master’s house in order to re-enter / re-turn inside.",
+      "e": ""
+    },
+    {
+      "q": "涙",
+      "s": "",
+      "m": "tears",
+      "d": "The story of Hachiko: the faithfull dog who still waited for his master to return long after he had died. Brings a tear to the eye.",
+      "e": ""
+    },
+    {
+      "q": "雇",
+      "s": "",
+      "m": "employ",
+      "d": "Our company has no standards. We'll employ any turkey who walks in the door.",
+      "e": ""
+    },
+    {
+      "q": "顧",
+      "s": "",
+      "m": "look back",
+      "d": "The people we employ are always turning their heads to look back. They don't want to be caught playing solitare on company time.",
+      "e": ""
+    },
+    {
+      "q": "啓",
+      "s": "",
+      "m": "disclose",
+      "d": "It's best not to disclose what you were forced to do with your mouth behind closed doors for your taskmaster.",
+      "e": ""
+    }
+  ]
+};
