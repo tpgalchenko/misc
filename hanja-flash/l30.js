2b30003f9718f8c94c0d301b48f09368cc512ea1
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-01-30 08:31:30 +0300

    .

---
+++ hanja-flash/l30.js
@@ -0,0 +1,299 @@
+const L30 = {
+  "name" : "L30",
+  "cards" : [
+    {
+      "q": "示/礻/𥘅",
+      "s": "시,",
+      "m": "show; *altar",
+      "d": "Although the elements two and little are available for the using, it may be easier to remember this character as a picture of an altar. Something placed atop the altar is put on show for all to see.",
+      "e": "展示 전시 display, exhibition; 揭示 게시 notice, notification, placard"
+    },
+    {
+      "q": "曲",
+      "s": "곡,국",
+      "m": "*bend; melody",
+      "d": "This is a guitar fretboard. You have to bend the strings to produce a melody.",
+      "e": "曲線 곡선 curved line, curve; 作曲 작곡 musical composition"
+    },
+    {
+      "q": "豆",
+      "s": "두",
+      "m": "beans",
+      "d": "If I eat beans, after one mouthful I fart like some kind of horned animal.",
+      "e": "豆腐 두부 tofu"
+    },
+    {
+      "q": "豊/豐",
+      "s": "풍",
+      "m": "abundant, *bountiful",
+      "d": "The <b>bountiful</b> feast was so bountiful that it <b>bent</b> the table below it under the weight of all kinds of <b>beans</b>!<br>We go camping on a <b>mountain</b> and get hungry. Luckily the <b>mountain</b> is absolutely covered with <b>bushes</b> that bear tasty berries! The supply of berry is so <b>plentiful</b> that I don't have to resort to eating the baked <b>beans</b> I had packed in my rucksack.",
+      "e": "豐饒 풍요 richness, abundance"
+    },
+    {
+      "q": "礼/禮",
+      "s": "례",
+      "m": "manners, courtesy, *salutation",
+      "d": "Salute the church altar with a bow (bend yourself like a fishhook).<br>Good manners require us to be kind with gods and to give them abundant offers on their altars.",
+      "e": "禮儀 예의 manners, etiquette, courtesy"
+    },
+    {
+      "q": "祥",
+      "s": "상",
+      "m": "good luck, *auspicious",
+      "d": "Bringing a sheep to sacrifice on the altar was a sign of an auspicious life. The poor could only afford a pigeon, the more auspicious an expensive sheep for an offering.",
+      "e": "祥瑞 상서 good omen, auspicious sign"
+    },
+    {
+      "q": "祝",
+      "s": "축,주",
+      "m": "*celebrate, pray for happiness",
+      "d": "When Japanese people turn 20 they usually visit the <b>altar</b> at their local shrine and <b>celebrate</b> no longer being a <b>teenager</b>.",
+      "e": "祝賀 축하 celebration, congratulations, felicitations"
+    },
+    {
+      "q": "福",
+      "s": "복,부",
+      "m": "*blessing, happiness",
+      "d": "Indulgences - one gives wealth to the altar and receives a blessing.",
+      "e": "福 fortune, good luck, blessing, happiness"
+    },
+    {
+      "q": "祉",
+      "s": "지",
+      "m": "*welfare, happiness, blessings",
+      "d": "Marxist belief: To improve general welfare, the influence of the altar must be stopped.",
+      "e": "福祉 복지 welfare, well-being"
+    },
+    {
+      "q": "社",
+      "s": "사",
+      "m": "company",
+      "d": "When you enter a Japanese company it's like you join a cult... your workdesk becomes your altar and you worship the very soil the office is built on.",
+      "e": "會社 회사 company, firm"
+    },
+    {
+      "q": "視/視",
+      "s": "시",
+      "m": "*inspection, observe, see",
+      "d": "At the altar, god looks at you, in an inspection of your soul.",
+      "e": "無視 무시 disregard, neglect"
+    },
+    {
+      "q": "奈",
+      "s": "내,나",
+      "m": "*Nara, but, however",
+      "d": "Nara is just a big show for tourists. There is also a big altar.",
+      "e": "奈落 (那落) 나락 hell; 莫無可奈 막무가내 stubbornly,obstinately"
+    },
+    {
+      "q": "尉",
+      "s": "위",
+      "m": "military officer",
+      "d": "Military officers glue to the flag as if it were a holy altar.",
+      "e": "大尉 대위 captain; 少尉 소위 second lieutenant, sublieutenant"
+    },
+    {
+      "q": "慰",
+      "s": "위",
+      "m": "consolation",
+      "d": "A military officer must offer consolation to the hearts of the families of his fallen men.",
+      "e": "慰勞 위로 consolation, solace, comfort"
+    },
+    {
+      "q": "款",
+      "s": "관",
+      "m": "article, clause; *goodwill",
+      "d": "Goodwill is something that samurais may lack but his oath contains a clause that does not allow him ignoring masters's commands.",
+      "e": "約款 약관 article, clause; 定款 정관 articles of association"
+    },
+    {
+      "q": "禁",
+      "s": "금",
+      "m": "prohibition",
+      "d": "You are <b>prohibited</b> from entering the <b>grove</b> because there is a sacred <b>altar</b> inside it.",
+      "e": "禁止 금지 prohibition, ban, taboo, interdiction"
+    },
+    {
+      "q": "襟",
+      "s": "금",
+      "m": "collar",
+      "d": "A collar is the part of your cloak that, if tightened too tight, will prohibit breathing.",
+      "e": "胸襟 흉금 the bossom, the heart, the inner mind"
+    },
+    {
+      "q": "宗",
+      "s": "종",
+      "m": "lineage, ancestry; *religion",
+      "d": "All the religions have their own temples, which are houses where they place their altars.",
+      "e": "宗敎 종교 religion; 宗派 종파 religious sect, main branch of a family"
+    },
+    {
+      "q": "崇",
+      "s": "숭",
+      "m": "*adore, esteem, venerate",
+      "d": "In ancient Greece, it was the religion of the time to adore the gods who live on Mount Olympus.",
+      "e": "崇尙 숭상 respect, veneration"
+    },
+    {
+      "q": "祭",
+      "s": "제",
+      "m": "*ritual; sacrifice to, worship",
+      "d": "A ritual such as a circumcision: a small piece of flesh from the crotch is left on the altar.",
+      "e": "祭祀 제사 memorial service; 祭壇 제단 altar"
+    },
+    {
+      "q": "察",
+      "s": "찰",
+      "m": "*guess; examine, investigate",
+      "d": "I have an informed guess about whose house the dark rituals are performed at, the police has to examine the house.",
+      "e": "警察 경찰 police"
+    },
+    {
+      "q": "擦",
+      "s": "찰",
+      "m": "*grate, wipe, scrub",
+      "d": "After he grated off his finger prints it was impossible for the police to guess his identity...",
+      "e": "摩擦 마찰 rubbing, chafing"
+    },
+    {
+      "q": "由",
+      "s": "유",
+      "m": "cause, *wherefore; *shoot, *sprout",
+      "d": "The “wherefore” of this kanji explains the reason or origin of a thing. It does this graphically by depicting a seed in a rice field lesson sending up a single sprout, which is the whole why and wherefore of the seed’s falling in the earth and dying.",
+      "e": "自由 자유 freedom; 理由 이유 reason, grounds, cause"
+    },
+    {
+      "q": "抽",
+      "s": "추",
+      "m": "*pluck, draw out",
+      "d": "Fingers pluck a single sprout.",
+      "e": "抽出 추출 abstraction, extgraction"
+    },
+    {
+      "q": "油",
+      "s": "유",
+      "m": "oil",
+      "d": "The liquid oil that sprouts from an oil field.",
+      "e": "石油 석유 oil, petroleum"
+    },
+    {
+      "q": "袖",
+      "s": "수",
+      "m": "sleeve",
+      "d": "Sleeves are just what sprouts out of the sides of garments.",
+      "e": "袖手傍觀 수수방관 merely watch, stand by idly and watch"
+    },
+    {
+      "q": "宙",
+      "s": "주",
+      "m": "*mid-air; home",
+      "d": "As a part of an experiment spouts are floating freely in the mid-air at the international space station.",
+      "e": "宇宙 우주 the universe, space"
+    },
+    {
+      "q": "届/屆",
+      "s": "계",
+      "m": "*deliver, document",
+      "d": "",
+      "e": "缺勤屆 결근계 report/notice of absence"
+    },
+    {
+      "q": "笛",
+      "s": "적",
+      "m": "*flute, whistle",
+      "d": "Flute is made of bamboo sprout.",
+      "e": "警笛 경적 alarm whistle, warning horn"
+    },
+    {
+      "q": "軸",
+      "s": "축",
+      "m": "axis",
+      "d": "Imagine a car going across a plain at a fast speed. Suddenly a single sprout (the only one to be seen) gets stuck to the car's wheels, causing the car to fly around its axis.",
+      "e": " 地軸 지축 the earth's axis"
+    },
+    {
+      "q": "甲",
+      "s": "갑",
+      "m": "armor; *roots",
+      "d": "The armor of the Teenage Mutant Ninja Turtles.",
+      "e": "同甲 동갑 the same age, a person of the same age"
+    },
+    {
+      "q": "押",
+      "s": "압",
+      "m": "*push",
+      "d": "Fingers pushing seeds downwards so roots grow deep.",
+      "e": "押釘 압정 thumbtack, pushpin"
+    },
+    {
+      "q": "岬",
+      "s": "갑",
+      "m": "cape, promontory, *headland",
+      "d": "A headland is a mountain that is firmly rooted despite being surrounded on 3 sides by water. (Just like your head is a mountain surrounded by air on 3 sides, a headland!)",
+      "e": ""
+    },
+    {
+      "q": "挿/插",
+      "s": "삽",
+      "m": "insert",
+      "d": "A rice farmer's delicate fingers have inserted 1,000 (千) rooted seedlings into his field.",
+      "e": "插入 삽입 insertion, interposition"
+    },
+    {
+      "q": "申",
+      "s": "신",
+      "m": "*speaketh; zodiacal sign of the monkey",
+      "d": "Imagine <i>th</i>omeone who can not pronoun<i>th</i> the 's; and speake<i>th</i> like thi<i>th</i> becau<i>th</i> they have a cane in their mouth.",
+      "e": "申請 신청 proposal, request, challenge; 申告 신고 report, declaration, return"
+    },
+    {
+      "q": "伸",
+      "s": "신",
+      "m": "extend, *expand",
+      "d": "Chuck Norris injected his blood into a <b>monkey</b>. It expanded and is now known as King Kong.",
+      "e": "伸張 신장 extension, expansion"
+    },
+    {
+      "q": "神",
+      "s": "신",
+      "m": "spirit, *gods",
+      "d": "At the <b>alter</b>, you <b>speaketh</b> to the gods.",
+      "e": "鬼神 귀신 a departed soul, a ghost"
+    },
+    {
+      "q": "捜/搜",
+      "s": "수",
+      "m": "*search, investigate",
+      "d": "Oh no! This monkey is using his fingers to search for a banana near my crotch. That ain't no banana.",
+      "e": " 搜査 수사  criminal investigation, search"
+    },
+    {
+      "q": "果",
+      "s": "과",
+      "m": "fruit",
+      "d": "If only Eve had used her brains and not taken the forbidden fruit from the tree.",
+      "e": "沙果 (砂果) 사과 apple; 效果 효과 effect, avail, result"
+    },
+    {
+      "q": "菓",
+      "s": "과",
+      "m": "fruits, nuts, berries; *candy",
+      "d": "Candy is coloured like flowers and flavoured like fruit.",
+      "e": "菓子 과자 confectionery, sweet stuff, cake, candy, pastry; 製菓 제과 confectionery"
+    },
+    {
+      "q": "課",
+      "s": "과",
+      "m": "lesson, *chapter",
+      "d": "The first chapter of the Bible are words telling the story of Adam and Eve and how they ate the forbidden fruit.",
+      "e": "課題 과제 question, problem, subject, theme, homework"
+    },
+    {
+      "q": "裸",
+      "s": "라",
+      "m": "naked",
+      "d": "Here we have Adam and Eve eating of the forbidden fruit and realizing they were naked, and so clothing themselves in a cloak.",
+      "e": " 裸體 나체 a naked body"
+    }
+  ]
+};
