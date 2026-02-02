dbffc7a817b53ff10aa8a99722f0e2b00970c39f
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-02-02 16:47:08 +0300

    .

--- hanja-flash/l31-2.js
+++ hanja-flash/l31-2.js
@@ -4,8 +4,8 @@ const L31_2 = {
     {
       "q": "尹",
       "s": "윤",
-      "m": "govern, oversee; *rake",
-      "d": "A single vertical stroke transforms broom into a rake. When an element comes above the rake, the vertical stroke is shortened, as we have seen before with other similar primitives such a ssheep and cow.",
+      "m": "govern, oversee; *rake, *mop",
+      "d": "A single vertical stroke transforms broom into a rake. When an element comes above the rake, the vertical stroke is shortened, as we have seen before with other similar primitives such a ssheep and cow.<br>The only thing distinguiinges a mop from a rake is the bent handle that does not cut through the top horizontal stroke. It depicts the swish-swash motion of a mop.",
       "e": ""
     },
     {
@@ -44,95 +44,95 @@ const L31_2 = {
       "e": "屑糖 설탕 sugar"
     },
     {
-      "q": "",
+      "q": "隶",
       "s": "",
-      "m": "",
-      "d": "",
+      "m": "subservient, servant; *sieve",
+      "d": "A rake and the grains of rice at the bottom give us a hint of winnowing, which relates clearly to the meaning of a sieve.",
       "e": ""
     },
     {
-      "q": "",
+      "q": "康",
+      "s": "강",
+      "m": "peaceful; healthy, *sane",
+      "d": "People on the cave age became more sane after they started using sieves to avoid eating harmful things with their grains.",
+      "e": "健康 건강 health; 平康 평강 peace"
+    },
+    {
+      "q": "逮",
+      "s": "체",
+      "m": "seize, catch; apprehend",
+      "d": "Think police roadblocks. They act like road sieves, letting good people through but apprehending criminals.",
+      "e": "逮捕 체포 arrest, apprehension"
+    },
+    {
+      "q": "伊",
+      "s": "이",
+      "m": "he, she, this, that; *Italy",
+      "d": "Chuck Norris mops up the mob in Italy.",
+      "e": "伊太利 이태리 Italy"
+    },
+    {
+      "q": "君",
       "s": "",
-      "m": "",
-      "d": "",
+      "m": "monarch, ruler; *old boy",
+      "d": "The Beatles, English old boys, had hairs like mop and a good singing mouth.<br>A monarch holding a baton in his hand and giving orders.",
+      "e": "檀君 단군 Tan-gun; 賢君 현군 wise lord/king"
+    },
+    {
+      "q": "群",
+      "s": "군",
+      "m": "*flock, crowd, mob",
+      "d": "Good ol' boys are like sheep; they tend to flock together.",
+      "e": "群像 군상 large group of people; 君集 군집 crowd, throng"
+    },
+    {
+      "q": "而",
+      "s": "이",
+      "m": "and then; *comb",
+      "d": "If you lose your <b>comb</b>, there is no point just hoping it will turn up. You must carefully <b>comb</b> the whole house looking for it, <b>and then</b> you will find it.",
+      "e": "似而非 사이비 false, mock"
+    },
+    {
+      "q": "耐",
+      "s": "내",
+      "m": "endure, resist, -proof",
+      "d": "My comb is rust-proof, water-proof, and fire-proof. But unfortunately it is not idiot-proof--and I glued it to myself.",
+      "e": "忍耐 인내 patience, endurance"
+    },
+    {
+      "q": "需",
+      "s": "수",
+      "m": "*demand, need",
+      "d": "During the rainy season, combs are in high demand for balding men.",
+      "e": "需要 수요 demand, request"
+    },
+    {
+      "q": "儒",
+      "s": "유",
+      "m": "Confucian scholar",
+      "d": "Chuck Norris says: 'No more Confucian jibba-jabba! I demand you tell me why it's raining combs!'",
+      "e": "儒敎 유교 Confucianism"
+    },
+    {
+      "q": "端",
+      "s": "단",
+      "m": "end, extreme, *edge",
+      "d": "Have you ever seen those shots in movies where the hero is standing at the edge of a sheer mountain face, hair blowing in the wind? It probably wouldn't seem so heroic if you saw him ten minutes earlier, when a crew of stylists was carefully combing his hair so it would fall perfectly.",
+      "e": "惹端 야단 uproar, clamor, tumult, stir; 極端 극단 an extreme, extremity, pole"
+    },
+    {
+      "q": "|_|",
+      "s": "",
+      "m": "shovel",
+      "d": "This enclosure — which embraces its relative primitive from the bottom — is a pictograph of the scoop of a shovel. When room lesson permits, the arms are extended upwards to nearly the same height as the relative element it holds",
       "e": ""
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
-    },
-    {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "両/兩",
+      "s": "량",
+      "m": "both",
+      "d": "Both effective ways to commit suicide, hang your self from the celing with your belt or jump off a mountain.<br>Both hanging yourself from the ceiling with your belt and getting a reverse assembly line (enter not people) of gunmen to mow you down were effective ways to kill yourself.",
+      "e": "兩班 양반 yangban, the nobility, a nobleman; 兩側 양측 both sides"
     },
     {
       "q": "",
