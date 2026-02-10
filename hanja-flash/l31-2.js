9d145b75e1d5b87d528ff3dd14890f93c4bb2aba
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-02-10 10:10:01 +0300

    .

--- hanja-flash/l31-2.js
+++ hanja-flash/l31-2.js
@@ -4,13 +4,15 @@ const L31_2 = {
     {
       "q": "尹",
       "s": "윤",
+	  "k": "다스릴",
       "m": "govern, oversee; *rake, *mop",
       "d": "A single vertical stroke transforms broom into a rake. When an element comes above the rake, the vertical stroke is shortened, as we have seen before with other similar primitives such a ssheep and cow.<br>The only thing distinguiinges a mop from a rake is the bent handle that does not cut through the top horizontal stroke. It depicts the swish-swash motion of a mop.",
       "e": ""
     },
     {
       "q": "争/爭",
-      "s": "",
+      "s": "쟁",
+	  "k": "다툴",
       "m": "contend",
       "d": "An ancient Roman gladiator <b>contending</b> for victory. Specifically he is the type of gladiator known as a “retiarius,” who <b>binds up</b> his opponents with a net then goes in for the kill with his <b>rake</b>-like trident.",
       "e": "競爭 경쟁 competition, contest, rivalry"
@@ -18,6 +20,7 @@ const L31_2 = {
     {
       "q": "浄/淨",
       "s": "정",
+	  "k": "깨끗할 ",
       "m": "clean",
       "d": "Bottled water marketers contend to make their version of tap water sound the cleanest.",
       "e": "洗淨 세정 wash, rinse, clean"
@@ -25,6 +28,7 @@ const L31_2 = {
     {
       "q": "事",
       "s": "사",
+	  "k": "일 ",
       "m": "affair, *matter",
       "d": "This is a pictograph of someone stepping on a rake, and the handle coming up to hit them in the face...no laughing matter.",
       "e": "事件 사건 event, happening, incident, matter"
@@ -32,20 +36,23 @@ const L31_2 = {
     {
       "q": "唐",
       "s": "당",
+	  "k": "당나라",
       "m": "Tang dinasty",
       "d": "Two archeologists are in a cave looking at Chinese artifacts from various eras. One picks up a rake and says, 'What about this – T'ang, do you think?' The second puts the rake in his mouth and answers, 'Yes, it does tang a bit'.",
-      "e": " 唐慌 (唐惶) 당황 perplexed, confused"
+      "e": "唐慌 (唐惶) 당황 perplexed, confused"
     },
     {
       "q": "糖",
       "s": "당,탕",
+	  "k": "엿",
       "m": "sugar",
       "d": "The T'ang are the ones who began using sugar in China - of course, the first thing they tried it on was rice.",
       "e": "屑糖 설탕 sugar"
     },
     {
       "q": "隶",
-      "s": "",
+      "s": "이",
+	  "k": "미칠",
       "m": "subservient, servant; *sieve",
       "d": "A rake and the grains of rice at the bottom give us a hint of winnowing, which relates clearly to the meaning of a sieve.",
       "e": ""
@@ -53,6 +60,7 @@ const L31_2 = {
     {
       "q": "康",
       "s": "강",
+	  "k": "편안",
       "m": "peaceful; healthy, *sane",
       "d": "People on the cave age became more sane after they started using sieves to avoid eating harmful things with their grains.",
       "e": "健康 건강 health; 平康 평강 peace"
@@ -60,6 +68,7 @@ const L31_2 = {
     {
       "q": "逮",
       "s": "체",
+	  "k": "잡을",
       "m": "seize, catch; apprehend",
       "d": "Think police roadblocks. They act like road sieves, letting good people through but apprehending criminals.",
       "e": "逮捕 체포 arrest, apprehension"
@@ -67,13 +76,15 @@ const L31_2 = {
     {
       "q": "伊",
       "s": "이",
+	  "k": "저",
       "m": "he, she, this, that; *Italy",
       "d": "Chuck Norris mops up the mob in Italy.",
       "e": "伊太利 이태리 Italy"
     },
     {
       "q": "君",
-      "s": "",
+      "s": "군",
+	  "k": "임금",
       "m": "monarch, ruler; *old boy",
       "d": "The Beatles, English old boys, had hairs like mop and a good singing mouth.<br>A monarch holding a baton in his hand and giving orders.",
       "e": "檀君 단군 Tan-gun; 賢君 현군 wise lord/king"
@@ -81,6 +92,7 @@ const L31_2 = {
     {
       "q": "群",
       "s": "군",
+	  "k": "무리 ",
       "m": "*flock, crowd, mob",
       "d": "Good ol' boys are like sheep; they tend to flock together.",
       "e": "群像 군상 large group of people; 君集 군집 crowd, throng"
@@ -88,6 +100,7 @@ const L31_2 = {
     {
       "q": "而",
       "s": "이",
+	  "k": "말 이을",
       "m": "and then; *comb",
       "d": "If you lose your <b>comb</b>, there is no point just hoping it will turn up. You must carefully <b>comb</b> the whole house looking for it, <b>and then</b> you will find it.",
       "e": "似而非 사이비 false, mock"
@@ -95,6 +108,7 @@ const L31_2 = {
     {
       "q": "耐",
       "s": "내",
+	  "k": "견딜",
       "m": "endure, resist, -proof",
       "d": "My comb is rust-proof, water-proof, and fire-proof. But unfortunately it is not idiot-proof--and I glued it to myself.",
       "e": "忍耐 인내 patience, endurance"
@@ -102,6 +116,7 @@ const L31_2 = {
     {
       "q": "需",
       "s": "수",
+	  "k": "기다릴",
       "m": "*demand, need",
       "d": "During the rainy season, combs are in high demand for balding men.",
       "e": "需要 수요 demand, request"
@@ -109,6 +124,7 @@ const L31_2 = {
     {
       "q": "儒",
       "s": "유",
+	  "k": "선비",
       "m": "Confucian scholar",
       "d": "Chuck Norris says: 'No more Confucian jibba-jabba! I demand you tell me why it's raining combs!'",
       "e": "儒敎 유교 Confucianism"
@@ -116,6 +132,7 @@ const L31_2 = {
     {
       "q": "端",
       "s": "단",
+	  "k": "끝",
       "m": "end, extreme, *edge",
       "d": "Have you ever seen those shots in movies where the hero is standing at the edge of a sheer mountain face, hair blowing in the wind? It probably wouldn't seem so heroic if you saw him ten minutes earlier, when a crew of stylists was carefully combing his hair so it would fall perfectly.",
       "e": "惹端 야단 uproar, clamor, tumult, stir; 極端 극단 an extreme, extremity, pole"
@@ -130,51 +147,122 @@ const L31_2 = {
     {
       "q": "両/兩",
       "s": "량",
+	  "k": "두",
       "m": "both",
       "d": "Both effective ways to commit suicide, hang your self from the celing with your belt or jump off a mountain.<br>Both hanging yourself from the ceiling with your belt and getting a reverse assembly line (enter not people) of gunmen to mow you down were effective ways to kill yourself.",
       "e": "兩班 양반 yangban, the nobility, a nobleman; 兩側 양측 both sides"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "満/滿",
+      "s": "만",
+      "k": "찰",
+      "m": "full",
+      "d": "I hate going on dates where a girl says 'I'll have a salad and a water' and then insists that they both make her full!",
+      "e": "滿足 만족 satisfaction"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "画/畫",
+      "s": "화",
+      "k": "그림",
+      "m": "drawing; *brush-stroke",
+      "d": "To paint a kanji in your ceiling with only natural materials, you use a sprout as a brush and shovel some dirt to use as paint for your strokes.<br>DaVinci is drawing his anatomy pictures again - he takes out his writing brush and draws the brains lying on the floor.",
+      "e": "畫家 화가 painter, artist"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "歯/齒",
+      "s": "치",
+      "k": "이",
+      "m": "tooth",
+      "d": "My dentist used his foot and a shovel to dislodge the rice stuck between my teeth.<br>Old people only have four teeth: two up and two down.",
+      "e": "齒牙 치아 teeth; 齒科 치과 dentistry"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "曹",
+      "s": "조",
+      "k": "무리",
+      "m": "ministry officials; *cadet",
+      "d": "A space cadet eager to make one bend around the sun. (a dangerous maneuver).",
+      "e": "法曹界 법조계 legal/judicial circles/world"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "遭",
+      "s": "조",
+      "k": "만날",
+      "m": "encounter",
+      "d": "Close encounters of the third kind. The space cadet encounters aliens on the road.",
+      "e": "遭遇 조우 encounter"
     },
     {
-      "q": "",
-      "s": "",
-      "m": "",
-      "d": "",
-      "e": ""
+      "q": "漕",
+      "s": "조",
+      "k": "배로 실어 나를",
+      "m": "transport by water; *rowing",
+      "d": "Water cadets (a freshwater branch of the sea scouts), love to go rowing. Stroke! Stroke!",
+      "e": "漕艇 조정 rowing, boating"
     },
+    {
+      "q": "槽",
+      "s": "조",
+      "k": "구유",
+      "m": "manger; *vat",
+      "d": "Cadets have to bathe in wooden vats.",
+      "e": "浴槽 욕조 bathtub"
+    },
+    {
+      "q": "斗",
+      "s": "두",
+      "k": "말",
+      "m": "measuring cup; *Big Dipper",
+      "d": "The Big Dipper here is of course the constellation of Ursa Major, of which this kanji is a sort of pictographic representation.",
+      "e": "斗頓 두둔 back, shield, support"
+    },
+    {
+      "q": "料",
+      "s": "료,요",
+      "k": "헤아릴",
+      "m": "consider; materials; *fee",
+      "d": "Your entry fee (doubling as your class materials) for the cooking class is a large measuring cup of rice.",
+      "e": "料理 요리 cooking; 無料 무료 free of charge; 材料 재료 material"
+    },
+    {
+      "q": "科",
+      "s": "과",
+      "k": "과목",
+      "m": "department",
+      "d": "University departments study things like wheat (agriculture) and the big dipper (physics/astronomy), and exact measuring cups (engineering).",
+      "e": "科目 과목 subject, course, curriculum"
+    },
+    {
+      "q": "図/圖",
+      "s": "도",
+      "k": "그림",
+      "m": "picture, *map",
+      "d": "A constellation map.<br>An Archaeology professor is arguing with his colleague about a dig site: 'You'll never find anything at this site - if you do I'll eat my <b>hat</b>!' Later the professor digs up a primitive stone <b>wheel</b>. Well true to his word his colleague stuffs his <b>top hat</b> into his <b>mouth</b> and eats it, and the professor decides to take a <b>picture</b> of the occasion.",
+      "e": "地圖 지도 map; 圖書 도서 books, publications, literature"
+    },
+    {
+      "q": "用",
+      "s": "용",
+      "k": "쓸",
+      "m": "utilize; *screwdriver",
+      "d": "A pictograph of someone who knows how to utilize a walking stick very well, like if it was a part of his body.<br>*As a primitive element, we shall substitute the image of a <b>screwdriver</b>, perhaps the most utilized of all tools around the house.",
+      "e": "使用 사용 use, employment, application"
+    },
+    {
+      "q": "庸",
+      "s": "용",
+      "k": "쓸",
+      "m": "usual, mediocre; *comfortable",
+      "d": "To live a common/comfortable life, single men need only a cave for living, a rake for cleaning and a screwdriver for fixing.",
+      "e": "凡庸 범용 mediocrity, banality; 中庸 중용 the (golder) mean, the middle path, moderation"
+    },
+    {
+      "q": "備",
+      "s": "비",
+      "k": "갖출",
+      "m": "prepare, *equip",
+      "d": "<b>Chuck Norris</b> always <b>equips</b> himself with a bunch of <b>flowers</b>, to use as parachutes when he throws over a <b>cliff</b>, and a <b>screwdriver</b> to climb back to the top.",
+      "e": "準備 준비 prepare"
+    }
   ]
 };
