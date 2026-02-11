abe2be0d9206daaec08f30cd001573f3fb3fa89e
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-02-11 09:34:41 +0300

    .

---
+++ hanja-flash/l32.js
@@ -0,0 +1,293 @@
+const L32 = {
+  "name" : "L32",
+  "cards" : [
+    {
+      "q": "龷",
+      "s": "",
+      "k": "",
+      "m": "salad",
+      "d": "The element for flowers joins with the long horizontal stroke beneath it to create the picture of a bowl of salad.",
+      "e": ""
+    },
+    {
+      "q": "昔",
+      "s": "석",
+      "k": "예",
+      "m": "one upon a time",
+      "d": "Once upon a time, the children were forced to eat their salad every day.",
+      "e": ""
+    },
+    {
+      "q": "錯",
+      "s": "착",
+      "k": "어긋날",
+      "m": "error, blunder, *confused",
+      "d": "<b>A long time ago</b>, people of the world got <b>confused</b> because there were so many kinds of <b>metal</b> coins, today we only have the euro.",
+      "e": "錯誤 착오 mistake, error; 倒錯 도착 perversion, inversion"
+    },
+    {
+      "q": "借",
+      "s": "차",
+      "k": "빌릴",
+      "m": "borrow",
+      "d": "Once upon a time, Chuck Norris borrowed something from me. Do you think I'm going to ask for it back? No way!",
+      "e": "借用 차용 borrowing, loan"
+    },
+    {
+      "q": "惜",
+      "s": "석",
+      "k": "아낄",
+      "m": "pity",
+      "d": "It's a pity that his state of mind isn't what it was once upon a time. Alzheimers can be very tragic.",
+      "e": "惜別 석별 unwillingness to part, to leave"
+    },
+    {
+      "q": "措",
+      "s": "조",
+      "k": "둘",
+      "m": "place, *set aside",
+      "d": "'Once upon a time...' - Oh hell, you're just too old for this. You close your fairy tale book and set it aside, pushing it away with your fingers.",
+      "e": "措置 조치 measure, step, arrangement"
+    },
+    {
+      "q": "散",
+      "s": "산,
+      "k": "흩을",
+      "m": "scatter",
+      "d": "The taskmaster is dissatisfied with the meal she's served, and scatters the salad and meat across the floor for the servants to clean up.",
+      "e": "散步 산보 walk, stroll"
+    },
+    {
+      "q": "廿",
+      "s": "입",
+      "k": "스물",
+      "m": "twenty",
+      "d": "The two tens joined at the bottom by a short line is actually the old character for twenty.",
+      "e": ""
+    },
+    {
+      "q": " 广+廿",
+      "s": "",
+      "k": "",
+      "m": "*caverns",
+      "d": "The primitive for caverns differs from that for cave by the presence of the twenty, suggesting a maze of underground caves.",
+      "e": ""
+    },
+    {
+      "q": "庶",
+      "s": "서",
+      "k": "여러",
+      "m": "numerous; *commoner",
+      "d": "Commoners have to huddle in caverns around a campfire, while higher class people get heating and central air conditioning in a hotel.",
+      "e": "庶民 서민 commoner, common people"
+    },
+    {
+      "q": "遮",
+      "s": "차",
+      "k": "가릴",
+      "m": "cover, shield; *intercept",
+      "d": "The commoners are revolting! We must intercept them on the road before they reach the castle!",
+      "e": "遮斷 차단 interception, isolation; 遮日 차일 sunshade, tent"
+    },
+    {
+      "q": "席",
+      "s": "석",
+      "k": "자리",
+      "m": "seat",
+      "d": "If you want to take a seat in the caverns, they're often pretty damp - best to lay down a towel first before getting a wet ass.",
+      "e": "座席 좌석 seat; 缺席 결석 absence, nonattendance"
+    },
+    {
+      "q": "度",
+      "s": "도",
+      "k": "법도",
+      "m": "degrees",
+      "d": "The cave is twenty degrees cooler than the outdoors. Twenty male crotches experience shrinkage.",
+      "e": "程度 정도 degree, extent; 態度 태도 attitude, bearing"
+    },
+    {
+      "q": "渡",
+      "s": "도",
+      "k": "건널",
+      "m": "cross, *transit",
+      "d": "his particular form of transit is a ferry (one of the actual meanings of this character). Naturally it can only cross the water if it's not frozen, so the temperature must be above 0 degrees.",
+      "e": "讓渡 양도 transfer, cession"
+    },
+    {
+      "q": "卉",
+      "s": "훼",
+      "k": "풀",
+      "m": "general term for plants; *haystack",
+      "d": "The three needles stacked up give us a haystack (in which it may be harder to find the hay than the needles).",
+      "e": ""
+    },
+    {
+      "q": "奔",
+      "s": "분",
+      "k": "달릴",
+      "m": "run fast, *bustle",
+      "d": "Hustle and bustle = a big St. Bernard dog frisking about in a haystack, knocking it down and spreading the hay about, after the field hands just stacked it up.",
+      "e": "奔走하다 분주하다 busy; 狂奔하다 광분하다 rush about, run madly about"
+    },
+    {
+      "q": "噴",
+      "s": "분",
+      "k": "뿜을",
+      "m": "erupt",
+      "d": "History of Pompei: An unexpect eruption and from the mouths of the volcano come lava that threatens the farmers collecting haystacks in the fields and the fisherman catching shells on the coast.",
+      "e": " 噴水 분수 fountain, jet of water"
+    },
+    {
+      "q": "墳",
+      "s": "분",
+      "k": "무덤",
+      "m": "tomb",
+      "d": "After the eruption all is a tomb. The soil has covered the remains of the farmers gathering haystacks and of the fisherman collecting shells.",
+      "e": "墳墓 분묘 tomb, grave"
+    },
+    {
+      "q": "憤",
+      "s": "분",
+      "k": "분할",
+      "m": "resent, hate; *aroused",
+      "d": "Someone's wild state of mind became aroused and indignant upon finding out that the money he'd hidden under the haystack is gone.",
+      "e": "憤怒 분노 anger, rage, fury; 憤하다 분다 vexatious, vexing"
+    },
+    {
+      "q": "尭",
+      "s": "요",
+      "k": "높을",
+      "m": "a legendary ancient emperor-sage; *strawman",
+      "d": "The two human legs added to the haystack (with the horizontal stroke to keep the two parts distinct from one another and avoid an ugly tangle) give us a straw man.",
+      "e": ""
+    },
+    {
+      "q": "焼/燒",
+      "s": "소",
+      "k": "불사를",
+      "m": "bake",
+      "d": "Ah, there's no better way to bake a pizza than an oven fueled by a burning strawman. And if you listen carefully, there are stifled strawman screams coming from the kitchen.",
+      "e": "燒酒 소주 soju; 燃燒 연소 burning, combustion"
+    },
+    {
+      "q": "暁/曉",
+      "s": "효",
+      "k": "새벽",
+      "m": "daybreak",
+      "d": "The first person to see the sun at daybreak is of course the straw man who stands outside all day.",
+      "e": "通曉 통효 conversance, mastery"
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+    {
+      "q": "",
+      "s": "",
+      "k": "",
+      "m": "",
+      "d": "",
+      "e": ""
+    },
+  ]
+};
