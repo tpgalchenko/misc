c3134c564dd5f1dfde3e465c870bea876c0a76a4
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-03-23 13:08:02 +0300

    .

---
+++ hanja-flash/l40-1.js
@@ -0,0 +1,269 @@
+const L40_1 = {
+  "name" : "L40-1",
+  "cards" : [
+    {
+      "q": "龶",
+      "s": "",
+      "k": "",
+      "m": "grow up",
+      "d": "As the plant grows up it sprouts leaves and a stalk, which are depicted here over a single horizontal stroke for the soil. Think of something (its relative primitive) growing up in a flash to many times its normal size, much like little Alice in Wonderland, who grew up so fast she was soon larger than the room in which she was sitting.",
+      "e": ""
+    },
+    {
+      "q": "毒",
+      "s": "독",
+      "k": "독",
+      "m": "poison",
+      "d": "To those who grow up lactose intolerant, mother's milk is a poison.",
+      "e": "中毒 중독 poisoning, intoxication"
+    },
+    {
+      "q": "素",
+      "s": "소",
+      "k": "본디",
+      "m": "plain, *elementary",
+      "d": "This is a pictogram of string theory. Strings are the most elementary particles of matter, which then grow up to become matter itself.",
+      "e": "要素 요소 element, factor; 平素 평소 everyday, usual"
+    },
+    {
+      "q": "麦/麥",
+      "s": "맥",
+      "k": "보리",
+      "m": "barley",
+      "d": "If you eat lots of barley, you'll grow up to have strong walking legs.<br>If you eat lots of barley, the thing between your walking legs will take a lot longer to come (來) ",
+      "e": "麥酒 맥주 beer"
+    },
+    {
+      "q": "青/靑",
+      "s": "청",
+      "k": "푸를",
+      "m": "*blue, green",
+      "d": "Grow up on the moon and you’ll be blue from lack of oxygen.<br>When Alice was growing up, she almost chocked on yen 円 coin. Thankfully she only fainted and turned pale old blue.",
+      "e": "靑少年 청소년 teenagers, youth"
+    },
+    {
+      "q": "精",
+      "s": "정",
+      "k": "정할",
+      "m": "*refined, essence, spirit, semen",
+      "d": "Very refined people in the upper classes have skin as pale as polished rice and blue blood.",
+      "e": "精神 정신 mind, spirit, soul; 精誠 정성 sincerity, devotion; 精製 정제 refinement"
+    },
+    {
+      "q": "請",
+      "s": "청",
+      "k": "청할",
+      "m": "*solicit, ask, request",
+      "d": "Solicitors often talk until they're blue.",
+      "e": "申請 신청 proposal, offer, request; 要請 요청 request, demand"
+    },
+    {
+      "q": "情",
+      "s": "정",
+      "k": "뜻",
+      "m": "feelings",
+      "d": "Feeling blue is a state of mind where you're dominated by your feelings.",
+      "e": "情報 정보 information; 感情 감정 feeling(s), emotion"
+    },
+    {
+      "q": "晴",
+      "s": "청",
+      "k": "갤",
+      "m": "clear up (weather)",
+      "d": "Sun and blue sky! Yep, the weather's cleared up!",
+      "e": "快晴 쾌청 fine weather"
+    },
+    {
+      "q": "清/淸",
+      "s": "청",
+      "k": "맑을",
+      "m": "*pure, clear",
+      "d": "Pure sea water is always a radiant blue.",
+      "e": "淸掃 청소 cleaning, sweeping"
+    },
+    {
+      "q": "静/靜",
+      "s": "정",
+      "k": "고요할",
+      "m": "quiet",
+      "d": "When someone you know who is normally talkative goes <b>quiet</b>, it's usually a sign that they are <b>contending</b> with the <b>blues</b>.",
+      "e": "安靜 안정 rest, quiet, repose"
+    },
+    {
+      "q": "責",
+      "s": "책",
+      "k": "꾸짖을",
+      "m": "*blame; one's responsibility, duty",
+      "d": "As one grows up, one typically starts to blame all of their problems on money.",
+      "e": "責任 책임 responsibility, duty; 責望 책망 blame, charge, reproach"
+    },
+    {
+      "q": "績",
+      "s": "적",
+      "k": "길쌈할",
+      "m": "exploits",
+      "d": "Spiderman's exploits have earned him fame and blame.",
+      "e": "成績 성적 performance, result, merit; 功績 공적 exploit, achievement"
+    },
+    {
+      "q": "積",
+      "s": "적",
+      "k": "쌓을",
+      "m": "accumulate, amass; *volume",
+      "d": "The <b>volume</b> of the <b>wheat</b> crop is way down this year! <b>Blame</b> the weather!",
+      "e": "面積 면적 area, square measure; 積極 적극 positive(ly), active(ly); 積金 적금 installment savings, deposit"
+    },
+    {
+      "q": "債",
+      "s": "채",
+      "k": "빚",
+      "m": "debt, loan, *bond",
+      "d": "Chuck Norris needs nor wallet for the <b>bonds</b>, nor <b>bonds</b> themselves, since he gets all debts paid personnaly from the <b>responsible</b> persons.",
+      "e": "私債 사채 corporate/company bond; 公債 공채 public loan/debt"
+    },
+    {
+      "q": "漬",
+      "s": "지",
+      "k": "담글",
+      "m": "soak, *pickling",
+      "d": "When the pickling doesn't come out right, generally the water is to blame.",
+      "e": "浸漬/沈漬 침지 digestion"
+    },
+    {
+      "q": "表",
+      "s": "표",
+      "k": "겉",
+      "m": "show, express; *surface",
+      "d": "As girls <b>grow up</b>, something slowly <b>shows up</b> from under their <b>scarf</b>.",
+      "e": "表示 표시 indication, expression"
+    },
+    {
+      "q": "俵",
+      "s": "표",
+      "k": "나누어 줄",
+      "m": "divide, distribute; *bag",
+      "d": "If you show Chuck Norris' face on the surface of any BAG, it will punch and kick any pickpocket who would dare approaching it. The best antitheft device ever invented for your bag!",
+      "e": ""
+    },
+    {
+      "q": "潔",
+      "s": "결",
+      "k": "깨끗할",
+      "m": "clean, pure, *undefiled",
+      "d": "To ensure she remained <b>undefiled</b>, a young girl <b>growing up</b> in a rough area always kept a <b>dagger</b> concealed under her <b>threads</b>, even when she went into the <b>water</b>.",
+      "e": "淸潔 청결 cleanliness, neatness"
+    },
+    {
+      "q": "契",
+      "s": "계",
+      "k": "맺을",
+      "m": "bond, contract, *pledge",
+      "d": "As of the contract a hostage grows up guarded by daggers and St. Bernard dogs.",
+      "e": "契約 계약 contract, agreement; 契機 계기 chance, opportunity, occasion"
+    },
+    {
+      "q": "喫",
+      "s": "끽",
+      "k": "먹을",
+      "m": "eat, drink, *consume",
+      "d": "I make my mouth pledge to consume less.",
+      "e": "滿喫하다 만끽하다  have (eat/drink) one's fill; 喫煙 끽연 smoking (tobacco)"
+    },
+    {
+      "q": "害",
+      "s": "해",
+      "k": "해할",
+      "m": "harm",
+      "d": "With kids growing up in your house, you have to keep them from harm of putting anything in their mouth, paint chips, bleach, knvies, etc.",
+      "e": "害하다/치다 해-하다/치다 injure, harm, damage"
+    },
+    {
+      "q": "轄",
+      "s": "할",
+      "k": "다스릴",
+      "m": "control",
+      "d": "If you lose control of your car, you may come to great harm.",
+      "e": "管轄 관할 jurisdiction, control; 直轄 직할 direct control/jurisdiction"
+    },
+    {
+      "q": "割",
+      "s": "할",
+      "k": "벨",
+      "m": "cut, divide, *proportion",
+      "d": "The harm is proportional to the size of the saber.",
+      "e": "役割 역할 role, duty, office, function; 割引 할인 discount, reduction"
+    },
+    {
+      "q": "憲",
+      "s": "헌",
+      "k": "법",
+      "m": "constitution (law)",
+      "d": "The constitution is important. At least my parents thought so. In the house where I grew up, it was framed on the wall. Under the watchful (sideways) eye of my mother, we had to study it until we knew it by heart.",
+      "e": "憲法 헌법 constituion (law)"
+    },
+    {
+      "q": "生",
+      "s": "생",
+      "k": "날",
+      "m": "*life, birth",
+      "d": "A single drop added to the element for <b>grow up</b> gives us the character for life.<br><b>*</b> As a primitive, we may think of a microscopic cell, that miraculous unit that grows up to become a living being.",
+      "e": "生活 생활 life, existence; 苦生 고생 hard life, trouble(s), hardship(s)"
+    },
+    {
+      "q": "星",
+      "s": "성",
+      "k": "별",
+      "m": "star, planet",
+      "d": "The sun is the nearest star to the only planet with life - Earth.",
+      "e": "彗星 혜성 comet; 衞星 위성 satellite"
+    },
+    {
+      "q": "醒",
+      "s": "성",
+      "k": "깰",
+      "m": "awakening",
+      "d": "After spending too much time with the whiskey bottle 酉, one often finds himself awakening under the stars 星.",
+      "e": "覺醒 각성 awakening; disillusion"
+    },
+    {
+      "q": "姓",
+      "s": "성",
+      "k": "성씨",
+      "m": "surname, clan",
+      "d": "When women marry, they adopt the surname of their husband, signifiying the start of a new life together.",
+      "e": "百姓 백성 the (common) people, the nation; 姓(氏) 성(씨) family name (respectfully)"
+    },
+    {
+      "q": "性",
+      "s": "성",
+      "k": "성품",
+      "m": "nature, *sex",
+      "d": "Your sex/gender shapes your state of mind all through life.",
+      "e": "性質 성질 one's nature, temperament; property, nature; 女性 여성 woman, feminine gender"
+    },
+    {
+      "q": "牲",
+      "s": "생",
+      "k": "희생",
+      "m": "animal sacrifice",
+      "d": "Animal sacrifice = taking the life of a cow.",
+      "e": "犧牲 희생 sacrifice"
+    },
+    {
+      "q": "産/產",
+      "s": "산",
+      "k": "낳을",
+      "m": "give birth, *products",
+      "d": "If you were to stand up all the products you have accumulated over the course of your entire life on top of one another, the result would probably be as tall as a cliff.",
+      "e": "遺產 유산 inheritance, legacy; 生產 생산 production"
+    },
+    {
+      "q": "隆",
+      "s": "륭/융",
+      "k": "높을",
+      "m": "prosperous, abundant; *hump",
+      "d": "You are struggling to the top across the humps, but you suddenly see the pinnacle and you feel new life in your walking legs.",
+      "e": "隆起 융기 protuberance, upheaval; 隆盛 융성 prosperity, flourishing"
+    }
+  ]
+};
---
+++ hanja-flash/l40-2.js
@@ -0,0 +1,285 @@
+const L40_2 = {
+  "name" : "L40-2",
+  "cards" : [
+    {
+      "q": "丰",
+      "s": "봉,풍",
+      "k": "예쁠",
+      "m": "abundant, lush, bountiful; *bushes",
+      "d": "The element itself differs from that for grow up only in the extension of the single vertical stroke beneath the final horizontal stroke and in the order of writing",
+      "e": ""
+    },
+    {
+      "q": "峰/峯",
+      "s": "봉",
+      "k": "봉우리",
+      "m": "summit",
+      "d": "At the summit of the mountain I could finally walk behind some bushes and pee.",
+      "e": "高峰/高峯 고봉 high/lofty peak"
+    },
+    {
+      "q": "蜂",
+      "s": "봉",
+      "k": "벌",
+      "m": "bee",
+      "d": "I got stung by an insect walking through the bushes, I think it was a bee.",
+      "e": ""
+    },
+    {
+      "q": "縫",
+      "s": "봉",
+      "k": "꿰맬",
+      "m": "*sew, mend",
+      "d": "Good thing I brought some <b>thread</b>, cause I need to <b>sew</b> my pants that I tore up <b>walking</b> through the <b>bushes</b> before we hit the <b>road</b> to go back down the mountain.",
+      "e": "縫合 봉합 suture, seam; 裁縫 재봉 sewing, needlework"
+    },
+    {
+      "q": "拝/拜",
+      "s": "배",
+      "k": "절",
+      "m": "*worship, do obeisance, bow",
+      "d": "I put my <b>fingers</b> together in <b>worship</b> of the <b>bush</b> that hangs from the <b>ceiling</b> - that wonderful mistletoe which has gotten me kissed by many a girl.",
+      "e": "歲拜 세배 formal bow of respect to one's elders on New Year' Day; 禮拜 예배 worship, church service"
+    },
+    {
+      "q": "寿/壽",
+      "s": "수",
+      "k": "목숨",
+      "m": "*longevity, old age, lifespan",
+      "d": "In an attempt to increase a <b>bush</b>'s <b>longevity</b>, an incompitent gardener <b>glues</b> all the leaves back on that have fallen off. Maybe he should have tried watering it instead.",
+      "e": "長壽 장수 long life, longevity"
+    },
+    {
+      "q": "鋳/鑄",
+      "s": "주",
+      "k": "불릴",
+      "m": "casting, melt, coin",
+      "d": "Casting things in metal improves their longevity.",
+      "e": "鑄貨/鑄錢 주화/주전 coin"
+    },
+    {
+      "q": "耒",
+      "s": "뢰/뇌",
+      "k": "가래",
+      "m": "handle of plow, plow; *Christmas tree",
+      "d": "The addition of the final two strokes to the element for bushes gives the sense of a tree that is also a bush. Hence, the Christmas tree.",
+      "e": ""
+    },
+    {
+      "q": "籍",
+      "s": "적",
+      "k": "문서",
+      "m": "enroll",
+      "d": "Once upon a time, in order to enroll in a Christian school you had to answer a series of questions to ensure that you were really Christian. For example, the principal might show you a picture of a Christmas tree and a picture of some bamboo and ask you which one is most suitable for decoration at Christmas time.",
+      "e": "符籍 부적 amulet, charm; 書籍 서적 book"
+    },
+    {
+      "q": "𡗗",
+      "s": "",
+      "k": "",
+      "m": "*bonsai",
+      "d": "The element for bushes has an extra stroke added (drawn from the point where the second and fifth strokes intersect when it “encloses” something beneath, otherwise from the point where the fourth and ³fth strokes intersect) to give the image of the crutches Japanese gardeners use to hold up a tree that is being bent into shape. From there it is but a short leap to the small bonsai plants that imitate this art in miniature.",
+      "e": ""
+    },
+    {
+      "q": "春",
+      "s": "춘",
+      "k": "봄",
+      "m": "springtime",
+      "d": "In the springtime bonsai get plenty of sun.",
+      "e": "靑春 청춘 one's youth, the springtime of life"
+    },
+    {
+      "q": "椿",
+      "s": "춘",
+      "k": "참죽나무",
+      "m": "*camelia; father; a plant with white flowers",
+      "d": "The tree of spring. Fact: Camellia are highly valued in Japan and elsewhere for their very early flowering, often among the first flowers to appear in the late winter [early spring?].",
+      "e": "椿府丈 춘부장 your honoured father"
+    },
+    {
+      "q": "泰",
+      "s": "태",
+      "k": "클",
+      "m": "great, exalted, big; *peaceful",
+      "d": "If you look into Japanese gardens they seem so peaceful - with their small bonsais and the white stones looking like rice grains.",
+      "e": "泰山 태산 a great/big/high mountain; 泰然 태연 calm, composed, imperturbable; 泰國 태국 Thailand"
+    },
+    {
+      "q": "奏",
+      "s": "주",
+      "k": "아뢸",
+      "m": "memorialize emperor; *play music",
+      "d": "When you play music to them, your bonsai will grow like they are in heaven.",
+      "e": "演奏 연주 (musical) performance, recital"
+    },
+    {
+      "q": "実/實",
+      "s": "실",
+      "k": "열매",
+      "m": "reality",
+      "d": "Bonsais are grown in a house, away from the harsh reality of the real world where they wouldn't survive.",
+      "e": "誠實 성실 sincerity, honesty, integrity; 實踐 실천 practice; 事實 사실 fact"
+    },
+    {
+      "q": "𠀆",
+      "s": "",
+      "k": "",
+      "m": "*cornstalk",
+      "d": "The element for bushes extended the vertical stroke beneath the final horizontal stroke; the cornstalk omits that final stroke altogether, leaving only the stalk and the leaves bursting forth on all sides.",
+      "e": ""
+    },
+    {
+      "q": "奉",
+      "s": "봉",
+      "k": "받들",
+      "m": "offer, *dedicate",
+      "d": "To join the temple, you have to <b>dedicate</b> yourself to raising <b>bonsai</b> for enlightenment, and <b>cornstalks</b> for sustenance.",
+      "e": "奉仕 봉사 service, attendance"
+    },
+    {
+      "q": "俸",
+      "s": "봉",
+      "k": "녹",
+      "m": "*stipend, wages, salary",
+      "d": "Chuck Norris receives a stipend to dedicate himself to the Bowflex. Oh, how the mighty have fallen.",
+      "e": "俸給 봉급 salary, pay, wages"
+    },
+    {
+      "q": "棒",
+      "s": "봉",
+      "k": "막대",
+      "m": "*rod, stick, club",
+      "d": "The wood from this tree is dedicated to making rods to flog naughty kids.",
+      "e": "棍棒 곤봉 club, cudgel; 綿棒 면봉 cotton swab (on a stick)"
+    },
+    {
+      "q": "堇",
+      "s": "근",
+      "k": "진흙",
+      "m": "*cabbage",
+      "d": "The flower, the mouth, and the element for grow up combine here to create the primitive for cabbage.",
+      "e": ""
+    },
+    {
+      "q": "謹",
+      "s": "근",
+      "k": "삼갈",
+      "m": "*discreet, prudent, cautious",
+      "d": "Muttering words of gratitude for a wonderful meal, I discreetly slipped my cabbage under the table for the dog.",
+      "e": "謹審 근심 anxiety, concertn, solicitude; 謹愼 근신 good behaviour/conduct, prudence, discretion"
+    },
+    {
+      "q": "僅",
+      "s": "근",
+      "k": "겨우",
+      "m": "only, merely, just; *trifle",
+      "d": "For Chuck Norris swallowing a whole cabbage is no more than a trifle.",
+      "e": "僅少하다 근소하다 few, little, trifling, scanty; 僅僅이 근근이 barely, hardly, just, only"
+    },
+    {
+      "q": "勤",
+      "s": "근",
+      "k": "부지런할",
+      "m": "diligence",
+      "d": "Growing cabbages takes muscle and diligence.",
+      "e": "勤務 근무 service, duty, work; 出勤 출근 attendance, going to work; being on the job"
+    },
+    {
+      "q": "廿+口+夫",
+      "s": "",
+      "k": "",
+      "m": "*scarecrow",
+      "d": "By twisting the final two strokes of our cabbage into a pair of legs, we get a scarecrow with a cabbage for a head.",
+      "e": ""
+    },
+    {
+      "q": "漢",
+      "s": "한",
+      "k": "한수/한나라",
+      "m": "Sino-, chinese",
+      "d": "Kanji came from across the water, on the Sino- side of things.",
+      "e": "漢字 한자 Chinese character"
+    },
+    {
+      "q": "嘆",
+      "s": "탄",
+      "k": "탄식할",
+      "m": "sign",
+      "d": "A sigh escapes the mouth of the scarecrow, who laments: '...if I only had a brain, I could learn kanji'.",
+      "e": "嘆息/歎息 탄식 sigh, lamentation"
+    },
+    {
+      "q": "難",
+      "s": "난",
+      "k": "어려울",
+      "m": "difficult",
+      "d": "It is difficult for a scarecrow to scare a turkey or else they would've been called scareturkeys.",
+      "e": "困難 곤란 difficulty, hardship, trouble; 非難 비난 (adverse) criticism, blame, reproach"
+    },
+    {
+      "q": "卄+干",
+      "s": "",
+      "k": "",
+      "m": "silage",
+      "d": "It is a pictograph of all sorts of plants and grasses thrown together to make silage. The vertical stroke is drawn here with a broken line to indicate that it will always double up with another primitive element’s vertical stroke.",
+      "e": ""
+    },
+    {
+      "q": "華",
+      "s": "화",
+      "k": "빛날",
+      "m": "splendor",
+      "d": "I could not find the needle in the heap of silage but was surprised to find a flower of great splendor growing there.",
+      "e": "昇華 승화 sublimation; 榮華 영화 prosperity, opulence"
+    },
+    {
+      "q": "垂",
+      "s": "수",
+      "k": "드리울",
+      "m": "*droop, hang, suspend",
+      "d": "For an old man with a walking stick, even a drop of silage to carry on his back will make him droop low down to the floor.",
+      "e": "垂直 수직 perpendicularity, verticality; 懸垂幕 현수막 drop curtain, banner, placard"
+    },
+    {
+      "q": "唾",
+      "s": "타",
+      "k": "침",
+      "m": "saliva, spit",
+      "d": "When some people are sleeping, their mouths droop open, and saliva drips out.",
+      "e": "唾棄하다 타기하다 detest, hate, abominate, abhor; 唾液 타액 spit, saliva"
+    },
+    {
+      "q": "睡",
+      "s": "수",
+      "k": "졸음",
+      "m": "*drowsy, sleep, doze",
+      "d": "",
+      "e": "睡眠 수면 sleep, slumber"
+    },
+    {
+      "q": "錘",
+      "s": "추",
+      "k": "저울추",
+      "m": "*spindle, balance weight on scale",
+      "d": "Wasn't it a cursed spindle that Sleeping Beauty pricked her finger on? Therefore the spindle was the metal that caused her to droop and eventually sleep for 100 years.",
+      "e": "錘 추 scale weight, fishing sinker/weight; 時計錘 시계추 clock pendulum"
+    },
+    {
+      "q": "乗/乘",
+      "s": "승",
+      "k": "탈",
+      "m": "ride",
+      "d": "In the near future, we hope to ride on biomass fueled vehicles. Good news for those who have wheat silage surplus.<br>I took my ride too see the wheat fields to the north.",
+      "e": "乘客 승객 passenger; 搭乘 탑승 boarding, riding"
+    },
+    {
+      "q": "剰/剩",
+      "s": "잉",
+      "k": "남을",
+      "m": "surplus, leftovers",
+      "d": "If there is a surplus of people on the ride, they may just need to get rid of a few passengers using a saber.",
+      "e": "剩餘 잉여 surplus, remainder; 過剩 과잉 excess, suplus, superfluity"
+    }
+  ]
+};
