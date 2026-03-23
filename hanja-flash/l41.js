b030b82942b70febabc436fa65b9e3fc43e6e5ce
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-03-23 17:53:13 +0300

    .

---
+++ hanja-flash/l41.js
@@ -0,0 +1,260 @@
+const L41 = {
+  "name" : "L41",
+  "cards" : [
+    {
+      "q": "今",
+      "s": "금",
+      "k": "이제",
+      "m": "now, *clock",
+      "d": "My meeting is at 7 - but that's now!",
+      "e": "只今 지금 the present, the present day, now; 今年 금년 this year"
+    },
+    {
+      "q": "含",
+      "s": "함",
+      "k": "머금을",
+      "m": "hold in mouth, *include",
+      "d": "This clock includes a mouth that will tell you what the time is now.",
+      "e": "包含 포함 inclusion, comprehension, implication"
+    },
+    {
+      "q": "貪",
+      "s": "탐",
+      "k": "탐낼",
+      "m": "covet",
+      "d": "One begins one's life coveting money. One ends one's life coveting time - those precious seconds ticking away on the clock.",
+      "e": "貪내다 탐내다 desire, want, crave for, covet"
+    },
+    {
+      "q": "吟",
+      "s": "음",
+      "k": "읊을",
+      "m": "*versify, sing, recite",
+      "d": "A rapper using his mouth to versify with a big clock around his neck: Hickery dickery dock, the mouth spat out the clock...",
+      "e": "吟味 음미 appreciation, savoring, enjoying; 呻吟 신음 groan, moan"
+    },
+    {
+      "q": "念",
+      "s": "념/염",
+      "k": "생각",
+      "m": "*wish; think of, recall",
+      "d": "Someone wishing from the bottom of their heart that the clock would stop. A mid-life crisis perhaps.",
+      "e": "槪念 개념 notion, general idea, concept; 理念 이념 ideology"
+    },
+    {
+      "q": "捻",
+      "s": "념염",
+      "k": "비틀",
+      "m": "*wrench, to twist or nip with the fingers",
+      "d": "'I wish you would accept our friendly offer', the mob guy said, as he started wrenching my fingers one by one.",
+      "e": "捻挫 염좌 sprain, wrench"
+    },
+    {
+      "q": "琴",
+      "s": "금",
+      "k": "거문고",
+      "m": "*harp; Chinese lute or guitar.",
+      "d": "A pair of kings exchange gifts: a clock and a harp.",
+      "e": "伽倻琴 가야금 kayagum; 琴瑟 금슬 Korean harp and lute; conjugal harmony"
+    },
+    {
+      "q": "陰",
+      "s": "음",
+      "k": "그늘",
+      "m": "‘female’ principle; dark, *shade",
+      "d": "The only shade at The Pinnacle is in the shadow of the giant clock tower, or from a rising cloud.",
+      "e": "陰陽 음양 Um and Yang, the positive and negative, the male and female; 陰曆 음력 lunar calendar"
+    },
+    {
+      "q": "予",
+      "s": "여/예",
+      "k": "나/줄/미리",
+      "m": "*beforehand, I, me, to give",
+      "d": "Beforehand, this was a halberd but I cut it short so it could be for hand to hand combat.",
+      "e": ""
+    },
+    {
+      "q": "序",
+      "s": "서",
+      "k": "차례",
+      "m": "serial order, sequence; *preface",
+      "d": "The preface to all human history was written beforehand in caves.",
+      "e": "秩序 질서 (public) order, discipline, system; 順序 순서 order, sequence; 序說 서설 preface, introduction"
+    },
+    {
+      "q": "預",
+      "s": "예",
+      "k": "미리/맡길",
+      "m": "*deposit; prepare, arrange",
+      "d": "Depositing money into your savings account is something you do beforehand if you use your head.",
+      "e": "預金 예금 deposit, bank account; "
+    },
+    {
+      "q": "野",
+      "s": "야",
+      "k": "들",
+      "m": "plains",
+      "d": "These days we all work in front of computers. Beforehand we all worked out in the fields, in the plains.",
+      "e": "野生 야생 wildness; 分野 분야 field, sphere, realm; 野菜 야채 vegetables, greens"
+    },
+    {
+      "q": "兼",
+      "s": "겸",
+      "k": "겸할",
+      "m": "unite, combine, *concurrently",
+      "d": "A <b>horned</b> bull attacks <b>one</b> gardener. Being ninja-trained, the gardener does not even stop his work, <b>concurrently</b> using his <b>rack</b> to sweep leaves, while using <b>two sticks</b> to topple the <b>legs</b> of the bull.",
+      "e": "兼하다 겸하다 combine, unite"
+    },
+    {
+      "q": "嫌",
+      "s": "혐",
+      "k": "싫어할",
+      "m": "*dislike, hate; suspect",
+      "d": "Naturally, if you're dating more than one woman concurrently, they will certainly dislike each other!",
+      "e": "嫌惡 혐오 hatred, dislike, disgust;  嫌疑 혐의 dislike, aversion, suspicion, charge"
+    },
+    {
+      "q": "鎌",
+      "s": "겸",
+      "k": "낫",
+      "m": "sickle",
+      "d": "This is a special sickle with metal blades on both sides. It can cut many things concurrently!",
+      "e": ""
+    },
+    {
+      "q": "謙",
+      "s": "겸",
+      "k": "겸손할",
+      "m": "*self-effacing, humble, modest",
+      "d": "When you are self-effacing you use words to concurrently bring yourself down and make yourself look better.",
+      "e": "謙遜 겸손 modesty, humility"
+    },
+    {
+      "q": "廉",
+      "s": "렴/염",
+      "k": "청렴할",
+      "m": "*bargain; upright, honorable, honest",
+      "d": "Stores don't want you to know it, but if you find the secret cave, they run bargains there concurrently to their regular prices. Such a dishonesty!",
+      "e": "淸廉 청렴 integrity, probity, uprightness"
+    },
+    {
+      "q": "西",
+      "s": "서",
+      "k": "서녘",
+      "m": "west, *Old West",
+      "d": "The old west represented by a completely empty whiskey bottle (cf. 酉).<br>As a primitive, the meaning is expanded to refer to the Old West of cowboy-movie fame: 覀. With the exception of one kanji, given in the following frame, this element always appears at the top of its relative primitives.",
+      "e": "西洋 서양  the West, Europe and America, Western countries"
+    },
+    {
+      "q": "価/價",
+      "s": "가",
+      "k": "값",
+      "m": "value",
+      "d": "When <b>Chuck Norris</b> was in the <b>old west</b> as a cowboy, the <b>value</b> of the reward on his head (dead or alive!) was extremely high, but nobody could ever claim it.",
+      "e": "價値 가치 value, worth, merit; 評價 평가 valuation, estimation"
+    },
+    {
+      "q": "要",
+      "s": "요",
+      "k": "요긴할",
+      "m": "*need, necessary",
+      "d": "In the Old West, women were needed because nobody knew how to cook or clean.",
+      "e": "需要 수요 demand, request; 綱要 강요"
+    },
+    {
+      "q": "腰",
+      "s": "요",
+      "k": "허리",
+      "m": "*loin(s), waist, kidney",
+      "d": "After being sat down in the office too long, loin is the part of the body that needs massaging.",
+      "e": "腰痛 요통 lumbago, pain in loins"
+    },
+    {
+      "q": "票",
+      "s": "표",
+      "k": "표",
+      "m": "*ballot; slip of paper or bamboo. ticket",
+      "d": "In the Old West there were no ballot boxes. Afraid of fraud and fighting among the gunslingers, the ballots were posted into a slot in the top of the local church altar instead. They may have been rough, but the cowboys wouldn't fight in the church.",
+      "e": "票 표 card, label, ticket; 手票 수표 (bank) check, cheque"
+    },
+    {
+      "q": "漂",
+      "s": "표",
+      "k": "떠다닐",
+      "m": "*drift, float",
+      "d": "Picture a drifting lifeboat, water splashing over the side, and the occupants holding a ballot to decide who to eat next.",
+      "e": "漂流 표류 drift, drifting, adrift; 漂白 표백 bleaching, decoloration"
+    },
+    {
+      "q": "標",
+      "s": "표",
+      "k": "우듬지",
+      "m": "*signpost; mark, label, sign",
+      "d": "Trees were frequenly used as signposts in those days, indicating where one had to go to cast their ballots.",
+      "e": "目標 목표 mark, sign, target, goal; 座標 좌표 coordinates"
+    },
+    {
+      "q": "栗",
+      "s": "률/율",
+      "k": "밤",
+      "m": "chestnut",
+      "d": "Chestnut tree, west nut tree.",
+      "e": ""
+    },
+    {
+      "q": "慄",
+      "s": "률/율",
+      "k": "떨릴",
+      "m": "*shudder, shiver, tremble",
+      "d": "You’re in the state of mind of shuddering with fear when the chestnut tree is falling over towards you.",
+      "e": ""
+    },
+    {
+      "q": "遷",
+      "s": "천",
+      "k": "옮길",
+      "m": "transition, move, shift",
+      "d": "Since the days of the Old West, traveling has transitioned from trying not to step on big snakes to driving on big roads.",
+      "e": "遷都 천도 the transfer of the capital; 左遷 좌천 degradation, downgrading, demotion"
+    },
+    {
+      "q": "覆",
+      "s": "복/부",
+      "k": "뒤집힐/덮을",
+      "m": "*capsize; cover",
+      "d": "The U.S.S. <b>Old West</b> has <b>capsized</b>, and all the cowboys on board are trying to <b>restore</b> her upright.",
+      "e": "顚覆 전복 overthrow, overturn, capsizal; 腹面 복면 mask, veil"
+    },
+    {
+      "q": "煙",
+      "s": "연",
+      "k": "연기",
+      "m": "smoke",
+      "d": "In the old west people not only fired up and smoked tobacco filling the air with its smoke but they chewed it too, spitting it on the ground.",
+      "e": "禁煙 금연 no smoking; 煙氣 연기 smoke, fumes"
+    },
+    {
+      "q": "南",
+      "s": "남",
+      "k": "남녘",
+      "m": "south",
+      "d": "Some people can only find happiness through the southern regions below their belt.",
+      "e": "南美 남미 South America"
+    },
+    {
+      "q": "楠",
+      "s": "남",
+      "k": "녹나무",
+      "m": "camphor tree",
+      "d": "There is a tree in the south that legand has if you camp under it, you will be visited by a whore.",
+      "e": ""
+    },
+    {
+      "q": "献/獻",
+      "s": "헌",
+      "k": "드릴",
+      "m": "offering",
+      "d": "A South Korean street vendor is offering chihuahua snacks.",
+      "e": "獻血 헌혈 blood donation; 獻金 헌금 a gift of money, contribution, donation, offering"
+    }  ]
+};
