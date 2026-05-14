d584cb00d2336af302f05df2aed1a78a99646b00
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-05-14 18:35:15 +0300

    .

---
+++ hanja-flash/l43.js
@@ -0,0 +1,309 @@
+const L43 = {
+  "name" : "L43",
+  "cards" : [
+    {
+      "q": "干",
+      "s": "건,안,한",
+      "k": "방패/줄기,마를,들개,일꾼",
+      "m": "oppose, offend, invade; *dry",
+      "d": "Dry one item in ten minutes on the clothesline.",
+      "e": "若干 약간 a few, a bit; 干涉 간섭 interference; 干拓 간척 land reclamation by drainage.<br>* The primitive meaning is (revolving circular) clothesline (viewed rom aside)."
+    },
+    {
+      "q": "肝",
+      "s": "간",
+      "k": "간",
+      "m": "liver",
+      "d": "Well you all know that the liver is the part of the body that would be quite grateful if you led a dry lifestyle (i.e. no alcohol).",
+      "e": "肝腸 간장 bowels and instestines; heart"
+    },
+    {
+      "q": "刊",
+      "s": "간",
+      "k": "새길",
+      "m": "publish",
+      "d": "The newspaper pages are hung on the clotheseline to dry. This gives the government a chance to censor the news: they cut up unwanted stories with a sabre. Therefore, only state-approved stories are published.",
+      "e": "刊行 간행 publication"
+    },
+    {
+      "q": "汗",
+      "s": "한",
+      "k": "땀",
+      "m": "sweat",
+      "d": "The way sweat is supposed to work: it gets your skin wet, and then as it evaporates/dries, it cools you off.",
+      "e": "汗蒸湯 한증탕 sauna"
+    },
+    {
+      "q": "軒",
+      "s": "헌",
+      "k": "집",
+      "m": "*flats",
+      "d": "A counter for houses. Two most important things to ask before renting flats. is their carparking space and colthlines for drying.",
+      "e": ""
+    },
+    {
+      "q": "岸",
+      "s": "안",
+      "k": "언덕",
+      "m": "beach",
+      "d": "At the base of mountain cliffs, there is usually a beach. You often can see people lying there, drying off after a swim.",
+      "e": "海岸 해안 seashore, coast; 沿岸 연안 coast, shore"
+    },
+    {
+      "q": "幹",
+      "s": "간",
+      "k": "줄기",
+      "m": "tree trunk",
+      "d": "A fine mist is falling, and so everyone gathers around near the tree-trunk, effectively using the rest of the tree as an umbrella in order to keep dry.",
+      "e": "才幹 재간 ability, talent, skill; 基幹 기간 mainstay, nucleus"
+    },
+    {
+      "q": "于",
+      "s": "우",
+      "k": "어조사",
+      "m": "*potato",
+      "d": "Note how this element differs from dry in virtue of the small hook at the end of the third stroke.",
+      "e": ""
+    },
+    {
+      "q": "芋",
+      "s": "우,후",
+      "k": "토란,클",
+      "m": "taro, *potato",
+      "d": "Keep your potatos dry otherwise they will rot or grow up and flower so that you would have nothing to eat.",
+      "e": ""
+    },
+    {
+      "q": "宇",
+      "s": "우",
+      "k": "집",
+      "m": "house, *eaves",
+      "d": "This kanji means universe, seen here as a roof as that was how it was represented back then the vault of heaven. Today, we know that seen from the universe, our planet has the aspect of a potato, and not a perfect sphere.",
+      "e": "宇宙 우주 the universe, cosmos, space"
+    },
+    {
+      "q": "余/予",
+      "s": "여",
+      "k": "나/남을",
+      "m": "*too much, *scale",
+      "d": "I dont want to criticize your farming, but shielding your patatoes with an umbrella is a little bit too much for me...<br>Since the phrase “too much” is overly abstract, we shall take the image of a <b>scale</b> whose indicator spins round and round on the dial because too much weight has been set on it.",
+      "e": ""
+    },
+    {
+      "q": "除",
+      "s": "제",
+      "k": "덜",
+      "m": "exclude",
+      "d": "To become a member of The Pinnacle, they test you on a scale. If you weigh too much, they exclude you.",
+      "e": "除하다 제하다 take off/away, deduct, subtract; 削除 삭제 deletion, elimination, erasure"
+    },
+    {
+      "q": "徐",
+      "s": "서",
+      "k": "천천히 할",
+      "m": "gradually",
+      "d": "You can't expect weight loss to happen overnight. You should limit going to the scale to weigh yourself to once a day or once a week, in order to observe the anticipated weight loss gradually.",
+      "e": "徐徐히 서서히 slowly, leisurely; 徐行 서행 slow motion"
+    },
+    {
+      "q": "叙/敍",
+      "s": "서",
+      "k": "차례",
+      "m": "express,state,*confer",
+      "d": "At parent-teacher <b>confer</b>ence the teacher <b>states</b>: 'Your son thinks <b>too much</b> with his <b>crotch</b>.'",
+      "e": "敍述 서술 description, narration; 敍事 서사 narration, description"
+    },
+    {
+      "q": "途",
+      "s": "도",
+      "k": "길",
+      "m": "route",
+      "d": "On this route, all trucks have to check in at the scales before they can get on the road.",
+      "e": "別途 별도 a separate way; 前途 전도 the way one has to go, prospect"
+    },
+    {
+      "q": "斜",
+      "s": "비낄 사",
+      "k": "비낄",
+      "m": "slanting, sloping, *diagonal",
+      "d": "The scale tilts diagonally when a measuring cup filled with too much is set on one end.",
+      "e": "傾斜 경사 inclination, slant, slope"
+    },
+    {
+      "q": "塗",
+      "s": "도",
+      "k": "칠할",
+      "m": "smear, *paint",
+      "d": "A true artisan mixes his own <b>paint</b> from <b>water</b> and <b>soils</b> on a <b>scale</b> in careful proportions.",
+      "e": "塗炭 도탄 misery, distress; 塗褙 도배 papering (of the walls of a room); 塗色 도색 painting"
+    },
+    {
+      "q": "束",
+      "s": "속",
+      "k": "묶을",
+      "m": "bind, *bundle",
+      "d": "Unlike in 杏, this big mouth isn't content to wait for apricots to fall in his mouth, so he has climbed the tree and is taking them in bundles.",
+      "e": "約束 약속 promise, agreement; 拘束 구속 restriction, restraint, binding"
+    },
+    {
+      "q": "頼/賴",
+      "s": "뢰/뇌",
+      "k": "의뢰할",
+      "m": "rely, depend on, *trust",
+      "d": "We found a bundle of severed heads/shells (money) in your room, its difficult to trust you.",
+      "e": "依賴 의뢰 dependence, reliance; request, solicitation; 信賴 신뢰 confidence, trust, reliance"
+    },
+    {
+      "q": "瀬/瀨",
+      "s": "뢰/뇌",
+      "k": "여울",
+      "m": "rapids",
+      "d": "The water in the rapids can't be trusted.",
+      "e": ""
+    },
+    {
+      "q": "勅",
+      "s": "칙",
+      "k": "칙서",
+      "m": "imperial order",
+      "d": "Imperial order carries quite a bundle of executive power.",
+      "e": "勅書 칙서 official letter from the Emperor"
+    },
+    {
+      "q": "疎/疏",
+      "s": "소",
+      "k": "소통할",
+      "m": "neglect; careless; *alienate/transcription",
+      "d": "Ma マ + stop 止 => zoo 疋. Aliens went astray on Earth, so they were bundled up and alienated in a zoo.<br>The zoo needed a transcription of the accident report involving the infant caught in the flood.",
+      "e": "疏通 소통 mutal understanind, communication; 疏外 소외 estrangement, alienation, neglect"
+    },
+    {
+      "q": "辣",
+      "s": "랄/날",
+      "k": "매울",
+      "m": "peppery, pungent, *bitter",
+      "d": "Too many spicy things bundled in a meal will make anyone feel bitter towards the cook.",
+      "e": "辛辣하다 신랄하다 sharp, bitter, severe, harsh"
+    },
+    {
+      "q": "速",
+      "s": "속",
+      "k": "빠를",
+      "m": "quick",
+      "d": "If you find a bundle of money on the road, pick it up quick before anyone notices.",
+      "e": "速度 속도 speed, velocity"
+    },
+    {
+      "q": "整",
+      "s": "정",
+      "k": "가지런할",
+      "m": "neat, organize",
+      "d": "Recycling in Japan, you have to make sure all your bundles are organized correctly otherwise the taskmasters won't accept them.",
+      "e": "整理 정리 arrangementm, order, adjustment; 調整 조정 regulation, adjustment"
+    },
+    {
+      "q": "㑒",
+      "s": "첨",
+      "k": "여러",
+      "m": "*awl",
+      "d": "One cannot hide an awl in a bag. At a <b>meeting</b> some <b>person</b> will open his <b>mouth</b> and tell the sharp truth.",
+      "e": ""
+    },
+    {
+      "q": "剣/劍",
+      "s": "검",
+      "k": "칼",
+      "m": "saber",
+      "d": "Every self-respecting ninja knows the story of 'The Awl and the Sabre'. According to the legend, a woman once defended herself by using an awl against a sabre! She used to work all day with that awl. However, the bandit using the sabre had no skill. And thus the awl beat the sabre. And from there the saying goes : 'The sabre is powerless against the awl, if one can't use the sabre.'<br>In olden times, debates were won with a <b>saber</b> duel.",
+      "e": "劍 검 sword, saber"
+    },
+    {
+      "q": "険/險",
+      "s": "험",
+      "k": "험할",
+      "m": "narrow pass, dangerous; *precipitous",
+      "d": "Getting to the Pinnacle is a very precipitous climb. You need to drive awls into the wall as you climb up.",
+      "e": "險하다 험하다 precipitous, dangerous, harsh, rough; 冒險 모험 adventure, venture, risky attempt"
+    },
+    {
+      "q": "検/檢",
+      "s": "검",
+      "k": "검사할",
+      "m": "examination",
+      "d": "Two experts are angrily debating the age of a tree when all they need do is examine the number of rings in its core.",
+      "e": "檢査 검사 inspection, examination, test"
+    },
+    {
+      "q": "倹/儉",
+      "s": "검",
+      "k": "검소할",
+      "m": "frugal",
+      "d": "Chuck Norris is a thrifty person and he would have a debate to save a few cents.",
+      "e": "勤儉 근검 thrift and diligence, economy"
+    },
+    {
+      "q": "重",
+      "s": "중",
+      "k": "무거울",
+      "m": "heavy",
+      "d": "The first computer 里 in the world was as heavy as thousand 千 computers are now.",
+      "e": " 重하다 중하다 important, weighty, serious; 愼重 신중 prudence, discretion"
+    },
+    {
+      "q": "動",
+      "s": "동",
+      "k": "움직일",
+      "m": "move",
+      "d": "To move something heavy, you need muscles.",
+      "e": "動物 동물 animal; 動機 동기 motive, incentive"
+    },
+    {
+      "q": "腫",
+      "s": "종",
+      "k": "종기",
+      "m": "tumor",
+      "d": "A tumor is a useless body part that is nothing but heavy.",
+      "e": "腫氣 종기 swelling, boil, tumor, abscess"
+    },
+    {
+      "q": "勲/勳",
+      "s": "훈",
+      "k": "공",
+      "m": "meritorious deed",
+      "d": "A meritorious deed? Like moving a damsel in distress out of a fire, maybe.",
+      "e": "勳章 훈장 decoration, order, medal; 功勳 공훈 exploit, great deed"
+    },
+    {
+      "q": "働",
+      "s": "동",
+      "k": "일할",
+      "m": "work",
+      "d": "A person who moves something is doing work, physically speaking.",
+      "e": ""
+    },
+    {
+      "q": "種",
+      "s": "종",
+      "k": "씨",
+      "m": "seed, race, species",
+      "d": "Our species has a wheat heavy diet...",
+      "e": "種類 종류 kind, sort, species; 人種 인종 race, ethnic group"
+    },
+    {
+      "q": "衝",
+      "s": "충",
+      "k": "찌를",
+      "m": "collide",
+      "d": "The LHC (Large Hadron Collider) is a boulevard for colliding heavy particles.",
+      "e": "衝突 충돌 collision, clash, bump"
+    },
+    {
+      "q": "薫/薰",
+      "s": "훈",
+      "k": "향초",
+      "m": "a fragant herb; cauterize",
+      "d": "These flowers emit very heavy fragrance when burned over the fire.",
+      "e": "薰氣 훈기 warm air, heat; 薰薰하다 훈훈하다 comfortably warm, nice and warm"
+    }
+  ]
+};
