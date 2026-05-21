37fe0128d19dda558b11da144e9ed3cf4cb5ae57
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-05-21 18:41:04 +0300

    .

---
+++ hanja-flash/l45-1.js
@@ -0,0 +1,277 @@
+const L45_1 = {
+  "name" : "L45-1",
+  "cards" : [
+    {
+      "q": "彡",
+      "s": "삼,섬",
+      "k": "터럭,성씨",
+      "m": "hair, *shape",
+      "d": "Hairs do model the shape of our heads.",
+      "e": ""
+    },
+    {
+      "q": "彫",
+      "s": "조",
+      "k": "새길",
+      "m": "*carve, decorate",
+      "d": "Start with a <b>circumference</b> and <b>carve</b> away until you get the <b>shape</b> you want.",
+      "e": "彫塑 조소 the plastic arts, carving and modeling;  彫刻 조각 sculpture, engraving, carving"
+    },
+    {
+      "q": "形",
+      "s": "형",
+      "k": "모양",
+      "m": "shape",
+      "d": "Shadow theatre requires two hands to make numerous shapes.",
+      "e": "形象 형상 shape, form; 形成 형성 formation, evolution"
+    },
+    {
+      "q": "影",
+      "s": "영",
+      "k": "그림자",
+      "m": "shadow",
+      "d": "The sun over the capital cast a shadow with a bizarre shape.",
+      "e": "影響 영향 influence, effect; 撮影 촬영 photographing, filming"
+    },
+    {
+      "q": "杉",
+      "s": "삼",
+      "k": "삼나무",
+      "m": "*cedar",
+      "d": "Cedar is an evergreen tree that keeps its shape all year long. That is, it doesn't loose its needles/leaves in the winter.",
+      "e": " 杉나무 삼나무 cryptomeria, Japanese cedar"
+    },
+    {
+      "q": "彩",
+      "s": "채",
+      "k": "채색",
+      "m": "coloring",
+      "d": "When coloring something in you grab 采 the crayon of choice to fill in the shapes.",
+      "e": "彩色 채색 coloring, coloration; 水彩 수채 aquarelle, aquarelle painting"
+    },
+    {
+      "q": "彰",
+      "s": "창",
+      "k": "드러날",
+      "m": "clear, manifest, *patent",
+      "d": "The shape of a sheriff's badge makes it patent who he is.",
+      "e": "表彰 표창 commendation, honouring, awarding"
+    },
+    {
+      "q": "彦/彥",
+      "s": "언",
+      "k": "선비",
+      "m": "elegant, *lad",
+      "d": "A young lad is standing atop a cliff, looking at the writhing shape of his fallen lass below.",
+      "e": ""
+    },
+    {
+      "q": "顔/顏",
+      "s": "안",
+      "k": "낯",
+      "m": "face",
+      "d": "The lad jumps off the cliff, breaks his head, and smashes his face.",
+      "e": "顏色 안색 face color, expression; 洗顏 세안 eyewashing"
+    },
+    {
+      "q": "須",
+      "s": "수",
+      "k": "모름지기/수염",
+      "m": "ought",
+      "d": "One ought to use his head to do things right. It is indispensable so as not to pull his hair afterwards out of his head (hence changing its shape).",
+      "e": "必須(의) 필수(의) indispensable, essential, mandatory"
+    },
+    {
+      "q": "膨",
+      "s": "팽",
+      "k": "부를",
+      "m": "swell",
+      "d": "An injured body part may swell. It will begin to throb like a bass drum and will change shape.",
+      "e": "膨脹 팽창 swelling, inflation, expansion, dilation, growth"
+    },
+    {
+      "q": "参/參",
+      "s": "참",
+      "k": "참여할",
+      "m": "take part in, intervene; *visit",
+      "d": "Three drunks at a bar, all 'bending elbows' over their cocktails, are participating in a lively conversation.",
+      "e": "參與 참여 participation; 參加 참가 participation (in a contest); 參考 참고 reference, consultation"
+    },
+    {
+      "q": "惨/慘",
+      "s": "참",
+      "k": "참혹할",
+      "m": "sad, pitiful, *wretched; cruel",
+      "d": "The hangover is a pitiful state after three companions spent an drinking evening together. The headache is but cruel.",
+      "e": "慘酷 참혹 misery, wretchedness; cruelty, brutality"
+    },
+    {
+      "q": "修",
+      "s": "수",
+      "k": "닦을",
+      "m": "study, repair, cultivate; *discipline",
+      "d": "Chuck Norris teaches some discipline to the evil taskmaster, hitting him with his walking stick until both the taskmaster and the stick are well bent out of shape.",
+      "e": "修理 수리 repair, mending; 修習 수습 apprenticeship, probation"
+    },
+    {
+      "q": "珍",
+      "s": "진",
+      "k": "보배",
+      "m": "precious, valuable, *rare",
+      "d": "It's rare to see the king carrying his own parasol - typically he gets one of his attendants to do it for him. [Umbrella over shape = parasol]",
+      "e": "珍景 something rare and wonderful, a miracle; 珍貴하다 진귀하다 rare and precious/valuable"
+    },
+    {
+      "q": "診",
+      "s": "진",
+      "k": "진찰할",
+      "m": "medical checkup",
+      "d": "At my last check-up, the doctor said to me: 'Start carrying a parasol, or you might get skin cancer. You've been tanning too much!' [Umbrella over shape = parasol].",
+      "e": "診脈 진맥 feeling the pulse for diagnosis; 診斷 진단 diagnosis"
+    },
+    {
+      "q": "文",
+      "s": "문",
+      "k": "글월",
+      "m": "literature, writing, *sentence; *plaid",
+      "d": "The teacher puts a tophat on all pupils with gramatically correct sentences, while the rest are left with a big red X on their homework.",
+      "e": "文法 문법 grammar"
+    },
+    {
+      "q": "対/對",
+      "s": "대",
+      "k": "대할",
+      "m": "correct, right; facing, opposed, *vis-a-vis",
+      "d": "Civilization is opposed to barbarity by means of holding fast to the literature and culture like being glued.<br>A sad example of what happens when the old go versus the young. They ended upside down in the row, glued to the dirt.",
+      "e": "對하다 대하다 to face, to confront, to be opposite to"
+    },
+    {
+      "q": "紋",
+      "s": "문",
+      "k": "무늬",
+      "m": "line, streak, stripe; wrinkle; *family crest",
+      "d": "On <b>Spiderman's</b> <b>family crest</b> reads the <b>sentence</b>: 'With great power, comes great responsibility'... in Latin of course.",
+      "e": "紋章 문장 crest of arms, family insignia, coat of arms; 波紋 파문 ripple, ring on the water; 指紋 지문 fingerprint, dactylogram"
+    },
+    {
+      "q": "蚊",
+      "s": "문",
+      "k": "모기",
+      "m": "mosquito",
+      "d": "The mosquito (an insect) knows only one sentence: Bzzzzz.",
+      "e": ""
+    },
+    {
+      "q": "斑",
+      "s": "반",
+      "k": "아롱질",
+      "m": "mottled, striped, *speckled",
+      "d": "The sentences exchanged between the two kings got more and more heated, finally leading to a fight which left the whole area speckled with blood.",
+      "e": "斑指 반지 (finger) ring; 斑點 반점 spot, speck, speckle, fleck"
+    },
+    {
+      "q": "/ |",
+      "s": "",
+      "k": "",
+      "m": "two fenceposts",
+      "d": "Two fenceposts enclose whatever comes between them, as distinct from a pair of walking sticks.",
+      "e": ""
+    },
+    {
+      "q": "斉/齊",
+      "s": "제",
+      "k": "가지런할",
+      "m": "even, uniform, of equal length, *adjusted",
+      "d": "The sentence was adjusted to fit in between the two fenceposts.",
+      "e": "一劑히 일제히 all together, all at once"
+    },
+    {
+      "q": "剤/劑",
+      "s": "제",
+      "k": "약제",
+      "m": "medicine, drug, *dose",
+      "d": "The pharmacist adjusted 斉 the size of the medicine dose with his knife刂.",
+      "e": "藥劑 약제 medicine, drug; 洗劑 세제 detergent, cleanser, detersive"
+    },
+    {
+      "q": "済/濟",
+      "s": "제",
+      "k": "건널",
+      "m": "help; ferry, cross; *finish",
+      "d": "When you finish hard exercise, drink some water and rest until your heart rate and breathing have adjusted and become even again.",
+      "e": "經濟 경제 economy; 決濟 결제 settlement, liquidation; 共濟 공제 mutual aid; 未濟 미제 unfinished"
+    },
+    {
+      "q": "斎/齋",
+      "s": "재",
+      "k": "재계할",
+      "m": "vegetarian diet, to fast, abstain; study; *purification",
+      "d": "The religious purification rite required you to enter between the doorposts, walk up to the altar and recite the sacred sentence.",
+      "e": "齋밥 잿밥 (boiled) rice offered to Buddha; 書齋 서재 a study, a library"
+    },
+    {
+      "q": "粛/肅",
+      "s": "숙",
+      "k": "엄숙할",
+      "m": "pay respects, reverently, *solemn",
+      "d": "Even though it's a maddening task, the monks stay solemn as they rake rice between the fenceposts.<br>The solemn old cleaner rakes the arm-wrestling table in between bouts.",
+      "e": "嚴肅 엄숙 solemnity, dignity, gravity"
+    },
+    {
+      "q": "X",
+      "s": "",
+      "k": "",
+      "m": "sparkler",
+      "d": "This element depicts spreading out or scattering from a focal point. To capture this meaning, we choose the image of a sparkler. It will often have another primitive put at its center point.",
+      "e": ""
+    },
+    {
+      "q": "塁/壘",
+      "s": "루/누",
+      "k": "보루",
+      "m": "rampart, military wall; *baseball bases",
+      "d": "Playing baseball in a rice field with sparklers stuck into the ground to act as bases.<br>Anyone old enough to play baseball knows that a baseball field has three bases on the ground.",
+      "e": "堡壘 보루 fort, fortress, rampart, stronghold"
+    },
+    {
+      "q": "楽/樂",
+      "s": "악",
+      "k": "노래",
+      "m": "happy, glad, enjoyable; *music",
+      "d": "The most amazing new bird has been found: it's white and produces the most beautiful music. It sits in trees all day and shoots out sparks as it sings.<br>The old women working at the top of the <b>tree</b> sing some merry <b>music</b> as they rob the silkworms of their <b>white cocoons</b> to make t-shirts and other crap.",
+      "e": "音樂 음악 music; 安樂 안락 comfort, ease"
+    },
+    {
+      "q": "薬/藥",
+      "s": "약",
+      "k": "약",
+      "m": "drug, medicine",
+      "d": "Old age medicine involved using flowers and shaman music and dances as treatment.",
+      "e": "藥劑 약제 medicine, drug; 痲藥 마약 narcotic, drug"
+    },
+    {
+      "q": "率",
+      "s": "솔,률/율",
+      "k": "거느릴, 비율",
+      "m": "to lead; *ratio, rate, limit",
+      "d": "When I first started downloading torrents, I thought that ratios were very mysterious 玄 . Now I keep a sparkling ten to one ratio!",
+      "e": "比率 비율 ratio, rate, proportion; 食率 식솔 family member; 率直 솔직 straightforwardness, frankness"
+    },
+    {
+      "q": "渋/澁",
+      "s": "삽",
+      "k": "껄끄러울",
+      "m": "*astringent; harsh, uneven, rough",
+      "d": "You ate something astringent? Quickly drink this water here to stop the sparkly sensation.",
+      "e": "難澁 난삽 difficulty"
+    },
+    {
+      "q": "摂/攝",
+      "s": "섭",
+      "k": "다스릴",
+      "m": "take in, absorb, *vicarious",
+      "d": "A man was taken into hospital with hearing loss and a burned finger after a sparkler accident, his company started looking for a (vicarious) deputy.",
+      "e": "攝理 섭리 providence, dispensation; 攝氏 섭씨 centigrade, Celsius"
+    }
+  ]
+};
---
+++ hanja-flash/l45-2.js
@@ -0,0 +1,157 @@
+const L45_2 = {
+  "name" : "L45-2",
+  "cards" : [
+    {
+      "q": "央",
+      "s": "앙,영",
+      "k": "가운데, 선명한 모양",
+      "m": "center",
+      "d": "A person with a big mouth wants to be the center of attention.",
+      "e": "中央 중앙 the center, the middle, the heart"
+    },
+    {
+      "q": "英",
+      "s": "영",
+      "k": "꽃부리",
+      "m": "petal; brave, a hero; England",
+      "d": "England was once the flowering center of civilization.",
+      "e": "英國 영국 England, Britain; 英特하다 영특하다 sagacious, intelligent, exception, great"
+    },
+    {
+      "q": "映",
+      "s": "영",
+      "k": "비칠",
+      "m": "reflect",
+      "d": "In the solar system, the Sun is the central source of all light, everything else just reflects it.",
+      "e": "映畫 영화 film, movie picture; 反映 반영 reflection, influence"
+    },
+    {
+      "q": "赤",
+      "s": "적",
+      "k": "붉을",
+      "m": "red, *apple",
+      "d": "The soil was red after the hunter used his dagger to cut off the animal's legs.<br>* As a primitive on the left, this kanji keeps the same form. Elsewhere, the first two strokes are abbreviated to a single dot, giving us 亦. This latter form will take the meaning of an <b>apple</b>",
+      "e": "赤色 적색 red color; 赤道 적도 the equator; 赤子 적자 deficit, a loss"
+    },
+    {
+      "q": "赦",
+      "s": "사",
+      "k": "용서할",
+      "m": "pardon",
+      "d": "The student blushed until his face was red, so the schoolmaster decided to pardon him.",
+      "e": "赦하다 사하다 forgive, pardon, excuse; 赦免 사면 pardon, amnesty"
+    },
+    {
+      "q": "変/變",
+      "s": "변",
+      "k": "변할",
+      "m": "change, transform; rebel; *unusual",
+      "d": "It's quite unusual and abnormal to see an apple with walking legs running away from the person who wants to eat it!<br>Spider-Man is still having words with himself in the mirror. But something has changed. He's no longer a hero. Now he's becoming an evil taskmaster.",
+      "e": "變하다 변하다 undergo a change, alter, become different;"
+    },
+    {
+      "q": "跡/迹",
+      "s": "적",
+      "k": "발자취",
+      "m": "search, trace, *tracks",
+      "d": "Pirate pokes the tip of his <b>wooden leg</b> into an <b>apple</b> to disguise his tracks.<br>The apple on the road was the only indication for the detective that could help trace the criminal...",
+      "e": "奇跡/奇迹 기적 miracle, marvel; 追跡 추적 chase, pursuit, tracking"
+    },
+    {
+      "q": "蛮/蠻",
+      "s": "만",
+      "k": "종족 이름",
+      "m": "barbarian",
+      "d": "The barbarian picks an apple from above and eats it even though there is a worm (insect) in it.",
+      "e": "野蠻 야만 savageness, barbarity"
+    },
+    {
+      "q": "恋/戀",
+      "s": "련/연",
+      "k": "그리워할",
+      "m": "love, long for, yearn for, *romance",
+      "d": "The romance between Snow White and Prince Charming started when Snow White took a bite of poison apple and her heart stopped.<br> In the [old] days, in order to win someone's heart and spark off a romance, both parties would first have to dress up as Spiderman and speak to each other at length.",
+      "e": "戀人 연인 one's love, sweetheart; 戀愛 연애 love"
+    },
+    {
+      "q": "彎",
+      "s": "만",
+      "k": "굽을",
+      "m": "bend, curve",
+      "d": "Stretching a bow: watch as two Spidermans stretch the huge bow they built with their own strings, to the cheers of the crowd that repeats the words: 'Spiderman! Spiderman!', trying to hit the apple.",
+      "e": ""
+    },
+    {
+      "q": "湾/灣",
+      "s": "만",
+      "k": "물굽이",
+      "m": "bay, bend of stream, *gulf",
+      "d": "In his most daring feat, William tell used his bow to shoot an apple off the head of his son across the water of the gulf.<br>A bend of a stream forms a bay.",
+      "e": "臺灣 대만 Taiwan, 灣 만 bay, gulf"
+    },
+    {
+      "q": "黄/黃",
+      "s": "황",
+      "k": "누를",
+      "m": "yellow",
+      "d": "The salad did not look very appetizing... The sprouts were yellow and there were animal legs sticking out of it.",
+      "e": "黃色 황색 yellow color; 黃金 황금 gold"
+    },
+    {
+      "q": "横/橫",
+      "s": "횡",
+      "k": "가로",
+      "m": "across, *sideways",
+      "d": "A tree went all yellow. It died and fell down across a zerba crossing.",
+      "e": "橫듣다 횡듣다 hear it wrong/amiss, misunderstand; 橫斷 횡단 crossing, traversing"
+    },
+    {
+      "q": "巴",
+      "s": "파",
+      "k": "바랄",
+      "m": "greatly desire, anxiously hope; *mosaic",
+      "d": "This is a comma-design outlaid in mosaic on the floor.",
+      "e": "三巴戰 삼파전 a three-cornered/-sided battle"
+    },
+    {
+      "q": "把",
+      "s": "파",
+      "k": "잡을",
+      "m": "hold, take, *grasp",
+      "d": "Japanese censorship laws require pornos to use a <b>mosaic</b> screen, so you can't see what the porno chick is grasping with her <b>fingers</b> (but you can assume, neetch).",
+      "e": "把握 파악 grasping, seizing, understanding"
+    },
+    {
+      "q": "色",
+      "s": "색",
+      "k": "빛",
+      "m": "color",
+      "d": "A variety of colors are bound up into one mosaic painting.",
+      "e": "色깔 색깔 color, hue, shade, tint"
+    },
+    {
+      "q": "絶/絕",
+      "s": "절",
+      "k": "끊을",
+      "m": "cut, sever, break off, *discontinue",
+      "d": "Spiderman has decided to discontinue his red color suit in favor of the black one, it looks way cooler.<br>The Spider-man comics were discountinued for a while, after the printers couldn't make any more red or blue color ink. Evenutally they brought the comic back in black and white, with spider-man in his black venom suit.",
+      "e": "絕壁 절벽 cliff, precipice; 拒絕 거절 refusal, rejection"
+    },
+    {
+      "q": "艶/艷",
+      "s": "염",
+      "k": "고울",
+      "m": "beautiful, sexy, voluptuous, *glossy",
+      "d": "The <b>glossy</b> 'male interest' magazines feature <b>bountiful</b> babes and were in full <b>colour</b> and bound up in soft-cover!",
+      "e": "濃艷하다 농염하다 voluptuous, enchanting; 艷聞 염문 gossip/rumor about a person's love affairs"
+    },
+    {
+      "q": "肥",
+      "s": "비",
+      "k": "살찔",
+      "m": "fat, plump; fertile, *fertilizer",
+      "d": "Natural fertilizer comes from a part of the body that cannot be shown on TV and therefore needs to be covered by mosaic. Besides the fertilizer come out in a comma-like form.",
+      "e": "肥沃 비옥 fertility, richness; 肥滿 비만 corpulence, fatness, obesity; 堆肥 퇴비 compost"
+    }
+  ]
+};
