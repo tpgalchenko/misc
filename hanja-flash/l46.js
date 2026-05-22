a6807172ee434e67b721edc5ce2d5b89be4bd955
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-05-22 13:38:42 +0300

    .

---
+++ hanja-flash/l46.js
@@ -0,0 +1,182 @@
+const L46 = {
+  "name" : "L46",
+  "cards" : [
+    {
+      "q": "甘",
+      "s": "감",
+      "k": "달",
+      "m": "sweet, *wicker basket",
+      "d": "Little red riding hood is carrying around a wicker basket filled with sweet baked cookies for granny.",
+      "e": "甘蔗 감자 sugarcane; 甘味 감미 sweetness, sweet/sugary taste"
+    },
+    {
+      "q": "紺",
+      "s": "감",
+      "k": "감색",
+      "m": "dark/deep/navy blue",
+      "d": "Spiderman looks so sweet in navy blue!",
+      "e": "紺色 감색 dark/deep blue"
+    },
+    {
+      "q": "某",
+      "s": "모",
+      "k": "아무",
+      "m": "certain thing or person, so-and-so",
+      "d": "A generic person is described here as having body sort of like a tree trunk and head sort of like a wicker basket.",
+      "e": "某 모 certain person; 某種 모종 a certain kind"
+    },
+    {
+      "q": "謀",
+      "s": "모",
+      "k": "꾀",
+      "m": "plan, scheme, *conspire",
+      "d": "Talking about <b>so-and-so</b> behind their back - this is a conspiracy.",
+      "e": "謀反 모반 trason, rebellion, revolt, conspiracy; 共謀 공모 conspiracy"
+    },
+    {
+      "q": "媒",
+      "s": "매",
+      "k": "중매",
+      "m": "go-between, matchmaker, medium, *mediator",
+      "d": "The little girls <b>conspired</b> against their schoolmate and now are having a fight. An adult woman (teacher) has to come between them an serve as a mediator.",
+      "e": "媒體 매체 a medium; 仲媒 중매 mathmaking"
+    },
+    {
+      "q": "其",
+      "s": "기",
+      "k": "그(것)",
+      "m": "his, her, its, their; that; *chess rook",
+      "d": "The character resembles a chess rook.",
+      "e": ""
+    },
+    {
+      "q": "欺",
+      "s": "기",
+      "k": "속일",
+      "m": "deceit",
+      "d": "Kasparov practices <b>deceit</b> by grabbing your <b>rook</b> while pretending to <b>yawn</b>.",
+      "e": "詐欺 사기 trickery, swindle, trick"
+    },
+    {
+      "q": "棋",
+      "s": "기",
+      "k": "바둑",
+      "m": "chess, *chess piece",
+      "d": "Chess piece = wooden rook.",
+      "e": "將棋/將碁 장기 changgi, Korean chess"
+    },
+    {
+      "q": "旗",
+      "s": "기",
+      "k": "기",
+      "m": "banner, *flag",
+      "d": "The United Republic of Chess' national flag is actually a banner with a rook, that way they can hang it at every tournament.",
+      "e": "國旗 국기 national flag"
+    },
+    {
+      "q": "期",
+      "s": "기",
+      "k": "기약할",
+      "m": "period",
+      "d": "The slowest remote chess game ever: a one month period between every move.",
+      "e": "期間 기간 period of time, term; 期約 기약 promise, pledge, engagement, appointment"
+    },
+    {
+      "q": "碁",
+      "s": "기",
+      "k": "바둑",
+      "m": "chess-like game, *Go",
+      "d": "Go is chess played with stones.",
+      "e": "將棋/將碁 장기 changgi, Korean chess"
+    },
+    {
+      "q": "基",
+      "s": "기",
+      "k": "터",
+      "m": "foundation, base, *fundamentals",
+      "d": "Fundamentals of chess: use the rooks to protect your ground.",
+      "e": "基礎 기초 foundation, basis"
+    },
+    {
+      "q": "甚",
+      "s": "심",
+      "k": "심할",
+      "m": "great extent, considerably, *tremendously",
+      "d": "It's a <b>tremendous</b> match between the two <b>chess</b>-players. They are clearly eachother's <b>equal</b>.",
+      "e": "甚하다 심하다 extreme, excessive, heavy, intense; 甚至於 심지어 what is worse, worst of all"
+    },
+    {
+      "q": "勘",
+      "s": "감",
+      "k": "헤아릴",
+      "m": "investigate, compare, *intuition",
+      "d": "Intuition is a tremendously powerful resource of the mind.",
+      "e": "勘案 감안 consideration, allowance"
+    },
+    {
+      "q": "堪",
+      "s": "감",
+      "k": "견딜",
+      "m": "withstand",
+      "d": "Because I hid myself behind a huge dirt barrier I dug, I was able to withstand the tremendous nuclear blast.",
+      "e": "堪當하다 감당하다 be equal to, be capable of carrying out, be up to, be fit for"
+    },
+    {
+      "q": "𠀐",
+      "s": "",
+      "k": "",
+      "m": "purse",
+      "d": "By adding a single stroke at the bottom of the kanji for in 中, we get a sort of pictograph of a purse.",
+      "e": ""
+    },
+    {
+      "q": "貴",
+      "s": "귀",
+      "k": "귀할",
+      "m": "expensive, *precious",
+      "d": "You put your precious shells (money) into your purse for safekeeping.",
+      "e": "貴하다 귀하다 noble, august, rare, scarce, precious, valuable"
+    },
+   
+    {
+      "q": "遺",
+      "s": "유",
+      "k": "남길",
+      "m": "leave, *bequeath",
+      "d": "When I take the road onto the afterlife I bequeath all my precious things to you.",
+      "e": "遺產 유산 legacy, bequest; 遺體 유체 corpse, (dead) body"
+    },
+    {
+      "q": "遣",
+      "s": "견",
+      "k": "보낼",
+      "m": "dispatch",
+      "d": "Dispatch all cars and <b>chase</b> the <b>maestro</b>! He's gone crazy and stolen a ladies <b>purse</b>!",
+      "e": "派遣 파견 dispatch, send someone somewhere"
+    },
+    {
+      "q": "潰",
+      "s": "궤",
+      "k": "무너질",
+      "m": "flooding river; militarily defeat; *defile",
+      "d": "Don't defile precious water.",
+      "e": "潰滅 궤멸 destruction, annihilation"
+    },
+    {
+      "q": "無",
+      "s": "무",
+      "k": "없을",
+      "m": "no/not. have no, *nothingness",
+      "d": "In a hot tub you can relax and have your stress vanish into nothingness. The top two strokes show someone reclining, and the next six are a pictograph of an oaken tub ribbed with metal strips, like the kind once used for bathing.",
+      "e": "無料 무료 free of charge; 無視 무시 disregard, neglect"
+    },
+    {
+      "q": "舞",
+      "s": "무",
+      "k": "춤출",
+      "m": "dance",
+      "d": "Attractive dancers should dance wearing nothing but sunglasses.",
+      "e": "舞臺 무대 stage, boards"
+    }
+  ]
+};
