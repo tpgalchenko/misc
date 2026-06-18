6a1105929619004ed3ac6f1b7118250dc98496a3
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-06-18 16:34:40 +0300

    .

---
+++ hanja-flash/l50.js
@@ -0,0 +1,245 @@
+const L50 = {
+  "name" : "L50",
+  "cards" : [
+    {
+      "q": "𠂆",
+      "s": "예",
+      "k": "당길",
+      "m": "drag",
+      "d": "This primitive depicts one stroke pulling another along behind it. Note how it differs from cliff and person because of this dragging effect, not to mention the fact that the first stroke is written right to left, almost as if it were a long drop. When this element comes under a different element, the strokes are drawn apart like this: //",
+      "e": ""
+    },
+    {
+      "q": "盾",
+      "s": "순",
+      "k": "방패",
+      "m": "shield",
+      "d": "This is a magic shield with ten eyes which have the power to drag the victim towards it.",
+      "e": "矛盾 모순 contradiction"
+    },
+    {
+      "q": "循",
+      "s": "순",
+      "k": "돌",
+      "m": "obey, comply with, follow, *sequential",
+      "d": "An anthropomorphization of the immune system: a column of little Roman soldiers with their shields marching sequentially through the veins of a person.",
+      "e": "循環 순환 circulation, cycle"
+    },
+    {
+      "q": "派",
+      "s": "파",
+      "k": "갈래",
+      "m": "school of thought, sect, branch, *faction",
+      "d": "The radical faction is almost eliminated, sir. They're living in the sewers full of dirty <b>water</b> dressed in <b>rags</b> and <b>dragging</b> their supplies.",
+      "e": "右派 우파 the right wing, the Right; 派送 파송/派遣 파견 dispatch, sending someone"
+    },
+    {
+      "q": "脈",
+      "s": "맥",
+      "k": "혈맥",
+      "m": "blood vessel, vein",
+      "d": "To get blood drawn from a vein you present the part of the body to the nurse, and they drag an alcohol swab (rag) across the vein.",
+      "e": "血脈 혈맥 blood vessel, family blood, lineage; 脈絡 맥락 veins, system of veins, coherence, logical connection, context"
+    },
+    {
+      "q": "衆",
+      "s": "중",
+      "k": "무리",
+      "m": "masses",
+      "d": "When the <b>masses</b> are hurt, say in a natural disaster like an earthquake, they can be seen everywhere <b>dragging</b> their <b>bloody rags</b>.",
+      "e": "公衆 공중 the public; 大衆 대중 masses (of people)"
+    },
+    {
+      "q": "逓/遞",
+      "s": "체",
+      "k": "갈마들",
+      "m": "hand over, *parcel post; substitute",
+      "d": "People complained about the delivery methods of UPS after finding out that they loaded <b>parcel post</b> onto a <b>towel</b> and <b>dragged</b> it down the <b>road</b>.<br>In the old days, deliverying <b>parcel post</b> was far more hazardous. Sometimes you had to <b>drag</b> a <b>tiger</b> 虎 down the <b>old road</b>. Most delivery men didn't last long.",
+      "e": "交遞/交替 교체 alternation, change; 郵遞 우체 mail/postal service"
+    },
+    {
+      "q": "段",
+      "s": "단",
+      "k": "조각",
+      "m": "section, piece, division; *grade",
+      "d": "The kanji connoting rank or class shows us a new element on the left: the familiar primitive for staples with an additional stroke cutting through the vertical stroke. It is easiest in these cases to make a primitive related to what we already know. Hence, we call it a <b>staple gun</b>. This is a military-<b>grade staple gun</b>! It can be loaded with real <b>missiles</b>.",
+      "e": "階段 계단 stairs; 段階 단계 level, rank, grade, step, stage, phase"
+    },
+    {
+      "q": "鍛",
+      "s": "단",
+      "k": "불릴",
+      "m": "forge",
+      "d": "As a discriminating metalsmith, I use the highest grade metals when forging such precision devices as staple guns and missiles.",
+      "e": "鍛鍊 단련 termper, forging, training, discipline"
+    },
+    {
+      "q": "后",
+      "s": "후",
+      "k": "임금/뒤",
+      "m": "empress",
+      "d": "Tell the empress to <b>F</b>uck off and leave me alone.",
+      "e": "后妃 후비/王后 왕후 empress, queen"
+    },
+    {
+      "q": "𠃌/",
+      "s": "",
+      "k": "",
+      "m": "*clothes hanger",
+      "d": "This element, which looks something like a backwards hook, we will call a clothes hanger. Used as an enclosure, it begins further to the left.",
+      "e": ""
+    },
+    {
+      "q": "幻",
+      "s": "환",
+      "k": "헛보일",
+      "m": "illusion, fantasy, phantasm",
+      "d": "After the play, The Phantasm of the Opera hangs up his cape on a clothes hanger and returns to his cocoon to sleep.",
+      "e": "夢幻 몽환 dreams and phantasma, fantasy"
+    },
+    {
+      "q": "司",
+      "s": "사",
+      "k": "맡을",
+      "m": "take charge of, manage, *director",
+      "d": "Every director is the same 同. But this director, this one, he's different.",
+      "e": "上司 상사 one's superior, boss; 司法 사법 the administration of justice, judicature; 司書 사서 librarian"
+    },
+    {
+      "q": "伺",
+      "s": "사",
+      "k": "엿볼",
+      "m": "serve, wait upon, attend; examine; *pay respects",
+      "d": "<b>Chuck Norris pays his respects</b> to the Lee family, for it was Bruce Lee who <b>directed</b> the movie that gave him his big break.",
+      "e": ""
+    },
+    {
+      "q": "詞",
+      "s": "사",
+      "k": "말",
+      "m": "words. phrase, expression; *parts of speech",
+      "d": "The big mouthed diretor is even picky about the parts of speech you use, shouting, 'More verbs! Less adjectives!' and such all the time.",
+      "e": "歌詞 가사 the words, the text, the libretto; 名詞 명사 noun"
+    },
+    {
+      "q": "飼",
+      "s": "사",
+      "k": "기를",
+      "m": "raise animals, feed; domesticate",
+      "d": "After a pride of hungry lions <b>ate</b> the <b>director</b>, the producer decided to tone down his documentary and film <b>domesticated</b> cats instead.",
+      "e": "飼育 사육 breeding/raising of cattle; 飼料 사료 feed, forage"
+    },
+    {
+      "q": "嗣",
+      "s": "사",
+      "k": "이을",
+      "m": "to connect, inherit, *heir",
+      "d": "",
+      "e": "後嗣 후사 successor, inheritor, heir(ess)"
+    },
+    {
+      "q": "舟",
+      "s": "주",
+      "k": "배",
+      "m": "boat",
+      "d": "This kanji is a pictograph of a two story boat seen from the front. The glass canopy is the body of the boat. The drop on top is the chimney. The horizontal line is the ceiling/floor between the the 1st/2nd floor. Finally, the two drops in the middle are the lights on the decks.",
+      "e": ""
+    },
+    {
+      "q": "舶",
+      "s": "박",
+      "k": "배",
+      "m": "large, ocean-going vessel, *liner",
+      "d": "The most famous <b>liner</b> is the Titanic, which struck an iceberg and sunk. Just imagine that huge <b>liner</b> strucking an even huger mass of <b>white</b> ice.",
+      "e": "船舶 선박 vessel, ship, seacraft"
+    },
+    {
+      "q": "航",
+      "s": "항",
+      "k": "배",
+      "m": "sail, *navigate",
+      "d": "A sail-<b>boat</b> needs a good wind to <b>navigate</b>, but what about a <b>whirlwind</b>?",
+      "e": "航空 항공 aviation, flight, aerial navigation; 航海 항해 sea voyage/navigation/sailing"
+    },
+    {
+      "q": "舷",
+      "s": "현",
+      "k": "뱃전",
+      "m": "the sides of a boat, bulwarks, *gunwale",
+      "d": "The gunwale is perhaps the most <b>mysterious</b> 玄 part of a <b>boat</b> for it is neither a gun nor a whale.",
+      "e": "右舷 우현 starboard; 舷門 현문 gangway"
+    },
+    {
+      "q": "般",
+      "s": "반",
+      "k": "일반",
+      "m": "sort, kind, category; *carrier",
+      "d": "In general, carrier boats have missiles.",
+      "e": "一般(의) 일반(의) general, universal, generic, common, widespread"
+    },
+    {
+      "q": "盤",
+      "s": "반",
+      "k": "소반",
+      "m": "*tray, plate, dish",
+      "d": "What do you call a <b>carrier</b> of <b>dishes</b>? A <b>tray</b>.",
+      "e": "鍵盤 건반 keyboard, clavier; 骨盤 골반 pelvis, basin; 錚盤 쟁반 tray, server, platter"
+    },
+    {
+      "q": "搬",
+      "s": "반",
+      "k": "옮길",
+      "m": "transfer, move, shift, *conveyor",
+      "d": "When workers neglect workplace safety, their <b>fingers</b> get cut off and carried away by the <b>conveyor</b> belt, which looks like a 'finger <b<carrier</b>' in that case.",
+      "e": "運搬 운반 transportation, conveyance, transport"
+    },
+    {
+      "q": "船",
+      "s": "선",
+      "k": "배",
+      "m": "ship",
+      "d": "A ship is a boat so big, it makes a river look like a gully.",
+      "e": "汽船 기선 steamship/boat; 貨物船 화물선 cargo boat"
+    },
+    {
+      "q": "艦",
+      "s": "함",
+      "k": "싸움배",
+      "m": "warship",
+      "d": "The <b>warship</b> towers over the smaller benign <b>boats</b>, <b>overseeing</b> their safe path from port to port.",
+      "e": "艦隊 함대 fleet, flotilla; 艦長 함장 captain of a warship"
+    },
+    {
+      "q": "艇",
+      "s": "정",
+      "k": "배",
+      "m": "small boat, *rowboat",
+      "d": "In venice, the <b>boat</b> used by the <b>courts</b> to transport criminals is a <b>rowboat</b>.",
+      "e": "高速艇 고속정 high-speed boat; 救命艇 구명정 lifeboat, rescue boat"
+    },
+    {
+      "q": "瓜",
+      "s": "과",
+      "k": "오이",
+      "m": "*melon, gourd, cucumber",
+      "d": "If you <b>claw</b> at a girls <b>瓜elons</b>, she is gonna give you the <b>elbow</b>.",
+      "e": "木果 모과 the fruit of a Chinese quince"
+    },
+    {
+      "q": "弧",
+      "s": "호",
+      "k": "활",
+      "m": "wooden bow, *arc, crescent",
+      "d": "When you use a bow to shoot a melon, the arc varies depending on how far away you are.",
+      "e": "括弧 괄호 parentheses"
+    },
+    {
+      "q": "孤",
+      "s": "고",
+      "k": "외로울",
+      "m": "*orphan, solitary",
+      "d": "An orphan will never know the joys on sucking on his mother's melons...",
+      "e": "孤獨 고독 solitude, lonelines; 孤兒 고아 orphan"
+    }
+  ]
+};
