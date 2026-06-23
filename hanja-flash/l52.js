fed91ae823a37f3c2f7a3823052128f2bb95fb32
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-06-23 14:48:14 +0300

    .

---
+++ hanja-flash/l52.js
@@ -0,0 +1,230 @@
+const L52 = {
+  "name" : "L52",
+  "cards" : [
+    {
+      "q": "牙",
+      "s": "아",
+      "k": "어금니",
+      "m": "molar tooth, *tusk",
+      "d": "The kanji looks like an elephant with tusks. the box is the elephant's eye. The final stroke is the tusk.",
+      "e": "齒牙 치아 teeth; 牙城 아성 inner citadel, stronghold"
+    },
+    {
+      "q": "芽",
+      "s": "아",
+      "k": "싹",
+      "m": "sprout, shoot, *bud",
+      "d": "Buds while they're still small tend to be tusk-shaped.",
+      "e": "發芽 발아 germination, sprouting, budding; 胚芽 배아 bud"
+    },
+    {
+      "q": "邪",
+      "s": "사",
+      "k": "간사할",
+      "m": "wrong, evil, *wicked",
+      "d": "The wicked queen builds her city walls out of menacing tusks.",
+      "e": "奸邪하다 간사하다 to have no priciples; 妖邪 요사 capricious, treacherous, weird, wicked, vicious"
+    },
+    {
+      "q": "雅",
+      "s": "아",
+      "k": "바를",
+      "m": "elegant, gracious",
+      "d": "Picture an elephant nearly stepping on a turkey. The elephant elegantly retracts its leg. 'Good gracious, I'm terribly sorry, old chap.' To which the turkey graciously responds, 'Think nothing of it, my dear sir, nothing at all.' Short version: The elephant was elegant, while the turkey was gracious.",
+      "e": "優雅하다 우아히다 elegant, graceful"
+    },
+    {
+      "q": "釆",
+      "s": "변",
+      "k": "분별할",
+      "m": "distinguish; *animal tracks",
+      "d": "Hansel and Gretel were walking through the woods, placing drops of rice to mark their path. But when they turned back, all they saw were animal tracks.",
+      "e": ""
+    },
+    {
+      "q": "釈/釋",
+      "s": "석",
+      "k": "풀",
+      "m": "elucidate, *interprete; release",
+      "d": "Her droppings 釆 had been spied on (睪) by a hidden camera in the bathroom. When she discovered what her husband was doing, she demanded that he explain 釋.",
+      "e": "解釋 해석 interpretation, explanation; 釋放 석방 release, discharge, liberation"
+    },
+    {
+      "q": "番",
+      "s": "번",
+      "k": "차례",
+      "m": "a turn, a time",
+      "d": "The famers take it in turns to watch out for any animal tracks on their rice field.",
+      "e": "番 번 alternation, change, shift, time; 낮番 낮번 day shift; 番號 번호 (ordinal) number"
+    },
+    {
+      "q": "審",
+      "s": "심",
+      "k": "살필",
+      "m": "examine, investigate, judge; *hearing",
+      "d": "In the house of law, both sides take turns during the hearing.",
+      "e": "審判 심판 judgement, decision, verdict; 審問 심문 trial, inquiring, hearing, interrogation"
+    },
+    {
+      "q": "翻",
+      "s": "번",
+      "k": "날",
+      "m": "upset, capsize, flip over",
+      "d": "It's your turn to flip the chicken wings on grill.",
+      "e": "翻覆/飜覆 번복 change, reverse, upset; 翻譯/飜譯 번역 translation"
+    },
+    {
+      "q": "藩",
+      "s": "번",
+      "k": "울타리",
+      "m": "fence, boundary; *clan",
+      "d": "In the particular clan depicted by this character, each member has to take his or her turn watering the flowers.",
+      "e": ""
+    },
+    {
+      "q": "毛",
+      "s": "모",
+      "k": "터럭",
+      "m": "hair, *fur, feathers",
+      "d": "This character simply reverses the direction of the final stroke of hand to produce fur. If you reverse your hand and put its palm down, you will have the side on which fur grows.",
+      "e": "毛布 모포 (fur) blanket"
+    },
+    {
+      "q": "耗",
+      "s": "모",
+      "k": "소모할",
+      "m": "consume, use up; waste, squander; *decrease",
+      "d": "Real fur coats and real christmas trees are dereasing the last couple of years. Plasctic chrismas trees and fake fur it is nowadays.",
+      "e": "消耗 소모 consumption, exhaustion, waste, use up"
+    },
+    {
+      "q": "尾",
+      "s": "미",
+      "k": "꼬리",
+      "m": "tail",
+      "d": "A dog waves its furry tail like a flag.",
+      "e": "交尾 교미 copulation, mating, coition; 尾行하다 미행히다 to follow up, to shadow, to trail"
+    },
+    {
+      "q": "乇",
+      "s": "탁",
+      "k": "풀잎",
+      "m": "*lock of hair",
+      "d": "This element is clearly derived from that for fur. By leaving out the second stroke, we get simply a lock of hair.",
+      "e": ""
+    },
+    {
+      "q": "宅",
+      "s": "댁/택",
+      "s": "댁/택",
+      "k": "댁/집",
+      "m": "residence, dwelling, home",
+      "d": "At home, all the drains in my house are clogged up with locks of hair.",
+      "e": "宅 댁 home, house; 住宅 주택 house, residence"
+    },
+    {
+      "q": "託",
+      "s": "탁",
+      "k": "부탁할",
+      "m": "entrust, rely on, *consign",
+      "d": "Jane Austen-esque scene: He consigns to her a lock of his hair, she gives him her word she will love him for ever.",
+      "e": "付託 부탁 request, favor, solicitation; 結託 결탁 conspiracy"
+    },
+    {
+      "q": "𠃌+灬",
+      "s": "",
+      "k": "",
+      "m": "tail feathers",
+      "d": "So as not to confuse this primitive element with the character for feathers, think of the extravagant tail-feather plumage of the peacock. The form itself is too pictographic to need breaking down further.",
+      "e": ""
+    },
+    {
+      "q": "為/爲",
+      "s": "위",
+      "k": "할",
+      "m": "do, handle, act",
+      "d": "One overcome three mountains ユ and caught the wicked vulture by its tail. This was a a great deed!",
+      "e": "爲하다 위하다 do for the good/benefit of; 行爲 행위 act, action, deed"
+    },
+    {
+      "q": "偽/僞",
+      "s": "위",
+      "k": "거짓",
+      "m": "falsehood",
+      "d": "Everything that Chuck Norris does is for the good of mankind. Anyone who says otherwise is telling a falsehood.",
+      "e": " 僞造 위조 forgery, counterfeiting"
+    },
+    {
+      "q": "-+𧘇",
+      "s": "",
+      "k": "",
+      "m": "*hairpin",
+      "d": "Here we have a quasi-pictograph of the colorful and decorated clips used to bind up long hair. Note its similarity to the scarf, which differs only by the addition of one stroke.",
+      "e": ""
+    },
+    {
+      "q": "畏",
+      "s": "외",
+      "k": "두려워할",
+      "m": "fear, dread, awe, *apprehensive",
+      "d": "She was very apprehensive about losing her hairpin in the ricefield.",
+      "e": "敬畏 경외 awe, dread"
+    },
+    {
+      "q": "長",
+      "s": "장",
+      "k": "길",
+      "m": "long",
+      "d": "The Geisha with <b>long</b> hair uses an ornate lacquered hairpin to bundle it all up.",
+      "e": "家長 가장 the head of a family, patriarch; 社長 사장 president of a company, head; 長期 장기 long term, perio"
+    },
+    {
+      "q": "張",
+      "s": "장",
+      "k": "베풀",
+      "m": "stretch, lengthen",
+      "d": "In desperation, after the Geisha broke the string on her bow, she lengthened out one of her own hairs and strung the bow with it.",
+      "e": "誇張 과장 exaggeration; 主張 주장 insistence, assertion, opinion"
+    },
+    {
+      "q": "帳",
+      "s": "장",
+      "k": "장막",
+      "m": "tent, screen; *notebook",
+      "d": "In ancient China books were rolled-up scrolls of paper. So notebooks were nothing more than long towels.",
+      "e": "揮帳 휘장 curtain, screen;  帳幕 장막 tent, pavilion; 練習帳 연습장 excercise book, workbook"
+    },
+    {
+      "q": "脹",
+      "s": "창",
+      "k": "부을",
+      "m": "swell, inflate, expand, *dilate",
+      "d": "Your pupils will dilate if you look at the moon for too long.",
+      "e": "膨脹 팽창 swelling, inflation, expansion, dilation"
+    },
+    {
+      "q": "髪/髮",
+      "s": "발",
+      "k": "터럭",
+      "m": "hair of the head",
+      "d": "Japanese teenage girls consider the style of the <b>hair</b> 彡 of the head very important. If a particular style is trendy, soon they all want their own <b>hair</b> 彡 of the same <b>length</b> 镸 as that of their <b>friends</b> up to the smallest touch.",
+      "e": "假髮 가발 wig"
+    },
+    {
+      "q": "展",
+      "s": "전",
+      "k": "펼",
+      "m": "unfold",
+      "d": "<b>Unfold</b> <b>flag</b>, take out the <b>salad</b>, pin the flag down with <b>hairpins</b> and have a picnic.",
+      "e": "展開 전개 spread, expand, unfolding, development; 發展 발전 expansion, extension, development, growth"
+    },
+    {
+      "q": "喪",
+      "s": "상",
+      "k": "잃을",
+      "m": "mourning, funeral; *to miss",
+      "d": "Here the dearly departed has been buried in the ground, with her favourite hairpin in place of a gravestone, and two mourners stand around crying with open mouths.",
+      "e": "初喪 초상 (a period of) mourning; 喪服 상복 mourning dress, funeral garments"
+    }
+  ]
+};
