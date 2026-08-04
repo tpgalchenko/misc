30e4e971f6e6f90ec75887e8d70264e6676eccfd
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-08-04 17:51:57 +0300

    .

---
+++ hanja-flash/l55.js
@@ -0,0 +1,173 @@
+const L55 = {
+  "name" : "L55",
+  "cards" : [
+    {
+      "q": "寅",
+      "s": "인",
+      "k": "범",
+      "m": "to respect, reverence; *sign of the tiger",
+      "d": "The zebra, at the first <b>sign of a tiger</b>, jumped into the <b>mid air</b> 宙. Unfortunately, he was in his <b>house</b>, got stuck in the <b>ceiling</b>, leaving only his <b>legs</b> poking out.",
+      "e": ""
+    },
+    {
+      "q": "演",
+      "s": "연",
+      "k": "펼",
+      "m": "perform, exercise",
+      "d": "At the kabuki play performance, the tiger mask wearing performer is scared of water.",
+      "e": "演出 연출 direction, production; 公演 공연 public performance"
+    },
+    {
+      "q": "辰",
+      "s": "진, 신",
+      "k": "별 이름, 때",
+      "m": "*sign of the dragon",
+      "d": "The <b>sign of the dragon</b> was carved into the <b>cliff</b> face with two <b>hairpins</b> (-+𧘇) that were abandoned in the sand below.",
+      "e": "日辰 일진 day's fortune; 生辰 생신 birthday"
+    },
+    {
+      "q": "辱",
+      "s": "욕",
+      "k": "욕될",
+      "m": "humiliate, insult, abuse; *embarass",
+      "d": "",
+      "e": "辱 욕 scolding, reprimand, rebuke, shame, disgrace, insult, humiliation"
+    },
+    {
+      "q": "震",
+      "s": "진",
+      "k": "우레",
+      "m": "shake, *quake",
+      "d": "When heavy rains disturb a dragon hiding under the earth, an earthquake may result.",
+      "e": "地震 지진 earthquake"
+    },
+    {
+      "q": "振",
+      "s": "진",
+      "k": "떨칠",
+      "m": "shake",
+      "d": "If you're talking to someone and notice that his fingers are starting to shake, it's often a sign of the dragon being behind you where the other person can see him coming.",
+      "e": "振子 진자 pendulum"
+    },
+    {
+      "q": "娠",
+      "s": "신",
+      "k": "아이 밸",
+      "m": "pregnant, *with child",
+      "d": "A woman who is with child can't be sacrificed to the dragon; virgins only need apply.",
+      "e": "姙娠/妊娠 임신 pregnancy"
+    },
+    {
+      "q": "唇",
+      "s": "진,순",
+      "k": "놀랄,입술",
+      "m": "lips",
+      "d": "Red lipstick on her mouth, she's wearing a shade called 'sign of the dragon'.",
+      "e": ""
+    },
+    {
+      "q": "農",
+      "s": "농",
+      "k": "농사",
+      "m": "agriculture",
+      "d": "The crops are starting to <b>bend</b> 曲 with their own weight, as <b>agriculture</b> is well under way under the <b>sign of the dragon</b> 辰 (late Spring).",
+      "e": "農事 농사 agriculture, farming"
+    },
+    {
+      "q": "濃",
+      "s": "농",
+      "k": "짙을",
+      "m": "thick, strong, *concentrated",
+      "d": "Water extracted from agricultural products makes them concentrated.",
+      "e": "濃度 농도 density, concentration, intensity"
+    },
+    {
+      "q": "关",
+      "s": "소,관",
+      "k": "웃을,관계할",
+      "m": "golden calf",
+      "d": "The story is told of the people of the Exodus that, in their distrust of Moses’ leadership, they gathered together and melted down their gold ornaments to fashion a golden calf for an idol. The animal horns and heavens here represent that god of theirs.",
+      "e": ""
+    },
+    {
+      "q": "送",
+      "s": "송",
+      "k": "보낼",
+      "m": "send off",
+      "d": "After worshipping the golden calf, God sent the Jews on a long road through the wilderness.",
+      "e": "郵送 우송 mail"
+    },
+    {
+      "q": "関/關",
+      "s": "관",
+      "k": "관계할",
+      "m": "relation, *connection",
+      "d": "The golden calf is sent off to the gates of Hell for it's connection to false idols.<br>In the old days, criminals with connections to particularly harsh crimes were strung together (聯), had their ears cut off, then sent out through the city gates. They were never seen again.",
+      "e": "關係 관계 relation, relationship; 關心 관심 interest, concern"
+    },
+    {
+      "q": "咲",
+      "s": "소",
+      "k": "웃음",
+      "m": "*blossom",
+      "d": "Evil laughs and blossoms from the mouths of those who worship the golden calf.",
+      "e": ""
+    },
+    {
+      "q": "鬼",
+      "s": "귀",
+      "k": "귀신",
+      "m": "ghost",
+      "d": "A ghost, or oni, has a single horn sticking out of the its forehead (the drop) and carries a huge club in the crook of its elbow. It wanders on its human legs, searching for skulls to smash open and brains to devour.",
+      "e": "鬼神 귀신 ghost"
+    },
+    {
+      "q": "醜",
+      "s": "추",
+      "k": "추할",
+      "m": "ugly",
+      "d": "Drink enough whisky and not even an ghost looks ugly any more...",
+      "e": "醜하다 추하다 ugly, bad, indecent, ignoble"
+    },
+    {
+      "q": "魂",
+      "s": "혼",
+      "k": "넋",
+      "m": "spirit, *soul",
+      "d": "When we die, our <b>soul</b> rises up in a <b>ghost</b>like <b>rising cloud</b> 云 of mist up to heaven.",
+      "e": "魂나다 혼나다 to get frightened out of one's wits, be startled"
+    },
+    {
+      "q": "魔",
+      "s": "마",
+      "k": "마귀",
+      "m": "demon, evil spirits, *witch",
+      "d": "The witch sold me some hemp that made me see ghosts.",
+      "e": "魔鬼 마귀 evil spirit, devil, demon, Satan"
+    },
+    {
+      "q": "魅",
+      "s": "매",
+      "k": "도깨비/매혹할",
+      "m": "fascination",
+      "d": "Even in these modern times our <b>fascination</b> with <b>ghosts</b> has <b>not yet</b> ended.",
+      "e": "魅力 매력 charm, bewitching, fascination"
+    },
+    {
+      "q": "塊",
+      "s": "괴",
+      "k": "흙덩이",
+      "m": "piece, lump, *clod",
+      "d": "A golem - an ghost creature made of soil.",
+      "e": "金塊 금괴 lump/nugger of gold"
+    },
+    {
+      "q": "襲",
+      "s": "습",
+      "k": "엄습할",
+      "m": "raid, attack",
+      "d": "One needs a fire-proof <b>garment</b> to <b>attack</b> a <b>dragon</b> 龍.",
+      "e": "掩襲 엄습 surprise/sudden attack"
+    }
+  ]
+};
