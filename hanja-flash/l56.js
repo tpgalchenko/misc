cdd9bc6e987d4d2e13b6d30bdb93fad643d90ac0
Taras P. Galchenko <t.galchenko@e-soft.ru>
2026-08-04 19:04:15 +0300

    .

---
+++ hanja-flash/l56.js
@@ -0,0 +1,149 @@
+const L56 = {
+  "name" : "L56",
+  "cards" : [
+    {
+      "q": "嚇",
+      "s": "하",
+      "k": "웃음소리",
+      "m": "*upbraid",
+      "d": "To <b>upbraid</b> someone, you criticize them in a menacing way. In other words, you jaw at them with your <b>mouth</b> until both sides of their face turn <b>red</b>.",
+      "e": ""
+    },
+    {
+      "q": "朕",
+      "s": "",
+      "k": "나 짐",
+      "m": "I, *majestic plural",
+      "d": "The king says 'Only We are allowed to partake of the flesh of the golden calf.'",
+      "e": "兆朕 조짐 omen, portent, symptoms"
+    },
+    {
+      "q": "雰",
+      "s": "분",
+      "k": "눈 날릴",
+      "m": "atmosphere",
+      "d": "<b>Rain</b> is only <b>part</b> of the <b>atmosphere</b> for a romantic porch date. Food, music, and candles are the rest.",
+      "e": "雰圍氣 분위기 atmosphere, air, environment"
+    },
+    {
+      "q": "個/箇/介",
+      "s": "개",
+      "k": "낱",
+      "m": "item",
+      "d": "This item is made of hardened bamboo.",
+      "e": ""
+    },
+    {
+      "q": "錬/鍊",
+      "s": "동",
+      "k": "보습 날",
+      "m": "smelt metals, *tempering",
+      "d": "The old sword smiths of the East were rightly famous. The secret of course was in how they tempered the metal.",
+      "e": "鍛鍊 단련 temper, forging"
+    },
+    {
+      "q": "遵",
+      "s": "준",
+      "k": "좇을",
+      "m": "obey, comply with, *abide by",
+      "d": "You must <b>abide by</b> the <b>revered</b> rules of the <b>road</b>! No devil's brew! And stay glued to your lane in the <b>road</b>!",
+      "e": "遵法 준법 obeying the law, law-abiding"
+    },
+    {
+      "q": "罷",
+      "s": "파",
+      "k": "마칠",
+      "m": "cease, finish, stop, *quit",
+      "d": "I won't <b>quit</b> studying until my <b>eyes</b> have the <b>ability</b> to read kanji!",
+      "e": "罷職 파직 dismissal/removal from office, discharge"
+    },
+    {
+      "q": "且",
+      "s": "차",
+      "k": "또",
+      "m": "moreover",
+      "d": "When someone says 'moreover', he wants you to look at, or cast your eye 目 over, one 一 more thing.",
+      "e": "苟且하다 구차하다 clumsy, awkward, ignoble, very poor, needy"
+    },
+    {
+      "q": "藻",
+      "s": "조",
+      "k": "마름",
+      "m": "seaweed",
+      "d": "Take seaweed (water + flower) and hang it to dry in a tree to produce nori goods.",
+      "e": "海藻 해조"
+    },
+    {
+      "q": "隷",
+      "s": "례/예",
+      "k": "종",
+      "m": "slave",
+      "d": "The samurai sits on the altar and watches the slave rake rice all day.",
+      "e": "奴隷 노예 slave"
+    },
+    {
+      "q": "癒",
+      "s": "유",
+      "k": "병 나을",
+      "m": "recovery, healing",
+      "d": "At the meeting of butchers, the prevention and healing of heart disease by eating lean cuts was discussed at length.",
+      "e": "治癒 치유 healing, cure, recovery"
+    },
+    {
+      "q": "璽",
+      "s": "새",
+      "k": "옥새",
+      "m": "imperial seal",
+      "d": "If you find a <b>jewel</b> with the <b>imperial seal</b> on it, just <b>let it be</b> 爾. You wouldn't want to be accused of stealing the royal family jewels.",
+      "e": "玉璽 옥새 the greate seal made of jade, imperial seal"
+    },
+    {
+      "q": "潟",
+      "s": "석",
+      "k": "개펄",
+      "m": "land impregnated with salt from the tide, *lagoon",
+      "d": "Right part: mortar bound up over oven fire, comprising boiling cauldron. Lagoon is a shallow pond, thus easily warmed by sun, large-scale equivalent of a boiling cauldron.",
+      "e": "干潟地 간석지 tideland, dry beach"
+    },
+    {
+      "q": "丹",
+      "s": "단",
+      "k": "붉을 단",
+      "m": "cinnabar",
+      "d": "How about a rusty cinnabar colored dilapidated boat, found abandoned somewhere. The smoke-stack and the keel have fallen off...",
+      "e": "丹楓 단풍 red leaves, tinted autumnal leaves"
+    },
+    {
+      "q": "丑",
+      "s": "축",
+      "k": "소",
+      "m": "sign of the cow",
+      "d": "77 cows behind a fence.",
+      "e": ""
+    },
+    {
+      "q": "羞",
+      "s": "수",
+      "k": "부끄러울",
+      "m": "disgrace, shame, humiliate",
+      "d": "The <b>sheep</b> is <b>ashamed</b> of it's sheepish nature, and <b>dons</b> the <b>sign of the cow</b> as a disguise.",
+      "e": "羞恥 수치 shyness, shame, dishonor, disgrace"
+    },
+    {
+      "q": "卯",
+      "s": "묘",
+      "k": "넷째 지지",
+      "m": "sign of the hare",
+      "d": "Rabbit ears",
+      "e": ""
+    },
+    {
+      "q": "巳",
+      "s": "사",
+      "k": "뱀",
+      "m": "sign of the snake",
+      "d": "Image of a snake",
+      "e": ""
+    }
+  ]
+};
